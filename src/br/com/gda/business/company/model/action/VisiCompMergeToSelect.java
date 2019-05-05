53cc705b84c0a7d11e40691c270c81f74228c187
mmaciel <mmaciel@mind5.com>
2019-05-05 19:29:25 -0300

    Company #04

--- src/br/com/gda/business/company/model/action/VisiCompMergeToSelect.java
+++ src/br/com/gda/business/company/model/action/VisiCompMergeToSelect.java
@@ -5,9 +5,8 @@ import java.util.List;
 
 import br.com.gda.business.company.info.CompInfo;
 import br.com.gda.business.company.info.CompMerger;
-import br.com.gda.business.company.model.decisionTree.RootCompSelect;
+import br.com.gda.model.action.ActionStd;
 import br.com.gda.model.action.ActionVisitorTemplateMergeV2;
-import br.com.gda.model.decisionTree.DeciTree;
 
 final class VisiCompMergeToSelect extends ActionVisitorTemplateMergeV2<CompInfo, CompInfo> {
 	
@@ -17,8 +16,8 @@ final class VisiCompMergeToSelect extends ActionVisitorTemplateMergeV2<CompInfo,
 	
 	
 	
-	@Override protected Class<? extends DeciTree<CompInfo>> getTreeClassHook() {
-		return RootCompSelect.class;
+	@Override protected Class<? extends ActionStd<CompInfo>> getActionClassHook() {
+		return StdCompSelect.class;
 	}
 	
 	
