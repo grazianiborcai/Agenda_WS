a13c8268fc65e7a4cc3a4de7007504cfc665fac7
mmaciel <mmaciel@mind5.com>
2019-05-14 19:18:11 -0300

    Stolevate #06

--- src/br/com/gda/business/storeLeaveDate/info/StolevateMerger.java
+++ src/br/com/gda/business/storeLeaveDate/info/StolevateMerger.java
@@ -2,25 +2,10 @@ package br.com.gda.business.storeLeaveDate.info;
 
 import java.util.List;
 
-import br.com.gda.business.planningTime.info.PlanDataInfo;
 import br.com.gda.info.InfoMerger;
 import br.com.gda.security.username.info.UsernameInfo;
 
 public final class StolevateMerger {
-	public static StolevateInfo mergeWithPlan(PlanDataInfo sourceOne, StolevateInfo sourceTwo) {
-		InfoMerger<StolevateInfo, PlanDataInfo> merger = new StolevateMergerPlan();		
-		return merger.merge(sourceOne, sourceTwo);
-	}
-	
-	
-	
-	public static List<StolevateInfo> mergeWithPlan(List<PlanDataInfo> sourceOnes, List<StolevateInfo> sourceTwos) {
-		InfoMerger<StolevateInfo, PlanDataInfo> merger = new StolevateMergerPlan();		
-		return merger.merge(sourceOnes, sourceTwos);
-	}
-	
-	
-	
 	public static StolevateInfo mergeWithUsername(UsernameInfo sourceOne, StolevateInfo sourceTwo) {
 		InfoMerger<StolevateInfo, UsernameInfo> merger = new StolevateMergerUsername();		
 		return merger.merge(sourceOne, sourceTwo);
