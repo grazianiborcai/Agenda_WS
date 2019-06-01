1500bf42eb66c1b35a64b57d9456031a68c62739
mmaciel <mmaciel@mind5.com>
2019-06-01 10:00:50 -0300

    Cart #19

--- src/br/com/gda/business/cart/model/action/VisiCartEnforceKey.java
+++ src/br/com/gda/business/cart/model/action/VisiCartEnforceKey.java
@@ -1,14 +1,14 @@
 package br.com.gda.business.cart.model.action;
 
 import br.com.gda.business.cart.info.CartInfo;
+import br.com.gda.business.cart.info.CartSetterKey;
+import br.com.gda.info.InfoSetter;
 import br.com.gda.model.action.ActionVisitorTemplateEnforce;
 
 final class VisiCartEnforceKey extends ActionVisitorTemplateEnforce<CartInfo> {
 	
 	@Override protected CartInfo enforceHook(CartInfo recordInfo) {
-		CartInfo enforcedRecord = new CartInfo();
-		enforcedRecord.codOwner = recordInfo.codOwner;
-		enforcedRecord.codUser = recordInfo.codUser;
-		return enforcedRecord;
+		InfoSetter<CartInfo> setter = new CartSetterKey();
+		return setter.setAttr(recordInfo);
 	}
 }
