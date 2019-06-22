aebcb85408531221244bb6d82232c1278a56105f
mmaciel <mmaciel@mind5.com>
2019-06-22 12:45:02 -0300

    Userap #02

---
+++ src/br/com/gda/business/userSnapshot/info/UserapCopier.java
@@ -0,0 +1,21 @@
+package br.com.gda.business.userSnapshot.info;
+
+
+import java.util.List;
+
+import br.com.gda.info.InfoCopier;
+import br.com.gda.payment.customerPartner.info.CusparInfo;
+
+public final class UserapCopier {	
+	public static UserapInfo copyFromCuspar(CusparInfo source) {
+		InfoCopier<UserapInfo, CusparInfo> copier = new UserapCopyCuspar();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<UserapInfo> copyFromCuspar(List<CusparInfo> sources) {
+		InfoCopier<UserapInfo, CusparInfo> copier = new UserapCopyCuspar();
+		return copier.makeCopy(sources);
+	}
+}
