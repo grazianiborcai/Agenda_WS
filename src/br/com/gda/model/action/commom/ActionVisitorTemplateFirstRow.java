36f1795a3be0e013073e4a23940d36dfaa8ebc24
mmaciel <mmaciel@mind5.com>
2019-07-25 16:04:47 -0300

    Model #04

--- src/br/com/gda/model/action/commom/ActionVisitorTemplateFirstRow.java
+++ src/br/com/gda/model/action/commom/ActionVisitorTemplateFirstRow.java
@@ -1,11 +1,14 @@
 package br.com.gda.model.action.commom;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import br.com.gda.common.SystemMessage;
 import br.com.gda.info.InfoRecord;
 import br.com.gda.model.action.ActionVisitorEnforce;
 
@@ -13,12 +16,50 @@ import br.com.gda.model.action.ActionVisitorEnforce;
 public abstract class ActionVisitorTemplateFirstRow<T extends InfoRecord> implements ActionVisitorEnforce<T> {
 	
 	@Override public List<T> executeTransformation(List<T> recordInfos) {
-		List<T> resultRecords = new ArrayList<>();		
+		List<T> sortedList = sortList(recordInfos);	
+		return getFirstRow(sortedList);
+	}
+	
+	
+	
+	private List<T> sortList(List<T> recordInfos) {
+		Comparator<T> comparator = getComparatorHook();		
+		checkArgument(comparator);		
 		
-		resultRecords.add(makeClone(recordInfos.get(0)));		
+		return tryToSortList(recordInfos, comparator);		
+	}
+	
+	
+	
+	protected Comparator<T> getComparatorHook() {
+		//Template method to be overridden by subclasses
+		logException(new IllegalStateException(SystemMessage.NO_TEMPLATE_IMPLEMENTATION));
+		throw new IllegalStateException(SystemMessage.NO_TEMPLATE_IMPLEMENTATION);
+	}
+	
+	
+	
+	private List<T> tryToSortList(List<T> recordInfos, Comparator<T> compator) {
+		try {
+			Collections.sort(recordInfos, compator); 
+			return recordInfos;
 		
-		return resultRecords;
-	}	
+		} catch (Exception e) {
+			logException(new IllegalArgumentException(e));
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	
+	
+	private List<T> getFirstRow(List<T> recordInfos) {
+		List<T> results = new ArrayList<>();
+		
+		T firstRow = makeClone(recordInfos.get(0));	
+		results.add(firstRow);
+		
+		return results;
+	}
 	
 	
 	
@@ -30,6 +71,15 @@ public abstract class ActionVisitorTemplateFirstRow<T extends InfoRecord> implem
 		} catch (CloneNotSupportedException e) {
 			logException(e);
 			throw new IllegalStateException(e);
+		}
+	}
+	
+	
+	
+	private void checkArgument(Comparator<T> compator) {
+		if (compator == null) {
+			logException(new NullPointerException("Compator" + SystemMessage.NULL_ARGUMENT));
+			throw new NullPointerException("Compator" + SystemMessage.NULL_ARGUMENT);
 		}
 	}
 	
