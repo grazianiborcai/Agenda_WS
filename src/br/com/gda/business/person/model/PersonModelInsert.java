85946bd9e254be1e4e3e47aee96573800cee416c
mmaciel <mmaciel@mind5.com>
2019-08-18 17:38:57 -0300

    Person #11

--- src/br/com/gda/business/person/model/PersonModelInsert.java
+++ src/br/com/gda/business/person/model/PersonModelInsert.java
@@ -1,72 +1,28 @@
 package br.com.gda.business.person.model;
 
-import java.sql.Connection;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Response;
-
 import br.com.gda.business.person.info.PersonInfo;
 import br.com.gda.business.person.model.decisionTree.RootPersonInsert;
-import br.com.gda.common.DbConnection;
-import br.com.gda.common.DbSchema;
-import br.com.gda.model.Model;
+import br.com.gda.model.ModelTemplate;
 import br.com.gda.model.decisionTree.DeciTree;
-import br.com.gda.model.decisionTree.DeciTreeFactory;
 import br.com.gda.model.decisionTree.DeciTreeOption;
-import br.com.gda.model.obsolete.ModelHelper_;
-import br.com.gda.model.obsolete.ModelOption_;
 
-public final class PersonModelInsert implements Model {
-	private Model helper;
-	private Connection conn;
-	private String schemaName;
-	
-	
+public final class PersonModelInsert extends ModelTemplate<PersonInfo> {
+
 	public PersonModelInsert(String incomingData, HttpServletRequest request) {
-		initialize();
-		buildHelper(incomingData, request);
+		super(incomingData, request, PersonInfo.class);
 	}
 	
 	
 	
-	private void initialize() {
-		this.conn = DbConnection.getConnection();
-		this.schemaName = DbSchema.getDefaultSchemaName();
+	@Override protected DeciTree<PersonInfo> getDecisionTreeHook(DeciTreeOption<PersonInfo> option) {
+		return new RootPersonInsert(option);
 	}
 	
 	
 	
-	private void buildHelper(String incomingData, HttpServletRequest request) {
-		ModelOption_<PersonInfo> helperOption = new ModelOption_<>();
-		
-		helperOption.recordClass = PersonInfo.class;
-		helperOption.deciTreeFactory = new TreeFactory();
-		helperOption.conn = this.conn;
-		helperOption.schemaName = this.schemaName;
-		
-		helper = ModelHelper_.factory(helperOption, incomingData, request);
-	}
-
-
-	
-	@Override public boolean executeRequest() {
-		return helper.executeRequest();
-	}
-
-
-	
-	@Override public Response getResponse() {
-		return helper.getResponse();
-	}
-	
-	
-	
-	
-	
-	
-	private static class TreeFactory implements DeciTreeFactory<PersonInfo> {		
-		@Override public DeciTree<PersonInfo> getInstance(DeciTreeOption<PersonInfo> option) {
-			return new RootPersonInsert(option);
-		}			
+	@Override protected Class<?> getImplamentationClassHook() {
+		return this.getClass();
 	}
 }
+		
