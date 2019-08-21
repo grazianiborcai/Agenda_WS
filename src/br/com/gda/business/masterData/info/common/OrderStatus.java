5744373b26bb27126fcc7c8aaced7eb08cdf85fa
mmaciel <mmaciel@mind5.com>
2019-08-21 17:02:47 -0300

    MasterData #09

--- src/br/com/gda/business/masterData/info/common/OrderStatus.java
+++ src/br/com/gda/business/masterData/info/common/OrderStatus.java
@@ -1,5 +1,10 @@
 package br.com.gda.business.masterData.info.common;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import br.com.gda.common.SystemMessage;
+
 public enum OrderStatus {
 	CANCELLED("CANCELLED"),
 	CREATED("CREATED"), 
@@ -19,5 +24,29 @@ public enum OrderStatus {
 	
 	public String getCodStatus() {
 		return codStatus;
+	}
+	
+	
+	
+	static public OrderStatus getOrderStatus(String cod) {
+		if (cod == null)
+			return null;
+		
+		
+		for(OrderStatus eachElem : OrderStatus.values()) {
+			if (eachElem.getCodStatus().equals(cod))
+				return eachElem;
+		}
+		
+		
+		logException(new IllegalArgumentException(SystemMessage.ARGUMENT_NOT_FOUND));
+		throw new IllegalArgumentException(SystemMessage.ARGUMENT_NOT_FOUND);
+	}
+	
+	
+	
+	static private void logException(Exception e) {
+		Logger logger = LogManager.getLogger(OrderStatus.class);
+		logger.error(e.getMessage(), e);
 	}
 }
