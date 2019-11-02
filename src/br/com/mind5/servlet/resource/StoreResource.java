d36c6313c1a822500bef674d09e294cc3e878d9d
mmaciel <mmaciel@mind5.com>
2019-11-02 11:57:16 -0300

    Store 30

--- src/br/com/mind5/servlet/resource/StoreResource.java
+++ src/br/com/mind5/servlet/resource/StoreResource.java
@@ -65,6 +65,7 @@ public class StoreResource {
 	@POST
 	@Path(INSERT_STORE)
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response insertStore(@Context HttpServletRequest request, String incomingData) {
 		
 		Model model = new StoreModelInsert(incomingData, request);
@@ -77,6 +78,7 @@ public class StoreResource {
 	@POST
 	@Path(UPDATE_STORE)
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response updateStore(@Context HttpServletRequest request, String incomingData) {
 		
 		Model model = new StoreModelUpdate(incomingData, request);
@@ -88,6 +90,7 @@ public class StoreResource {
 	
 	@DELETE
 	@Path(DELETE_STORE)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response deleteStore(@HeaderParam("TOKEN_OWNER")    	@DefaultValue("-1") long codOwner, 
 			                    @HeaderParam("codStore")       	@DefaultValue("-1") long codStore,
 			                    @HeaderParam("codLanguage")		@DefaultValue("EN") String codLanguage,
@@ -108,6 +111,7 @@ public class StoreResource {
 	
 	@GET
 	@Path(SELECT_STORE)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response selectStore(@HeaderParam("TOKEN_OWNER")    @DefaultValue("-1") long codOwner, 
 			                    @HeaderParam("codStore")       @DefaultValue("-1") int codStore,
 			                    @HeaderParam("codLanguage")	   @DefaultValue("EN") String codLanguage,
@@ -128,6 +132,7 @@ public class StoreResource {
 	
 	@GET
 	@Path(SELECT_STORE_LIST)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response selectStolis(@HeaderParam("TOKEN_OWNER")    @DefaultValue("-1") long codOwner, 
 			                     @HeaderParam("codStore")       @DefaultValue("-1") int codStore,
 			                     @HeaderParam("codLanguage")	   @DefaultValue("EN") String codLanguage,
@@ -148,6 +153,7 @@ public class StoreResource {
 	
 	@GET
 	@Path(SELECT_STORE_WTIME)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response selectStoreWTime(@HeaderParam("TOKEN_OWNER") @DefaultValue("-1") long codOwner, 
 			                         @HeaderParam("codStore")    @DefaultValue("-1") int codStore,
 			                         @HeaderParam("codWeekday")  @DefaultValue("-1") int codWeekday,			                         
@@ -182,6 +188,7 @@ public class StoreResource {
 	
 	@DELETE
 	@Path(DELETE_STORE_WTIME)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response deleteStoreWTime(@HeaderParam("TOKEN_OWNER") @DefaultValue("-1") long codOwner, 
 			                         @HeaderParam("codStore")    @DefaultValue("-1") int codStore,
 			                         @HeaderParam("codWeekday")  @DefaultValue("-1") int codWeekday,
@@ -205,6 +212,7 @@ public class StoreResource {
 	@POST
 	@Path(UPDATE_STORE_WTIME)
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response updateStoreWTime(@Context HttpServletRequest request, String incomingData) {
 
 		Model model = new StowotmModelUpdate(incomingData, request);
@@ -318,9 +326,10 @@ public class StoreResource {
 	
 	@GET
 	@Path(SELECT_STORE_PAY_PARTNER_LIST)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response selectStoplis(@HeaderParam("TOKEN_OWNER")    @DefaultValue("-1") long codOwner, 
 			                      @HeaderParam("codStore")       @DefaultValue("-1") int codStore,
-			                      @HeaderParam("codLanguage")	@DefaultValue("EN") String codLanguage,
+			                      @HeaderParam("codLanguage")	 @DefaultValue("EN") String codLanguage,
 			                      @HeaderParam("TOKEN_USERNAME") String username) {
 
 		StoplisInfo recordInfo = new StoplisInfo();
