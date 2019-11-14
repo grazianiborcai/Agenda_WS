228c07e61d1e0af905582dad6e265478b5946036
mmaciel <mmaciel@mind5.com>
2019-11-14 20:24:39 -0200

    Emposarch #06

---
+++ src/br/com/mind5/business/employeePositionSearch/dao/EmposarchJoinEmp.java
@@ -0,0 +1,59 @@
+package br.com.mind5.business.employeePositionSearch.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.mind5.business.employee.dao.EmpDbTableColumn;
+import br.com.mind5.business.employeePosition.dao.EmposDbTableColumn;
+import br.com.mind5.dao.DaoJoin;
+import br.com.mind5.dao.DaoJoinBuilder;
+import br.com.mind5.dao.DaoJoinColumn;
+import br.com.mind5.dao.DaoJoinType;
+import br.com.mind5.dao.common.DaoDbTable;
+
+final class EmposarchJoinEmp implements DaoJoinBuilder {
+	private final String leftTable;
+	
+	
+	public EmposarchJoinEmp(String leftTableName) {
+		leftTable = leftTableName;
+	}
+
+	
+	
+	@Override public DaoJoin build() {
+		DaoJoin join = new DaoJoin();
+		join.rightTableName = DaoDbTable.EMP_TABLE;
+		join.joinType = DaoJoinType.INNER_JOIN;
+		join.joinColumns = getJoinColumns(leftTable);
+		join.constraintClause = null;
+		
+		return join;
+	}
+	
+	
+	
+	private List<DaoJoinColumn> getJoinColumns(String leftTableName) {
+		List<DaoJoinColumn> joinColumns = new ArrayList<>();
+		
+		DaoJoinColumn oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = EmposDbTableColumn.COL_COD_EMPLOYEE;
+		oneColumn.rightColumnName = EmpDbTableColumn.COL_COD_EMPLOYEE;
+		joinColumns.add(oneColumn);
+		
+		oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = EmposDbTableColumn.COL_COD_OWNER;
+		oneColumn.rightColumnName = EmpDbTableColumn.COL_COD_OWNER;
+		joinColumns.add(oneColumn);
+		
+		oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = EmposDbTableColumn.COL_RECORD_MODE;
+		oneColumn.rightColumnName = EmpDbTableColumn.COL_RECORD_MODE;
+		joinColumns.add(oneColumn);
+		
+		return joinColumns;
+	}
+}
