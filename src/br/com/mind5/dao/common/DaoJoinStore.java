0125a377fdd2cca6d7613c84c3a3d393dd64afef
mmaciel <mmaciel@mind5.com>
2019-11-15 10:05:43 -0200

    Dao #08

---
+++ src/br/com/mind5/dao/common/DaoJoinStore.java
@@ -0,0 +1,59 @@
+package br.com.mind5.dao.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.mind5.business.employeePosition.dao.EmposDbTableColumn;
+import br.com.mind5.business.store.dao.StoreDbTableColumn;
+import br.com.mind5.dao.DaoJoin;
+import br.com.mind5.dao.DaoJoinBuilder;
+import br.com.mind5.dao.DaoJoinColumn;
+import br.com.mind5.dao.DaoJoinType;
+import br.com.mind5.dao.common.DaoDbTable;
+
+public final class DaoJoinStore implements DaoJoinBuilder {
+	private final String leftTable;
+	
+	
+	public DaoJoinStore(String leftTableName) {
+		leftTable = leftTableName;
+	}
+
+	
+	
+	@Override public DaoJoin build() {
+		DaoJoin join = new DaoJoin();
+		join.rightTableName = DaoDbTable.STORE_TABLE;
+		join.joinType = DaoJoinType.INNER_JOIN;
+		join.joinColumns = getJoinColumns(leftTable);
+		join.constraintClause = null;
+		
+		return join;
+	}
+	
+	
+	
+	private List<DaoJoinColumn> getJoinColumns(String leftTableName) {
+		List<DaoJoinColumn> joinColumns = new ArrayList<>();
+		
+		DaoJoinColumn oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = EmposDbTableColumn.COL_COD_STORE;
+		oneColumn.rightColumnName = StoreDbTableColumn.COL_COD_STORE;
+		joinColumns.add(oneColumn);
+		
+		oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = EmposDbTableColumn.COL_COD_OWNER;
+		oneColumn.rightColumnName = StoreDbTableColumn.COL_COD_OWNER;
+		joinColumns.add(oneColumn);
+		
+		oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = EmposDbTableColumn.COL_RECORD_MODE;
+		oneColumn.rightColumnName = StoreDbTableColumn.COL_RECORD_MODE;
+		joinColumns.add(oneColumn);
+		
+		return joinColumns;
+	}
+}
