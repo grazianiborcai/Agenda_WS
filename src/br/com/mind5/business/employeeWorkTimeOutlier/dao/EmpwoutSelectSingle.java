347a673005e19c6433e384d5022a6f8b39bc25c6
mmaciel <mmaciel@mind5.com>
2019-11-16 11:06:30 -0200

    Empwout #05

--- src/br/com/mind5/business/employeeWorkTimeOutlier/dao/EmpwoutSelectSingle.java
+++ src/br/com/mind5/business/employeeWorkTimeOutlier/dao/EmpwoutSelectSingle.java
@@ -8,48 +8,47 @@ import java.util.List;
 
 import br.com.mind5.business.employeeWorkTimeOutlier.info.EmpwoutInfo;
 import br.com.mind5.dao.DaoFormatter;
+import br.com.mind5.dao.DaoJoin;
+import br.com.mind5.dao.DaoJoinBuilder;
 import br.com.mind5.dao.DaoOperation;
 import br.com.mind5.dao.DaoResultParser;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper;
-import br.com.mind5.dao.DaoStmtOption;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
+import br.com.mind5.dao.common.DaoJoinEmp;
+import br.com.mind5.dao.common.DaoJoinStore;
 import br.com.mind5.dao.common.DaoOptionValue;
 
-public final class EmpwoutSelectSingle implements DaoStmt<EmpwoutInfo> {
-	private final String LT_EMP_WT = DaoDbTable.EMP_WT_TABLE;	
-	
-	private DaoStmt<EmpwoutInfo> stmtSql;
-	private DaoStmtOption<EmpwoutInfo> stmtOption;
-	
+public final class EmpwoutSelectSingle extends DaoStmtTemplate<EmpwoutInfo> {
+	private final String MAIN_TABLE = DaoDbTable.EMP_WT_TABLE;	
 	
 	
 	public EmpwoutSelectSingle(Connection conn, EmpwoutInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();		
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, EmpwoutInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = LT_EMP_WT;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(DaoDbTable.EMP_WT_OUTLIER_VIEW);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause(stmtOption.tableName, stmtOption.recordInfo);
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause(String tableName, EmpwoutInfo recordInfo) {
+	@Override protected String getLookupTableHook() {
+		return DaoDbTable.EMP_WT_OUTLIER_VIEW;
+	}
+	
+	
+	
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, EmpwoutInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
 		whereOption.ignoreNull = DaoOptionValue.DONT_IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.DONT_IGNORE_RECORD_MODE;		
@@ -60,65 +59,43 @@ public final class EmpwoutSelectSingle implements DaoStmt<EmpwoutInfo> {
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<EmpwoutInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<EmpwoutInfo> getNewInstance() {
-		return new EmpwoutSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	private class ResultParser implements DaoResultParser<EmpwoutInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
+	@Override protected List<DaoJoin> getJoinsHook() {
+		List<DaoJoin> joins = new ArrayList<>();
 		
-		@Override public List<EmpwoutInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<EmpwoutInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
+		DaoJoinBuilder joinStore = new DaoJoinStore(MAIN_TABLE);		
+		joins.add(joinStore.build());
 		
-			do {				
-				EmpwoutInfo dataInfo = new EmpwoutInfo();
-				dataInfo.codOwner = stmtResult.getLong(EmpwoutDbTableColumn.COL_COD_OWNER);
-				dataInfo.codStore = stmtResult.getLong(EmpwoutDbTableColumn.COL_COD_STORE);
-				dataInfo.codEmployee = stmtResult.getLong(EmpwoutDbTableColumn.COL_COD_EMPLOYEE);
-				dataInfo.codWeekday = stmtResult.getInt(EmpwoutDbTableColumn.COL_COD_WEEKDAY);
-				dataInfo.recordMode = stmtResult.getString(EmpwoutDbTableColumn.COL_RECORD_MODE);
-				dataInfo.beginTime = DaoFormatter.sqlToLocalTime(stmtResult, EmpwoutDbTableColumn.COL_BEGIN_TIME);
-				dataInfo.endTime = DaoFormatter.sqlToLocalTime(stmtResult, EmpwoutDbTableColumn.COL_END_TIME);
+		DaoJoinBuilder joinEmp = new DaoJoinEmp(MAIN_TABLE);		
+		joins.add(joinEmp.build());
+		
+		return joins;
+	}
+	
+	
+	
+	@Override protected DaoResultParser<EmpwoutInfo> getResultParserHook() {
+		return new DaoResultParser<EmpwoutInfo>() {
+			@Override public List<EmpwoutInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
+				List<EmpwoutInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);				
-			} while (stmtResult.next());
+				if (stmtResult.next() == false)				
+					return finalResult;
 			
-			return finalResult;
-		}
+				do {				
+					EmpwoutInfo dataInfo = new EmpwoutInfo();
+					dataInfo.codOwner = stmtResult.getLong(EmpwoutDbTableColumn.COL_COD_OWNER);
+					dataInfo.codStore = stmtResult.getLong(EmpwoutDbTableColumn.COL_COD_STORE);
+					dataInfo.codEmployee = stmtResult.getLong(EmpwoutDbTableColumn.COL_COD_EMPLOYEE);
+					dataInfo.codWeekday = stmtResult.getInt(EmpwoutDbTableColumn.COL_COD_WEEKDAY);
+					dataInfo.recordMode = stmtResult.getString(EmpwoutDbTableColumn.COL_RECORD_MODE);
+					dataInfo.beginTime = DaoFormatter.sqlToLocalTime(stmtResult, EmpwoutDbTableColumn.COL_BEGIN_TIME);
+					dataInfo.endTime = DaoFormatter.sqlToLocalTime(stmtResult, EmpwoutDbTableColumn.COL_END_TIME);
+					
+					finalResult.add(dataInfo);				
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
