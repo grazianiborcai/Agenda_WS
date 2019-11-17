f73e1f1444a85be932fa712b963e50eb942f3a66
mmaciel <mmaciel@mind5.com>
2019-11-17 20:45:29 -0200

    Stowotarch #03

--- src/br/com/mind5/business/storeWorkTimeSearch/dao/StowotarchSelectSingle.java
+++ src/br/com/mind5/business/storeWorkTimeSearch/dao/StowotarchSelectSingle.java
@@ -7,117 +7,87 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.business.storeWorkTimeSearch.info.StowotarchInfo;
+import br.com.mind5.dao.DaoJoin;
+import br.com.mind5.dao.DaoJoinBuilder;
 import br.com.mind5.dao.DaoOperation;
 import br.com.mind5.dao.DaoResultParser;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper;
-import br.com.mind5.dao.DaoStmtOption;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
+import br.com.mind5.dao.common.DaoJoinStore;
 import br.com.mind5.dao.common.DaoOptionValue;
 
-public final class StowotarchSelectSingle implements DaoStmt<StowotarchInfo> {
-	private final String LT_STORE_WORK_TIME = DaoDbTable.STORE_WT_TABLE;	
-	
-	private DaoStmt<StowotarchInfo> stmtSql;
-	private DaoStmtOption<StowotarchInfo> stmtOption;
-	
+public final class StowotarchSelectSingle extends DaoStmtTemplate<StowotarchInfo> {
+	private final String MAIN_TABLE = DaoDbTable.STORE_WT_TABLE;		
 	
 	
 	public StowotarchSelectSingle(Connection conn, StowotarchInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, StowotarchInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = LT_STORE_WORK_TIME;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(DaoDbTable.STORE_WTIME_SEARCH_VIEW);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected String getLookupTableHook() {
+		return DaoDbTable.STORE_WTIME_SEARCH_VIEW;
+	}
+	
+	
+	
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, StowotarchInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
 		whereOption.ignoreNull = DaoOptionValue.IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.DONT_IGNORE_RECORD_MODE;		
 		
-		DaoStmtWhere whereClause = new StowotarchWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new StowotarchWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
 	}
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<StowotarchInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<StowotarchInfo> getNewInstance() {
-		return new StowotarchSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser<StowotarchInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
+	@Override protected List<DaoJoin> getJoinsHook() {
+		List<DaoJoin> joins = new ArrayList<>();
 		
-		@Override public List<StowotarchInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<StowotarchInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				StowotarchInfo dataInfo = new StowotarchInfo();
-				dataInfo.codOwner = stmtResult.getLong(StowotarchDbTableColumn.COL_COD_OWNER);
-				dataInfo.codStore = stmtResult.getLong(StowotarchDbTableColumn.COL_COD_STORE);
-				dataInfo.codWeekday = stmtResult.getInt(StowotarchDbTableColumn.COL_COD_WEEKDAY);
-				dataInfo.recordMode = stmtResult.getString(StowotarchDbTableColumn.COL_RECORD_MODE);				
+		DaoJoinBuilder joinStore = new DaoJoinStore(MAIN_TABLE);		
+		joins.add(joinStore.build());
+		
+		return joins;
+	}
+	
+	
+	
+	@Override protected DaoResultParser<StowotarchInfo> getResultParserHook() {
+		return new DaoResultParser<StowotarchInfo>() {		
+			@Override public List<StowotarchInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
+				List<StowotarchInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					StowotarchInfo dataInfo = new StowotarchInfo();
+					dataInfo.codOwner = stmtResult.getLong(StowotarchDbTableColumn.COL_COD_OWNER);
+					dataInfo.codStore = stmtResult.getLong(StowotarchDbTableColumn.COL_COD_STORE);
+					dataInfo.codWeekday = stmtResult.getInt(StowotarchDbTableColumn.COL_COD_WEEKDAY);
+					dataInfo.recordMode = stmtResult.getString(StowotarchDbTableColumn.COL_RECORD_MODE);				
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
