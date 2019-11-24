c1566dbb003a98f49650cd9611f0620b64a8c9ab
mmaciel <mmaciel@mind5.com>
2019-11-24 13:05:33 -0200

    Stowotarch #06

---
+++ src/br/com/mind5/business/storeWorkTimeSearch/info/StowotarchCopyEmpwotm.java
@@ -0,0 +1,24 @@
+package br.com.mind5.business.storeWorkTimeSearch.info;
+
+import br.com.mind5.business.employeeWorkTime.info.EmpwotmInfo;
+import br.com.mind5.info.InfoCopierTemplate;
+
+final class StowotarchCopyEmpwotm extends InfoCopierTemplate<StowotarchInfo, EmpwotmInfo>{
+	
+	public StowotarchCopyEmpwotm() {
+		super();
+	}
+	
+	
+	
+	@Override protected StowotarchInfo makeCopyHook(EmpwotmInfo source) {
+		StowotarchInfo result = new StowotarchInfo();
+		
+		result.codOwner = source.codOwner;
+		result.codStore = source.codStore;
+		result.username = source.username;
+		result.codLanguage = source.codLanguage;
+		
+		return result;
+	}
+}
