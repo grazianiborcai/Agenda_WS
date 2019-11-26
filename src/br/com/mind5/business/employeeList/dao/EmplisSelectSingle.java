c7a02f92d89d8a433761c4dde696933281fe4267
mmaciel <mmaciel@mind5.com>
2019-11-26 20:00:26 -0200

    Emplis #05

--- src/br/com/mind5/business/employeeList/dao/EmplisSelectSingle.java
+++ src/br/com/mind5/business/employeeList/dao/EmplisSelectSingle.java
@@ -9,117 +9,76 @@ import java.util.List;
 import br.com.mind5.business.employeeList.info.EmplisInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class EmplisSelectSingle implements DaoStmt<EmplisInfo> {
-	private final String LT_EMP = DaoDbTable.EMP_TABLE;
-	
-	private DaoStmt<EmplisInfo> stmtSql;
-	private DaoStmtOption_<EmplisInfo> stmtOption;
-	
+public final class EmplisSelectSingle extends DaoStmtTemplate<EmplisInfo> {
+	private final String MAIN_TABLE = DaoDbTable.EMP_TABLE;
 	
 	
 	public EmplisSelectSingle(Connection conn, EmplisInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, EmplisInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = LT_EMP;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(DaoDbTable.EMP_LIST_VIEW);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected String getLookupTableHook() {
+		return DaoDbTable.EMP_LIST_VIEW;
+	}
+	
+	
+	
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, EmplisInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
 		whereOption.ignoreNull = DaoOptionValue.IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.DONT_IGNORE_RECORD_MODE;		
 		
-		DaoStmtWhere whereClause = new EmplisWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new EmplisWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
 	}
-
-	
-		
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<EmplisInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<EmplisInfo> getNewInstance() {
-		return new EmplisSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
 	
 	
 	
 	
 	
 	
-	private static class ResultParser implements DaoResultParser_<EmplisInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<EmplisInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<EmplisInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				EmplisInfo dataInfo = new EmplisInfo();
-				dataInfo.codOwner = stmtResult.getLong(EmplisDbTableColumn.COL_COD_OWNER);
-				dataInfo.codEmployee = stmtResult.getLong(EmplisDbTableColumn.COL_COD_EMPLOYEE);
-				dataInfo.recordMode = stmtResult.getString(EmplisDbTableColumn.COL_RECORD_MODE);
-				dataInfo.codPerson = DaoFormatter.sqlToLong(stmtResult, EmplisDbTableColumn.COL_COD_PERSON);
-				dataInfo.codSnapshot = DaoFormatter.sqlToLong(stmtResult, EmplisDbTableColumn.COL_COD_SNAPSHOT);				
+	@Override protected DaoResultParserV2<EmplisInfo> getResultParserHook() {
+		return new DaoResultParserV2<EmplisInfo>() {
+			@Override public List<EmplisInfo> parseResult(EmplisInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<EmplisInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					EmplisInfo dataInfo = new EmplisInfo();
+					dataInfo.codOwner = stmtResult.getLong(EmplisDbTableColumn.COL_COD_OWNER);
+					dataInfo.codEmployee = stmtResult.getLong(EmplisDbTableColumn.COL_COD_EMPLOYEE);
+					dataInfo.recordMode = stmtResult.getString(EmplisDbTableColumn.COL_RECORD_MODE);
+					dataInfo.codPerson = DaoFormatter.sqlToLong(stmtResult, EmplisDbTableColumn.COL_COD_PERSON);
+					dataInfo.codSnapshot = DaoFormatter.sqlToLong(stmtResult, EmplisDbTableColumn.COL_COD_SNAPSHOT);				
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
