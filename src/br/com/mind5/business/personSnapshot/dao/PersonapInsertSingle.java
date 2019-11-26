fba6868bb7e7572e3e52e2c5ac4e87fd1d6cb9f7
mmaciel <mmaciel@mind5.com>
2019-11-26 08:46:29 -0200

    Personap #02

--- src/br/com/mind5/business/personSnapshot/dao/PersonapInsertSingle.java
+++ src/br/com/mind5/business/personSnapshot/dao/PersonapInsertSingle.java
@@ -10,119 +10,67 @@ import java.util.List;
 import br.com.mind5.business.personSnapshot.info.PersonapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
 import br.com.mind5.dao.DaoStmtParamTranslator;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class PersonapInsertSingle implements DaoStmt<PersonapInfo> {	
-	private DaoStmt<PersonapInfo> stmtSql;
-	private DaoStmtOption_<PersonapInfo> stmtOption;
-	
+public final class PersonapInsertSingle extends DaoStmtTemplate<PersonapInfo> {	
+	private final String MAIN_TABLE = DaoDbTable.PERSON_SNAPSHOT_TABLE;
 	
 	
 	public PersonapInsertSingle(Connection conn, PersonapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
-		
+		super(conn, recordInfo, schemaName);		
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, PersonapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = DaoDbTable.PERSON_SNAPSHOT_TABLE;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(this.stmtOption.tableName);
-		this.stmtOption.stmtParamTranslator = new ParamTranslator();
-		this.stmtOption.resultParser = new ResultParser(recordInfo);
-		this.stmtOption.whereClause = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.INSERT, this.stmtOption, this.getClass());
-	}
-		
-	
-	
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();
-		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<PersonapInfo> getResultset() {
-		return stmtSql.getResultset();
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.INSERT;
 	}
 	
 	
 	
-	private class ParamTranslator implements DaoStmtParamTranslator<PersonapInfo> {		
-		@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, PersonapInfo recordInfo) throws SQLException {			
-			int i = 1;
-			stmt.setLong(i++, recordInfo.codOwner);
-			stmt.setLong(i++, recordInfo.codPerson);
-			stmt.setString(i++, recordInfo.cpf);
-			stmt.setString(i++, recordInfo.name);			
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codGender);
-			stmt = DaoFormatter.localDateToStmt(stmt, i++, recordInfo.birthDate);
-			stmt.setString(i++, recordInfo.email);			
-			stmt.setString(i++, recordInfo.recordMode);			
-			stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
-			stmt.setString(i++, recordInfo.codEntityCateg);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);			
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);	
-			stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);
-			stmt.setString(i++, recordInfo.nameSearch);	
-			
-			return stmt;
-		}		
+	@Override protected DaoStmtParamTranslator<PersonapInfo> getParamTranslatorHook() {
+		return new DaoStmtParamTranslator<PersonapInfo>() {	
+			@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, PersonapInfo recordInfo) throws SQLException {			
+				int i = 1;
+				stmt.setLong(i++, recordInfo.codOwner);
+				stmt.setLong(i++, recordInfo.codPerson);
+				stmt.setString(i++, recordInfo.cpf);
+				stmt.setString(i++, recordInfo.name);			
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codGender);
+				stmt = DaoFormatter.localDateToStmt(stmt, i++, recordInfo.birthDate);
+				stmt.setString(i++, recordInfo.email);			
+				stmt.setString(i++, recordInfo.recordMode);			
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
+				stmt.setString(i++, recordInfo.codEntityCateg);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);			
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);	
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);
+				stmt.setString(i++, recordInfo.nameSearch);	
+				
+				return stmt;
+			}		
+		};
 	}
 	
 	
 	
-	@Override public DaoStmt<PersonapInfo> getNewInstance() {
-		return new PersonapInsertSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
+	@Override protected DaoResultParserV2<PersonapInfo> getResultParserHook() {
+		return new DaoResultParserV2<PersonapInfo>() {	
+			@Override public List<PersonapInfo> parseResult(PersonapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<PersonapInfo> finalResult = new ArrayList<>();
+				recordInfo.codSnapshot = lastId;
+				finalResult.add(recordInfo);			
+				return finalResult;
+			}
+		};
 	}
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<PersonapInfo> {
-		private PersonapInfo recordInfo;
-		
-		public ResultParser(PersonapInfo recordToParse) {
-			recordInfo = recordToParse;
-		}
-		
-		
-		
-		@Override public List<PersonapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<PersonapInfo> finalResult = new ArrayList<>();
-			recordInfo.codSnapshot = lastId;
-			finalResult.add(recordInfo);			
-			return finalResult;
-		}
-	}	
 }
--- src/br/com/mind5/business/personSnapshot/dao/PersonapSelectSingle.java
+++ src/br/com/mind5/business/personSnapshot/dao/PersonapSelectSingle.java
@@ -9,127 +9,77 @@ import java.util.List;
 import br.com.mind5.business.personSnapshot.info.PersonapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class PersonapSelectSingle implements DaoStmt<PersonapInfo> {
-	private final String LT_PERSON = DaoDbTable.PERSON_SNAPSHOT_TABLE;
-	
-	private DaoStmt<PersonapInfo> stmtSql;
-	private DaoStmtOption_<PersonapInfo> stmtOption;
-	
+public final class PersonapSelectSingle extends DaoStmtTemplate<PersonapInfo> {
+	private final String MAIN_TABLE = DaoDbTable.PERSON_SNAPSHOT_TABLE;
 	
 	
 	public PersonapSelectSingle(Connection conn, PersonapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, PersonapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = LT_PERSON;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(LT_PERSON);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, PersonapInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
 		whereOption.ignoreNull = DaoOptionValue.DONT_IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.IGNORE_RECORD_MODE;		
 		
-		DaoStmtWhere whereClause = new PersonapWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new PersonapWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
 	}
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<PersonapInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<PersonapInfo> getNewInstance() {
-		return new PersonapSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<PersonapInfo> {		
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<PersonapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<PersonapInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				PersonapInfo dataInfo = new PersonapInfo();
-				dataInfo.codOwner = stmtResult.getLong(PersonapDbTableColumn.COL_COD_OWNER);
-				dataInfo.codSnapshot = stmtResult.getLong(PersonapDbTableColumn.COL_COD_SNAPSHOT);
-				dataInfo.codPerson = stmtResult.getLong(PersonapDbTableColumn.COL_COD_PERSON);
-				dataInfo.cpf = stmtResult.getString(PersonapDbTableColumn.COL_CPF);
-				dataInfo.name = stmtResult.getString(PersonapDbTableColumn.COL_NAME);	
-				dataInfo.nameSearch = stmtResult.getString(PersonapDbTableColumn.COL_NAME_SEARCH);
-				dataInfo.email = stmtResult.getString(PersonapDbTableColumn.COL_EMAIL);						
-				dataInfo.recordMode = stmtResult.getString(PersonapDbTableColumn.COL_RECORD_MODE);
-				dataInfo.codEntityCateg = stmtResult.getString(PersonapDbTableColumn.COL_COD_ENTITY_CATEG);
-				dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, PersonapDbTableColumn.COL_CREATED_ON);
-				dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, PersonapDbTableColumn.COL_CREATED_BY);
-				dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, PersonapDbTableColumn.COL_LAST_CHANGED_BY);
-				dataInfo.codGender = DaoFormatter.sqlToInt(stmtResult, PersonapDbTableColumn.COL_COD_GENDER);
-				dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, PersonapDbTableColumn.COL_LAST_CHANGED);
-				dataInfo.birthDate = DaoFormatter.sqlToLocalDate(stmtResult, PersonapDbTableColumn.COL_COD_BIRTH_DATE);
+	@Override protected DaoResultParserV2<PersonapInfo> getResultParserHook() {
+		return new DaoResultParserV2<PersonapInfo>() {	
+			@Override public List<PersonapInfo> parseResult(PersonapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<PersonapInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					PersonapInfo dataInfo = new PersonapInfo();
+					dataInfo.codOwner = stmtResult.getLong(PersonapDbTableColumn.COL_COD_OWNER);
+					dataInfo.codSnapshot = stmtResult.getLong(PersonapDbTableColumn.COL_COD_SNAPSHOT);
+					dataInfo.codPerson = stmtResult.getLong(PersonapDbTableColumn.COL_COD_PERSON);
+					dataInfo.cpf = stmtResult.getString(PersonapDbTableColumn.COL_CPF);
+					dataInfo.name = stmtResult.getString(PersonapDbTableColumn.COL_NAME);	
+					dataInfo.nameSearch = stmtResult.getString(PersonapDbTableColumn.COL_NAME_SEARCH);
+					dataInfo.email = stmtResult.getString(PersonapDbTableColumn.COL_EMAIL);						
+					dataInfo.recordMode = stmtResult.getString(PersonapDbTableColumn.COL_RECORD_MODE);
+					dataInfo.codEntityCateg = stmtResult.getString(PersonapDbTableColumn.COL_COD_ENTITY_CATEG);
+					dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, PersonapDbTableColumn.COL_CREATED_ON);
+					dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, PersonapDbTableColumn.COL_CREATED_BY);
+					dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, PersonapDbTableColumn.COL_LAST_CHANGED_BY);
+					dataInfo.codGender = DaoFormatter.sqlToInt(stmtResult, PersonapDbTableColumn.COL_COD_GENDER);
+					dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, PersonapDbTableColumn.COL_LAST_CHANGED);
+					dataInfo.birthDate = DaoFormatter.sqlToLocalDate(stmtResult, PersonapDbTableColumn.COL_COD_BIRTH_DATE);
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
