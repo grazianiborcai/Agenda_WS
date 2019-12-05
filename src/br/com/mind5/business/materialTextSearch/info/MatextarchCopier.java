62a627cf2f1c12215a61837091016f39e7a86a1b
mmaciel <mmaciel@mind5.com>
2019-12-05 20:45:05 -0200

    Matextarch #02

--- src/br/com/mind5/business/materialTextSearch/info/MatextarchCopier.java
+++ src/br/com/mind5/business/materialTextSearch/info/MatextarchCopier.java
@@ -3,6 +3,7 @@ package br.com.mind5.business.materialTextSearch.info;
 import java.util.List;
 
 import br.com.mind5.business.materialText.info.MatextInfo;
+import br.com.mind5.business.materialTextSnapshot.info.MatextsnapInfo;
 import br.com.mind5.info.InfoCopier;
 
 public final class MatextarchCopier {
@@ -16,6 +17,20 @@ public final class MatextarchCopier {
 	
 	public static List<MatextarchInfo> copyFromMatext(List<MatextInfo> sources) {
 		InfoCopier<MatextarchInfo, MatextInfo> copier = new MatextarchCopyMatext();
+		return copier.makeCopy(sources);
+	}	
+	
+	
+	
+	public static MatextarchInfo copyFromMatextsnap(MatextsnapInfo source) {
+		InfoCopier<MatextarchInfo, MatextsnapInfo> copier = new MatextarchCopyMatextsnap();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<MatextarchInfo> copyFromMatextsnap(List<MatextsnapInfo> sources) {
+		InfoCopier<MatextarchInfo, MatextsnapInfo> copier = new MatextarchCopyMatextsnap();
 		return copier.makeCopy(sources);
 	}	
 }
