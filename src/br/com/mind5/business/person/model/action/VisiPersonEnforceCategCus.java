9433a2ba30f6ed704a44bdc5b68887d51b2bf65b
mmaciel <mmaciel@mind5.com>
2019-12-21 01:53:51 -0200

    Person #22

---
+++ src/br/com/mind5/business/person/model/action/VisiPersonEnforceCategCus.java
@@ -0,0 +1,14 @@
+package br.com.mind5.business.person.model.action;
+
+import br.com.mind5.business.person.info.PersonInfo;
+import br.com.mind5.business.person.info.PersonSetterCategCus;
+import br.com.mind5.info.InfoSetter;
+import br.com.mind5.model.action.ActionVisitorTemplateEnforce;
+
+final class VisiPersonEnforceCategCus extends ActionVisitorTemplateEnforce<PersonInfo> {
+
+	@Override protected PersonInfo enforceHook(PersonInfo recordInfo) {
+		InfoSetter<PersonInfo> attrSetter = new PersonSetterCategCus();
+		return attrSetter.setAttr(recordInfo);
+	}
+}
---
+++ src/br/com/mind5/business/person/model/decisionTree/RootPersonInsertCus.java
@@ -0,0 +1,48 @@
+package br.com.mind5.business.person.model.decisionTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.mind5.business.person.info.PersonInfo;
+import br.com.mind5.business.person.model.action.LazyPersonRootInsert;
+import br.com.mind5.business.person.model.action.StdPersonEnforceCategCus;
+import br.com.mind5.business.person.model.checker.PersonCheckDummy;
+import br.com.mind5.model.action.ActionLazy;
+import br.com.mind5.model.action.ActionStd;
+import br.com.mind5.model.checker.ModelChecker;
+import br.com.mind5.model.checker.ModelCheckerQueue;
+import br.com.mind5.model.decisionTree.DeciTreeOption;
+import br.com.mind5.model.decisionTree.DeciTreeWriteTemplate;
+
+public final class RootPersonInsertCus extends DeciTreeWriteTemplate<PersonInfo> {
+	
+	public RootPersonInsertCus(DeciTreeOption<PersonInfo> option) {
+		super(option);
+	}
+	
+	
+	
+	@Override protected ModelChecker<PersonInfo> buildDecisionCheckerHook(DeciTreeOption<PersonInfo> option) {
+		List<ModelChecker<PersonInfo>> queue = new ArrayList<>();		
+		ModelChecker<PersonInfo> checker;	
+		
+		checker = new PersonCheckDummy();
+		queue.add(checker);
+			
+		return new ModelCheckerQueue<>(queue);
+	}
+	
+	
+	
+	@Override protected List<ActionStd<PersonInfo>> buildActionsOnPassedHook(DeciTreeOption<PersonInfo> option) {
+		List<ActionStd<PersonInfo>> actions = new ArrayList<>();
+		
+		ActionStd<PersonInfo> enforceCategCus = new StdPersonEnforceCategCus(option);	
+		ActionLazy<PersonInfo> insert = new LazyPersonRootInsert(option.conn, option.schemaName);
+		
+		enforceCategCus.addPostAction(insert);
+		
+		actions.add(enforceCategCus);
+		return actions;
+	}
+}
