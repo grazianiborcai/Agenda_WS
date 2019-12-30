ed0d270c6af23c0957784979863c94a40ef4a521
mmaciel <mmaciel@mind5.com>
2019-12-30 18:57:44 -0200

    Stolis #08

--- src/br/com/mind5/business/storeList/dao/StolisSelectSingle.java
+++ src/br/com/mind5/business/storeList/dao/StolisSelectSingle.java
@@ -9,121 +9,79 @@ import java.util.List;
 import br.com.mind5.business.storeList.info.StolisInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class StolisSelectSingle implements DaoStmt<StolisInfo> {
-	private final static String LT_STORE = DaoDbTable.STORE_TABLE;	
-	
-	private DaoStmt<StolisInfo> stmtSql;
-	private DaoStmtOption_<StolisInfo> stmtOption;
-	
+public final class StolisSelectSingle extends DaoStmtTemplate<StolisInfo> {
+	private final String MAIN_TABLE = DaoDbTable.STORE_TABLE;	
 	
 	
 	public StolisSelectSingle(Connection conn, StolisInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, StolisInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = LT_STORE;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(DaoDbTable.STORE_LIST_VIEW);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}	
+	
+	
+	
+	@Override protected String getLookupTableHook() {
+		return DaoDbTable.STORE_LIST_VIEW;
+	}	
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, StolisInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
+		
 		whereOption.ignoreNull = DaoOptionValue.IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.DONT_IGNORE_RECORD_MODE;		
 		
-		DaoStmtWhere whereClause = new StolisWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new StolisWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
-	}
+	}	
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<StolisInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<StolisInfo> getNewInstance() {
-		return new StolisSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<StolisInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<StolisInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<StolisInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				StolisInfo dataInfo = new StolisInfo();
-				dataInfo.codOwner = stmtResult.getLong(StolisDbTableColumn.COL_COD_OWNER);
-				dataInfo.codStore = stmtResult.getLong(StolisDbTableColumn.COL_COD_STORE);
-				dataInfo.codCurr = stmtResult.getString(StolisDbTableColumn.COL_COD_CURRENCY);
-				dataInfo.codTimezone = stmtResult.getString(StolisDbTableColumn.COL_COD_TIMEZONE);
-				dataInfo.recordMode = stmtResult.getString(StolisDbTableColumn.COL_RECORD_MODE);	
-				dataInfo.codCompany = DaoFormatter.sqlToLong(stmtResult, StolisDbTableColumn.COL_COD_COMPANY);
-				dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, StolisDbTableColumn.COL_LAST_CHANGED);
-				dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, StolisDbTableColumn.COL_LAST_CHANGED_BY);
-				dataInfo.codSnapshot = DaoFormatter.sqlToLong(stmtResult, StolisDbTableColumn.COL_COD_SNAPSHOT);		
+	@Override protected DaoResultParserV2<StolisInfo> getResultParserHook() {
+		return new DaoResultParserV2<StolisInfo>() {
+			@Override public List<StolisInfo> parseResult(StolisInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<StolisInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					StolisInfo dataInfo = new StolisInfo();
+					
+					dataInfo.codOwner = stmtResult.getLong(StolisDbTableColumn.COL_COD_OWNER);
+					dataInfo.codStore = stmtResult.getLong(StolisDbTableColumn.COL_COD_STORE);
+					dataInfo.codCurr = stmtResult.getString(StolisDbTableColumn.COL_COD_CURRENCY);
+					dataInfo.codTimezone = stmtResult.getString(StolisDbTableColumn.COL_COD_TIMEZONE);
+					dataInfo.recordMode = stmtResult.getString(StolisDbTableColumn.COL_RECORD_MODE);	
+					dataInfo.codCompany = DaoFormatter.sqlToLong(stmtResult, StolisDbTableColumn.COL_COD_COMPANY);
+					dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, StolisDbTableColumn.COL_LAST_CHANGED);
+					dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, StolisDbTableColumn.COL_LAST_CHANGED_BY);
+					dataInfo.codSnapshot = DaoFormatter.sqlToLong(stmtResult, StolisDbTableColumn.COL_COD_SNAPSHOT);		
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
