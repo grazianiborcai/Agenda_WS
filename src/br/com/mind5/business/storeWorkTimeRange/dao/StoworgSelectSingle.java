1068720e6766f2801be2e4dc6eff55adc6e9651e
mmaciel <mmaciel@mind5.com>
2019-12-30 18:55:41 -0200

    Stoworg #02

--- src/br/com/mind5/business/storeWorkTimeRange/dao/StoworgSelectSingle.java
+++ src/br/com/mind5/business/storeWorkTimeRange/dao/StoworgSelectSingle.java
@@ -9,118 +9,76 @@ import java.util.List;
 import br.com.mind5.business.storeWorkTimeRange.info.StoworgInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class StoworgSelectSingle implements DaoStmt<StoworgInfo> {
-	private final String LT_STORE_WORK_TIME = DaoDbTable.STORE_WT_TABLE;
-	
-	private DaoStmt<StoworgInfo> stmtSql;
-	private DaoStmtOption_<StoworgInfo> stmtOption;
-	
+public final class StoworgSelectSingle extends DaoStmtTemplate<StoworgInfo> {
+	private final String MAIN_TABLE = DaoDbTable.STORE_WT_TABLE;
 	
 	
 	public StoworgSelectSingle(Connection conn, StoworgInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, StoworgInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = LT_STORE_WORK_TIME;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(DaoDbTable.STORE_WTIME_RANGE_VIEW);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}	
+	
+	
+	
+	@Override protected String getLookupTableHook() {
+		return DaoDbTable.STORE_WTIME_RANGE_VIEW;
+	}	
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, StoworgInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
+		
 		whereOption.ignoreNull = DaoOptionValue.IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.DONT_IGNORE_RECORD_MODE;		
 		
-		DaoStmtWhere whereClause = new StoworgWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new StoworgWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
 	}
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<StoworgInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<StoworgInfo> getNewInstance() {
-		return new StoworgSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<StoworgInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<StoworgInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<StoworgInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				StoworgInfo dataInfo = new StoworgInfo();
-				dataInfo.codOwner = stmtResult.getLong(StoworgDbTableColumn.COL_COD_OWNER);
-				dataInfo.codStore = stmtResult.getLong(StoworgDbTableColumn.COL_COD_STORE);
-				dataInfo.codWeekday = stmtResult.getInt(StoworgDbTableColumn.COL_COD_WEEKDAY);
-				dataInfo.recordMode = stmtResult.getString(StoworgDbTableColumn.COL_RECORD_MODE);	
-				dataInfo.beginTime = DaoFormatter.sqlToLocalTime(stmtResult, StoworgDbTableColumn.COL_BEGIN_TIME);
-				dataInfo.endTime = DaoFormatter.sqlToLocalTime(stmtResult, StoworgDbTableColumn.COL_END_TIME);
+	@Override protected DaoResultParserV2<StoworgInfo> getResultParserHook() {
+		return new DaoResultParserV2<StoworgInfo>() {
+			@Override public List<StoworgInfo> parseResult(StoworgInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<StoworgInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					StoworgInfo dataInfo = new StoworgInfo();
+					
+					dataInfo.codOwner = stmtResult.getLong(StoworgDbTableColumn.COL_COD_OWNER);
+					dataInfo.codStore = stmtResult.getLong(StoworgDbTableColumn.COL_COD_STORE);
+					dataInfo.codWeekday = stmtResult.getInt(StoworgDbTableColumn.COL_COD_WEEKDAY);
+					dataInfo.recordMode = stmtResult.getString(StoworgDbTableColumn.COL_RECORD_MODE);	
+					dataInfo.beginTime = DaoFormatter.sqlToLocalTime(stmtResult, StoworgDbTableColumn.COL_BEGIN_TIME);
+					dataInfo.endTime = DaoFormatter.sqlToLocalTime(stmtResult, StoworgDbTableColumn.COL_END_TIME);
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
