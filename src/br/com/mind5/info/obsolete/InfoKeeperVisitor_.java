2e43c0a575c54bed11a57155a04cb46e8122a3cf
mmaciel <mmaciel@mind5.com>
2019-12-31 14:22:29 -0200

    Mind5 #08

--- src/br/com/mind5/info/obsolete/InfoKeeperVisitor_.java
+++
@@ -1,7 +0,0 @@
-package br.com.mind5.info.obsolete;
-
-public interface InfoKeeperVisitor_<T,S> {
-	public T keepAtribute(T sourceOne, S sourceTwo);
-	
-	public boolean shouldWrite(T sourceOne, S sourceTwo);
-}
--- src/br/com/mind5/info/obsolete/InfoMergerVisitor_.java
+++
@@ -1,7 +0,0 @@
-package br.com.mind5.info.obsolete;
-//	TODO: remover o S e transforma-lo em T
-public interface InfoMergerVisitor_<T,K,S> {
-	public T writeRecord(K sourceOne, S sourceTwo);	
-	
-	public boolean shouldWrite(K sourceOne, S sourceTwo);
-}
