c7e7ba1d2361d25289c7e2e563b3daa6a672a16e
mmaciel <mmaciel@mind5.com>
2020-01-04 01:40:14 -0200

    Cuslis #04

--- src/br/com/mind5/business/customerList/info/CuslisCopier.java
+++ src/br/com/mind5/business/customerList/info/CuslisCopier.java
@@ -3,12 +3,27 @@ package br.com.mind5.business.customerList.info;
 import java.util.List;
 
 import br.com.mind5.business.addressSnapshot.info.AddresnapInfo;
+import br.com.mind5.business.cart.info.CartInfo;
 import br.com.mind5.business.phoneSnapshot.info.PhonapInfo;
 import br.com.mind5.business.scheduleWeek.info.SchedeekInfo;
 import br.com.mind5.info.InfoCopier;
 import br.com.mind5.info.InfoCopierOneToMany;
 
 public final class CuslisCopier {
+	public static CuslisInfo copyFromCart(CartInfo source) {
+		InfoCopier<CuslisInfo, CartInfo> copier = new CuslisCopyCart();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<CuslisInfo> copyFromCart(List<CartInfo> sources) {
+		InfoCopier<CuslisInfo, CartInfo> copier = new CuslisCopyCart();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
 	public static List<CuslisInfo> copyFromSchedeek(SchedeekInfo source) {
 		InfoCopierOneToMany<CuslisInfo, SchedeekInfo> copier = new CuslisCopySchedeek();
 		return copier.makeCopy(source);
