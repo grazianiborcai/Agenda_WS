a783edbf9a4d52a8bb71f0324cab4d97b113a72e
mmaciel <mmaciel@mind5.com>
2020-01-12 10:28:16 -0200

    Addarch #03

--- src/br/com/mind5/business/addressSearch/info/AddarchCopier.java
+++ src/br/com/mind5/business/addressSearch/info/AddarchCopier.java
@@ -3,10 +3,25 @@ package br.com.mind5.business.addressSearch.info;
 
 import java.util.List;
 
+import br.com.mind5.business.address.info.AddressInfo;
 import br.com.mind5.business.customer.info.CusInfo;
 import br.com.mind5.info.InfoCopier;
 
 public final class AddarchCopier {	
+	public static AddarchInfo copyFromAdddressRef(AddressInfo source) {
+		InfoCopier<AddarchInfo, AddressInfo> copier = new AddarchCopyAddressRef();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<AddarchInfo> copyFromAdddressRef(List<AddressInfo> sources) {
+		InfoCopier<AddarchInfo, AddressInfo> copier = new AddarchCopyAddressRef();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
 	public static AddarchInfo copyFromCusKey(CusInfo source) {
 		InfoCopier<AddarchInfo, CusInfo> copier = new AddarchCopyCusKey();
 		return copier.makeCopy(source);
---
+++ src/br/com/mind5/business/addressSearch/info/AddarchCopyAddressRef.java
@@ -0,0 +1,28 @@
+package br.com.mind5.business.addressSearch.info;
+
+import br.com.mind5.business.address.info.AddressInfo;
+import br.com.mind5.info.InfoCopierTemplate;
+
+final class AddarchCopyAddressRef extends InfoCopierTemplate<AddarchInfo, AddressInfo>{
+	
+	public AddarchCopyAddressRef() {
+		super();
+	}
+	
+	
+	
+	@Override protected AddarchInfo makeCopyHook(AddressInfo source) {
+		AddarchInfo result = new AddarchInfo();
+		
+		result.codOwner = source.codOwner;
+		result.codCustomer = source.codCustomer;
+		result.codStore = source.codStore;
+		result.codEmployee = source.codEmployee;
+		result.codUser = source.codUser;
+		result.codOwnerRef = source.codOwnerRef;
+		result.codLanguage = source.codLanguage;
+		result.username = source.username;
+		
+		return result;
+	}
+}
