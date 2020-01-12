67cd310906b9a39b6c875ebbed02a71c9bb15d10
mmaciel <mmaciel@mind5.com>
2020-01-12 10:39:33 -0200

    Phone #25

--- src/br/com/mind5/business/phone/model/checker/PhoneCheckLimit.java
+++ src/br/com/mind5/business/phone/model/checker/PhoneCheckLimit.java
@@ -1,26 +1,36 @@
 package br.com.mind5.business.phone.model.checker;
 
+import java.util.List;
+
 import br.com.mind5.business.phone.info.PhoneInfo;
-import br.com.mind5.business.phone.model.action.StdPhoneSelect;
+import br.com.mind5.business.phoneSearch.info.PhonarchCopier;
+import br.com.mind5.business.phoneSearch.info.PhonarchInfo;
+import br.com.mind5.business.phoneSearch.model.decisionTree.RootPhonarchSelect;
 import br.com.mind5.common.SystemCode;
 import br.com.mind5.model.action.ActionStd;
 import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerTemplateActionV2;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
-public final class PhoneCheckLimit extends ModelCheckerTemplateActionV2<PhoneInfo, PhoneInfo> {
+public final class PhoneCheckLimit extends ModelCheckerTemplateActionV2<PhoneInfo, PhonarchInfo> {
 	private final int MAX_RECORD_COUNT = 10;
 	
 	
 	public PhoneCheckLimit(ModelCheckerOption option) {
-		super(option, PhoneInfo.class);
+		super(option, PhonarchInfo.class);
 	}
 	
 	
 	
-	@Override protected ActionStd<PhoneInfo> buildActionHook(DeciTreeOption<PhoneInfo> option) {
-		ActionStd<PhoneInfo> select = new StdPhoneSelect(option);
+	@Override protected ActionStd<PhonarchInfo> buildActionHook(DeciTreeOption<PhonarchInfo> option) {
+		ActionStd<PhonarchInfo> select = new RootPhonarchSelect(option).toAction();
 		return select;
+	}
+	
+	
+	
+	@Override protected List<PhonarchInfo> toActionClassHook(List<PhoneInfo> recordInfos) {
+		return PhonarchCopier.copyFromPhoneRef(recordInfos);	
 	}
 	
 	
