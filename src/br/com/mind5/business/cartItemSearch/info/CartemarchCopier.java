d2ecf16069dee17ded2a3ec95a429de180a3830f
mmaciel <mmaciel@mind5.com>
2020-01-12 10:00:07 -0200

    Cartemarch #03

--- src/br/com/mind5/business/cartItemSearch/info/CartemarchCopier.java
+++ src/br/com/mind5/business/cartItemSearch/info/CartemarchCopier.java
@@ -4,6 +4,7 @@ package br.com.mind5.business.cartItemSearch.info;
 import java.util.List;
 
 import br.com.mind5.business.cart.info.CartInfo;
+import br.com.mind5.business.cartItem.info.CartemInfo;
 import br.com.mind5.info.InfoCopierTemplate;
 
 public final class CartemarchCopier {
@@ -16,6 +17,20 @@ public final class CartemarchCopier {
 	
 	public static List<CartemarchInfo> copyFromCartKey(List<CartInfo> sources) {
 		InfoCopierTemplate<CartemarchInfo, CartInfo> copier = new CartemarchCopyCartKey();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
+	public static CartemarchInfo copyFromCartemKey(CartemInfo source) {
+		InfoCopierTemplate<CartemarchInfo, CartemInfo> copier = new CartemarchCopyCartemKey();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<CartemarchInfo> copyFromCartemKey(List<CartemInfo> sources) {
+		InfoCopierTemplate<CartemarchInfo, CartemInfo> copier = new CartemarchCopyCartemKey();
 		return copier.makeCopy(sources);
 	}
 }
---
+++ src/br/com/mind5/business/cartItemSearch/info/CartemarchCopyCartemKey.java
@@ -0,0 +1,24 @@
+package br.com.mind5.business.cartItemSearch.info;
+
+import br.com.mind5.business.cartItem.info.CartemInfo;
+import br.com.mind5.info.InfoCopierTemplate;
+
+final class CartemarchCopyCartemKey extends InfoCopierTemplate<CartemarchInfo, CartemInfo>{
+	
+	public CartemarchCopyCartemKey() {
+		super();
+	}
+	
+	
+	
+	@Override protected CartemarchInfo makeCopyHook(CartemInfo source) {	
+		CartemarchInfo result = new CartemarchInfo();
+		
+		result.codOwner = source.codOwner;
+		result.codUser = source.codUser;
+		result.username	= source.username;
+		result.codLanguage = source.codLanguage;
+		
+		return result;
+	}
+}
