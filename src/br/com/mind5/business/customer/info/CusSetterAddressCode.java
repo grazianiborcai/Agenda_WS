e24590f342a1ce69e9a682e5604f64c3ebc601af
mmaciel <mmaciel@mind5.com>
2020-01-13 13:36:04 -0200

    Cus #35

---
+++ src/br/com/mind5/business/customer/info/CusSetterAddressCode.java
@@ -0,0 +1,43 @@
+package br.com.mind5.business.customer.info;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import br.com.mind5.business.address.info.AddressInfo;
+import br.com.mind5.common.DefaultValue;
+import br.com.mind5.common.SystemMessage;
+import br.com.mind5.info.InfoSetter;
+
+public final class CusSetterAddressCode implements InfoSetter<CusInfo> {
+	
+	public CusInfo setAttr(CusInfo recordInfo) {
+		checkArgument(recordInfo);
+		return setAddressKey(recordInfo);
+	}
+	
+	
+	
+	private void checkArgument(CusInfo recordInfo) {
+		if (recordInfo == null) {
+			logException(new NullPointerException("recordInfo" + SystemMessage.NULL_ARGUMENT));
+			throw new NullPointerException("recordInfo" + SystemMessage.NULL_ARGUMENT);
+		}
+	}
+	
+	
+	
+	private CusInfo setAddressKey(CusInfo recordInfo) {
+		for (AddressInfo eachRecord : recordInfo.addresses) {
+			eachRecord.codAddress = DefaultValue.number();
+		}
+		
+		return recordInfo;
+	}
+	
+	
+	
+	private void logException(Exception e) {
+		Logger logger = LogManager.getLogger(this.getClass());
+		logger.error(e.getMessage(), e);
+	}	
+}
---
+++ src/br/com/mind5/business/customer/model/checker/CusCheckInsertFromUser.java
@@ -0,0 +1,36 @@
+package br.com.mind5.business.customer.model.checker;
+
+import java.sql.Connection;
+
+import br.com.mind5.business.customer.info.CusInfo;
+import br.com.mind5.common.SystemCode;
+import br.com.mind5.model.checker.ModelCheckerOption;
+import br.com.mind5.model.checker.ModelCheckerTemplateSimpleV2;
+
+public final class CusCheckInsertFromUser extends ModelCheckerTemplateSimpleV2<CusInfo> {
+
+	public CusCheckInsertFromUser(ModelCheckerOption option) {
+		super(option);
+	}
+	
+	
+	
+	@Override protected boolean checkHook(CusInfo recordInfo, Connection conn, String schemaName) {	
+		if ( recordInfo.codOwner	<= 0	||
+			 recordInfo.codUser		<= 0	||
+			 recordInfo.personData 	== null ||
+			 recordInfo.username 	== null ||
+			 recordInfo.codLanguage == null 	)
+				
+			return super.FAILED;
+			
+			
+			return super.SUCCESS;
+	}
+	
+	
+	
+	@Override protected int getCodMsgOnResultFalseHook() {
+		return SystemCode.CUS_MANDATORY_FIELD_EMPTY;
+	}
+}
