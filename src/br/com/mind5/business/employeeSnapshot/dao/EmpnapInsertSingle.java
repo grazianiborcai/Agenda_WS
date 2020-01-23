2cec70e374006b3b5d4f0e2f6065012895338ed5
mmaciel <mmaciel@mind5.com>
2020-01-23 19:23:31 -0200

    Empnap #05

--- src/br/com/mind5/business/employeeSnapshot/dao/EmpnapInsertSingle.java
+++ src/br/com/mind5/business/employeeSnapshot/dao/EmpnapInsertSingle.java
@@ -10,117 +10,65 @@ import java.util.List;
 import br.com.mind5.business.employeeSnapshot.info.EmpnapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
 import br.com.mind5.dao.DaoStmtParamTranslator;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class EmpnapInsertSingle implements DaoStmt<EmpnapInfo> {	
-	private DaoStmt<EmpnapInfo> stmtSql;
-	private DaoStmtOption_<EmpnapInfo> stmtOption;
-	
+public final class EmpnapInsertSingle extends DaoStmtTemplate<EmpnapInfo> {	
+	private final String MAIN_TABLE = DaoDbTable.EMP_SNAPSHOT_TABLE;	
 	
 	
 	public EmpnapInsertSingle(Connection conn, EmpnapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
-		
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, EmpnapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = DaoDbTable.EMP_SNAPSHOT_TABLE;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(this.stmtOption.tableName);
-		this.stmtOption.stmtParamTranslator = new ParamTranslator();
-		this.stmtOption.resultParser = new ResultParser(recordInfo);
-		this.stmtOption.whereClause = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.INSERT, this.stmtOption, this.getClass());
-	}
-		
-	
-	
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();
-		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<EmpnapInfo> getResultset() {
-		return stmtSql.getResultset();
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.INSERT;
 	}
 	
 	
 	
-	private class ParamTranslator implements DaoStmtParamTranslator<EmpnapInfo> {		
-		@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, EmpnapInfo recordInfo) throws SQLException {
-			
-			int i = 1;
-			stmt.setLong(i++, recordInfo.codOwner);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);	
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codPerson);
-			stmt.setString(i++, recordInfo.recordMode);
-			stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUser);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codPersonSnapshot);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUserSnapshot);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);
-			stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);
-			
-			return stmt;
-		}		
+	@Override protected DaoStmtParamTranslator<EmpnapInfo> getParamTranslatorHook() {	
+		return new DaoStmtParamTranslator<EmpnapInfo>() {			
+			@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, EmpnapInfo recordInfo) throws SQLException {	
+				int i = 1;
+				
+				stmt.setLong(i++, recordInfo.codOwner);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codPerson);
+				stmt.setString(i++, recordInfo.recordMode);
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUser);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codPersonSnapshot);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUserSnapshot);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);
+				
+				return stmt;
+			}		
+		};
 	}
 	
 	
 	
-	@Override public DaoStmt<EmpnapInfo> getNewInstance() {
-		return new EmpnapInsertSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<EmpnapInfo> {
-		private EmpnapInfo recordInfo;
-		
-		public ResultParser(EmpnapInfo recordToParse) {
-			recordInfo = recordToParse;
-		}
-		
-		
-		
-		@Override public List<EmpnapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<EmpnapInfo> finalResult = new ArrayList<>();
-			recordInfo.codSnapshot = lastId;
-			finalResult.add(recordInfo);			
-			return finalResult;
-		}
+	@Override protected DaoResultParserV2<EmpnapInfo> getResultParserHook() {
+		return new DaoResultParserV2<EmpnapInfo>() {		
+			@Override public List<EmpnapInfo> parseResult(EmpnapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<EmpnapInfo> finalResult = new ArrayList<>();
+				recordInfo.codSnapshot = lastId;
+				finalResult.add(recordInfo);			
+				return finalResult;
+			}
+		};
 	}
 }
--- src/br/com/mind5/business/employeeSnapshot/dao/EmpnapSelectSingle.java
+++ src/br/com/mind5/business/employeeSnapshot/dao/EmpnapSelectSingle.java
@@ -9,124 +9,76 @@ import java.util.List;
 import br.com.mind5.business.employeeSnapshot.info.EmpnapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class EmpnapSelectSingle implements DaoStmt<EmpnapInfo> {
-	private final String LT_EMP = DaoDbTable.EMP_SNAPSHOT_TABLE;	
-	
-	private DaoStmt<EmpnapInfo> stmtSql;
-	private DaoStmtOption_<EmpnapInfo> stmtOption;
-	
+public final class EmpnapSelectSingle extends DaoStmtTemplate<EmpnapInfo> {
+	private final String MAIN_TABLE = DaoDbTable.EMP_SNAPSHOT_TABLE;	
 	
 	
 	public EmpnapSelectSingle(Connection conn, EmpnapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, EmpnapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = LT_EMP;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(LT_EMP);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, EmpnapInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
+		
 		whereOption.ignoreNull = DaoOptionValue.DONT_IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.DONT_IGNORE_RECORD_MODE;		
 		
-		DaoStmtWhere whereClause = new EmpnapWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new EmpnapWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
 	}
-
-	
-		
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<EmpnapInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
 	
 	
 	
-	@Override public DaoStmt<EmpnapInfo> getNewInstance() {
-		return new EmpnapSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<EmpnapInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<EmpnapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<EmpnapInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				EmpnapInfo dataInfo = new EmpnapInfo();
-				dataInfo.codOwner = stmtResult.getLong(EmpnapDbTableColumn.COL_COD_OWNER);
-				dataInfo.codSnapshot = stmtResult.getLong(EmpnapDbTableColumn.COL_COD_SNAPSHOT);
-				dataInfo.codEmployee = stmtResult.getLong(EmpnapDbTableColumn.COL_COD_EMPLOYEE);
-				dataInfo.recordMode = stmtResult.getString(EmpnapDbTableColumn.COL_RECORD_MODE);	
-				dataInfo.codUser = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_USER);
-				dataInfo.codUserSnapshot = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_USER_SNAPSHOT);
-				dataInfo.codPerson = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_PERSON);
-				dataInfo.codPersonSnapshot = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_PERSON_SNAPSHOT);
-				dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, EmpnapDbTableColumn.COL_LAST_CHANGED);
-				dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_LAST_CHANGED_BY);		
-				dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, EmpnapDbTableColumn.COL_CREATED_ON);
-				dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_CREATED_BY);
+	@Override protected DaoResultParserV2<EmpnapInfo> getResultParserHook() {
+		return new DaoResultParserV2<EmpnapInfo>() {
+			@Override public List<EmpnapInfo> parseResult(EmpnapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<EmpnapInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					EmpnapInfo dataInfo = new EmpnapInfo();
+					
+					dataInfo.codOwner = stmtResult.getLong(EmpnapDbTableColumn.COL_COD_OWNER);
+					dataInfo.codSnapshot = stmtResult.getLong(EmpnapDbTableColumn.COL_COD_SNAPSHOT);
+					dataInfo.codEmployee = stmtResult.getLong(EmpnapDbTableColumn.COL_COD_EMPLOYEE);
+					dataInfo.recordMode = stmtResult.getString(EmpnapDbTableColumn.COL_RECORD_MODE);	
+					dataInfo.codUser = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_USER);
+					dataInfo.codUserSnapshot = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_USER_SNAPSHOT);
+					dataInfo.codPerson = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_PERSON);
+					dataInfo.codPersonSnapshot = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_COD_PERSON_SNAPSHOT);
+					dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, EmpnapDbTableColumn.COL_LAST_CHANGED);
+					dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_LAST_CHANGED_BY);		
+					dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, EmpnapDbTableColumn.COL_CREATED_ON);
+					dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, EmpnapDbTableColumn.COL_CREATED_BY);
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
