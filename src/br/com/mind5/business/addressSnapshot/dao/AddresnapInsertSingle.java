b71e04282d8447167628e71e07f8f71d172a6d7f
mmaciel <mmaciel@mind5.com>
2020-01-23 19:16:34 -0200

    Addresnap #09

--- src/br/com/mind5/business/addressSnapshot/dao/AddresnapInsertSingle.java
+++ src/br/com/mind5/business/addressSnapshot/dao/AddresnapInsertSingle.java
@@ -10,138 +10,88 @@ import java.util.List;
 import br.com.mind5.business.addressSnapshot.info.AddresnapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
 import br.com.mind5.dao.DaoStmtParamTranslator;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class AddresnapInsertSingle implements DaoStmt<AddresnapInfo> {
-	private DaoStmt<AddresnapInfo> stmtSql;
-	private DaoStmtOption_<AddresnapInfo> stmtOption;
-	
+public final class AddresnapInsertSingle extends DaoStmtTemplate<AddresnapInfo> {
+	private final String MAIN_TABLE = DaoDbTable.ADDRESS_SNAPSHOT_TABLE;	
 	
 	
 	public AddresnapInsertSingle(Connection conn, AddresnapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();		
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, AddresnapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = DaoDbTable.ADDRESS_SNAPSHOT_TABLE;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(this.stmtOption.tableName);
-		this.stmtOption.stmtParamTranslator = new ParamTranslator();
-		this.stmtOption.resultParser = new ResultParser(recordInfo);
-		this.stmtOption.whereClause = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.INSERT, this.stmtOption, this.getClass());
-	}
-		
-	
-	
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<AddresnapInfo> getResultset() {
-		return stmtSql.getResultset();
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.INSERT;
 	}
 	
 	
 	
-	private class ParamTranslator implements DaoStmtParamTranslator<AddresnapInfo> {		
-		@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, AddresnapInfo recordInfo) throws SQLException {			
-			int i = 1;
-			
-			stmt.setLong(i++, recordInfo.codOwner);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codAddress);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStore);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomer);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);			
-			stmt.setString(i++, recordInfo.codCountry);
-			stmt.setString(i++, recordInfo.codState);
-			stmt.setString(i++, recordInfo.city);
-			stmt.setString(i++, recordInfo.district);
-			stmt.setString(i++, recordInfo.street);
-			stmt.setString(i++, recordInfo.streetNumber);
-			stmt.setString(i++, recordInfo.complement);
-			stmt.setString(i++, recordInfo.postalCode);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.latitude);	
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.longitude);	
-			stmt.setString(i++, recordInfo.line1);
-			stmt.setString(i++, recordInfo.line2);
-			stmt.setString(i++, recordInfo.line3);
-			stmt.setString(i++, recordInfo.line4);
-			stmt.setString(i++, recordInfo.line5);
-			stmt.setString(i++, recordInfo.line6);
-			stmt.setString(i++, recordInfo.line7);
-			stmt.setString(i++, recordInfo.recordMode);
-			stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);	
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUser);	
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRef);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomerSnapshot);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployeeSnapshot);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStoreSnapshot);	
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUserSnapshot);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRefSnapshot);	
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);	
-			stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);	
-
-			return stmt;
-		}		
+	@Override protected DaoStmtParamTranslator<AddresnapInfo> getParamTranslatorHook() {
+		return new DaoStmtParamTranslator<AddresnapInfo>() {		
+			@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, AddresnapInfo recordInfo) throws SQLException {				
+				int i = 1;
+				
+				stmt.setLong(i++, recordInfo.codOwner);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codAddress);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStore);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomer);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);			
+				stmt.setString(i++, recordInfo.codCountry);
+				stmt.setString(i++, recordInfo.codState);
+				stmt.setString(i++, recordInfo.city);
+				stmt.setString(i++, recordInfo.district);
+				stmt.setString(i++, recordInfo.street);
+				stmt.setString(i++, recordInfo.streetNumber);
+				stmt.setString(i++, recordInfo.complement);
+				stmt.setString(i++, recordInfo.postalCode);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.latitude);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.longitude);	
+				stmt.setString(i++, recordInfo.line1);
+				stmt.setString(i++, recordInfo.line2);
+				stmt.setString(i++, recordInfo.line3);
+				stmt.setString(i++, recordInfo.line4);
+				stmt.setString(i++, recordInfo.line5);
+				stmt.setString(i++, recordInfo.line6);
+				stmt.setString(i++, recordInfo.line7);
+				stmt.setString(i++, recordInfo.recordMode);
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUser);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRef);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomerSnapshot);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployeeSnapshot);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStoreSnapshot);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUserSnapshot);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRefSnapshot);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);	
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);	
+	
+				return stmt;
+			}		
+		};
 	}
 	
 	
 	
-	@Override public DaoStmt<AddresnapInfo> getNewInstance() {
-		return new AddresnapInsertSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<AddresnapInfo> {
-		private AddresnapInfo recordInfo;
-		
-		public ResultParser(AddresnapInfo recordToParse) {
-			recordInfo = recordToParse;
-		}
-		
-		
-		
-		@Override public List<AddresnapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<AddresnapInfo> finalResult = new ArrayList<>();
-			recordInfo.codSnapshot = lastId;
-			finalResult.add(recordInfo);			
-			return finalResult;
-		}
+	@Override protected DaoResultParserV2<AddresnapInfo> getResultParserHook() {
+		return new DaoResultParserV2<AddresnapInfo>() {		
+			@Override public List<AddresnapInfo> parseResult(AddresnapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<AddresnapInfo> finalResult = new ArrayList<>();
+				recordInfo.codSnapshot = lastId;
+				finalResult.add(recordInfo);			
+				return finalResult;
+			}
+		};
 	}
 }
--- src/br/com/mind5/business/addressSnapshot/dao/AddresnapSelectSingle.java
+++ src/br/com/mind5/business/addressSnapshot/dao/AddresnapSelectSingle.java
@@ -9,151 +9,99 @@ import java.util.List;
 import br.com.mind5.business.addressSnapshot.info.AddresnapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class AddresnapSelectSingle implements DaoStmt<AddresnapInfo> {
+public final class AddresnapSelectSingle extends DaoStmtTemplate<AddresnapInfo> {
 	private final String MAIN_TABLE = DaoDbTable.ADDRESS_SNAPSHOT_TABLE;	
-	
-	private DaoStmt<AddresnapInfo> stmtSql;
-	private DaoStmtOption_<AddresnapInfo> stmtOption;
-	
 	
 	
 	public AddresnapSelectSingle(Connection conn, AddresnapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, AddresnapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = MAIN_TABLE;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(MAIN_TABLE);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, AddresnapInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
+		
 		whereOption.ignoreNull = DaoOptionValue.DONT_IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.IGNORE_RECORD_MODE;
 		
-		DaoStmtWhere whereClause = new AddresnapWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new AddresnapWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
-	}
+	}	
 	
 	
 	
-	
-	
-	
-	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<AddresnapInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<AddresnapInfo> getNewInstance() {
-		return new AddresnapSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<AddresnapInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<AddresnapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<AddresnapInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				AddresnapInfo dataInfo = new AddresnapInfo();
-				dataInfo.codSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_SNAPSHOT);	
-				dataInfo.codAddress = stmtResult.getLong(AddresnapDbTableColumn.COL_COD_ADDRESS);
-				dataInfo.codOwner = stmtResult.getLong(AddresnapDbTableColumn.COL_COD_OWNER);
-				dataInfo.codCountry = stmtResult.getString(AddresnapDbTableColumn.COL_COUNTRY);
-				dataInfo.codState = stmtResult.getString(AddresnapDbTableColumn.COL_STATE_PROVINCE);
-				dataInfo.city = stmtResult.getString(AddresnapDbTableColumn.COL_CITY);
-				dataInfo.district = stmtResult.getString(AddresnapDbTableColumn.COL_DISTRICT);
-				dataInfo.street = stmtResult.getString(AddresnapDbTableColumn.COL_STREET);
-				dataInfo.streetNumber = stmtResult.getString(AddresnapDbTableColumn.COL_STREET_NUMBER);
-				dataInfo.complement = stmtResult.getString(AddresnapDbTableColumn.COL_COMPLEMENT);
-				dataInfo.postalCode = stmtResult.getString(AddresnapDbTableColumn.COL_POSTALCODE);
-				dataInfo.line1 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE1);
-				dataInfo.line2 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE2);
-				dataInfo.line3 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE3);
-				dataInfo.line4 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE4);
-				dataInfo.line5 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE5);
-				dataInfo.line6 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE6);
-				dataInfo.line7 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE7);
-				dataInfo.recordMode = stmtResult.getString(AddresnapDbTableColumn.COL_RECORD_MODE);
-				dataInfo.codStore = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_STORE);
-				dataInfo.codStoreSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_STORE_SNAPSHOT);
-				dataInfo.codCustomer = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_CUSTOMER);
-				dataInfo.codCustomerSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_CUSTOMER_SNAPSHOT);
-				dataInfo.codEmployee = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_EMPLOYEE);
-				dataInfo.codEmployeeSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_EMPLOYEE_SNAPSHOT);
-				dataInfo.codUser = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_USER);
-				dataInfo.codUserSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_USER_SNAPSHOT);
-				dataInfo.codOwnerRef = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_OWNER_REF);
-				dataInfo.codOwnerRefSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_OWNER_REF_SNAPSHOT);
-				dataInfo.latitude = DaoFormatter.sqlToFloat(stmtResult, AddresnapDbTableColumn.COL_LATITUDE);
-				dataInfo.longitude = DaoFormatter.sqlToFloat(stmtResult, AddresnapDbTableColumn.COL_LONGITUDE);
-				dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, AddresnapDbTableColumn.COL_LAST_CHANGED);
-				dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_LAST_CHANGED_BY);		
-				dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, AddresnapDbTableColumn.COL_CREATED_ON);
-				dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_CREATED_BY);
+	@Override protected DaoResultParserV2<AddresnapInfo> getResultParserHook() {
+		return new DaoResultParserV2<AddresnapInfo>() {
+			@Override public List<AddresnapInfo> parseResult(AddresnapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<AddresnapInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					AddresnapInfo dataInfo = new AddresnapInfo();
+					
+					dataInfo.codSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_SNAPSHOT);	
+					dataInfo.codAddress = stmtResult.getLong(AddresnapDbTableColumn.COL_COD_ADDRESS);
+					dataInfo.codOwner = stmtResult.getLong(AddresnapDbTableColumn.COL_COD_OWNER);
+					dataInfo.codCountry = stmtResult.getString(AddresnapDbTableColumn.COL_COUNTRY);
+					dataInfo.codState = stmtResult.getString(AddresnapDbTableColumn.COL_STATE_PROVINCE);
+					dataInfo.city = stmtResult.getString(AddresnapDbTableColumn.COL_CITY);
+					dataInfo.district = stmtResult.getString(AddresnapDbTableColumn.COL_DISTRICT);
+					dataInfo.street = stmtResult.getString(AddresnapDbTableColumn.COL_STREET);
+					dataInfo.streetNumber = stmtResult.getString(AddresnapDbTableColumn.COL_STREET_NUMBER);
+					dataInfo.complement = stmtResult.getString(AddresnapDbTableColumn.COL_COMPLEMENT);
+					dataInfo.postalCode = stmtResult.getString(AddresnapDbTableColumn.COL_POSTALCODE);
+					dataInfo.line1 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE1);
+					dataInfo.line2 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE2);
+					dataInfo.line3 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE3);
+					dataInfo.line4 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE4);
+					dataInfo.line5 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE5);
+					dataInfo.line6 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE6);
+					dataInfo.line7 = stmtResult.getString(AddresnapDbTableColumn.COL_LINE7);
+					dataInfo.recordMode = stmtResult.getString(AddresnapDbTableColumn.COL_RECORD_MODE);
+					dataInfo.codStore = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_STORE);
+					dataInfo.codStoreSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_STORE_SNAPSHOT);
+					dataInfo.codCustomer = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_CUSTOMER);
+					dataInfo.codCustomerSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_CUSTOMER_SNAPSHOT);
+					dataInfo.codEmployee = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_EMPLOYEE);
+					dataInfo.codEmployeeSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_EMPLOYEE_SNAPSHOT);
+					dataInfo.codUser = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_USER);
+					dataInfo.codUserSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_USER_SNAPSHOT);
+					dataInfo.codOwnerRef = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_OWNER_REF);
+					dataInfo.codOwnerRefSnapshot = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_COD_OWNER_REF_SNAPSHOT);
+					dataInfo.latitude = DaoFormatter.sqlToFloat(stmtResult, AddresnapDbTableColumn.COL_LATITUDE);
+					dataInfo.longitude = DaoFormatter.sqlToFloat(stmtResult, AddresnapDbTableColumn.COL_LONGITUDE);
+					dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, AddresnapDbTableColumn.COL_LAST_CHANGED);
+					dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_LAST_CHANGED_BY);		
+					dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, AddresnapDbTableColumn.COL_CREATED_ON);
+					dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, AddresnapDbTableColumn.COL_CREATED_BY);
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
