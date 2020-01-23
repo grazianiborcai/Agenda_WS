5ae76d727bb05278aa77bd69478d0790f3418f73
mmaciel <mmaciel@mind5.com>
2020-01-23 19:01:18 -0200

    Schedyerat #03

--- src/br/com/mind5/business/scheduleYearData/dao/SchedyeratSelectSingle.java
+++ src/br/com/mind5/business/scheduleYearData/dao/SchedyeratSelectSingle.java
@@ -9,117 +9,71 @@ import java.util.List;
 import br.com.mind5.business.scheduleYearData.info.SchedyeratInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public class SchedyeratSelectSingle implements DaoStmt<SchedyeratInfo> {
-	private final String LT_SCHEDULE = DaoDbTable.SCHEDULE_YEAR_TABLE;
-	
-	private DaoStmt<SchedyeratInfo> stmtSql;
-	private DaoStmtOption_<SchedyeratInfo> stmtOption;
-	
+public class SchedyeratSelectSingle extends DaoStmtTemplate<SchedyeratInfo> {
+	private final String MAIN_TABLE = DaoDbTable.SCHEDULE_YEAR_TABLE;
 	
 	
 	public SchedyeratSelectSingle(Connection conn, SchedyeratInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();		
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, SchedyeratInfo recordInfo, String schemaName) {
-		stmtOption = new DaoStmtOption_<>();
-		stmtOption.conn = conn;
-		stmtOption.recordInfo = recordInfo;
-		stmtOption.schemaName = schemaName;
-		stmtOption.tableName = LT_SCHEDULE;
-		stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(stmtOption.tableName);
-		stmtOption.stmtParamTranslator = null;
-		stmtOption.resultParser = new ResultParser();
-		stmtOption.whereClause = buildWhereClause();
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, SchedyeratInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
-		whereOption.ignoreNull = DaoOptionValue.IGNORE_NULL;
+		
+		whereOption.ignoreNull = DaoOptionValue.DONT_IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.IGNORE_RECORD_MODE;		
 		
-		DaoStmtWhere whereClause = new SchedyeratWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new SchedyeratWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
 	}
 	
 	
-	
-	private void buildStmt() {
-		stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, stmtOption, this.getClass());
-	}
 		
-	
-	
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<SchedyeratInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<SchedyeratInfo> getNewInstance() {
-		return new SchedyeratSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<SchedyeratInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<SchedyeratInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<SchedyeratInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
-				return finalResult;
-			
-			do {
-				SchedyeratInfo dataInfo = new SchedyeratInfo();
-				dataInfo.codOwner = DaoFormatter.sqlToLong(stmtResult, SchedyeratDbTableColumn.COL_COD_OWNER);
-				dataInfo.codStore = DaoFormatter.sqlToLong(stmtResult, SchedyeratDbTableColumn.COL_COD_STORE);
-				dataInfo.month = DaoFormatter.sqlToInt(stmtResult, SchedyeratDbTableColumn.COL_MONTH);
-				dataInfo.year = DaoFormatter.sqlToInt(stmtResult, SchedyeratDbTableColumn.COL_YEAR);
-				dataInfo.confirmed = stmtResult.getInt(SchedyeratDbTableColumn.COL_CONFIRMED);
-				dataInfo.waiting = stmtResult.getInt(SchedyeratDbTableColumn.COL_WAITING);
-				dataInfo.counter = stmtResult.getInt(SchedyeratDbTableColumn.COL_COUNTER);
+	@Override protected DaoResultParserV2<SchedyeratInfo> getResultParserHook() {
+		return new DaoResultParserV2<SchedyeratInfo>() {
+			@Override public List<SchedyeratInfo> parseResult(SchedyeratInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<SchedyeratInfo> finalResult = new ArrayList<>();
 				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					SchedyeratInfo dataInfo = new SchedyeratInfo();
+					
+					dataInfo.codOwner = DaoFormatter.sqlToLong(stmtResult, SchedyeratDbTableColumn.COL_COD_OWNER);
+					dataInfo.codStore = DaoFormatter.sqlToLong(stmtResult, SchedyeratDbTableColumn.COL_COD_STORE);
+					dataInfo.month = DaoFormatter.sqlToInt(stmtResult, SchedyeratDbTableColumn.COL_MONTH);
+					dataInfo.year = DaoFormatter.sqlToInt(stmtResult, SchedyeratDbTableColumn.COL_YEAR);
+					dataInfo.confirmed = stmtResult.getInt(SchedyeratDbTableColumn.COL_CONFIRMED);
+					dataInfo.waiting = stmtResult.getInt(SchedyeratDbTableColumn.COL_WAITING);
+					dataInfo.counter = stmtResult.getInt(SchedyeratDbTableColumn.COL_COUNTER);
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
+				return finalResult;
+			}
+		};
 	}
 }
