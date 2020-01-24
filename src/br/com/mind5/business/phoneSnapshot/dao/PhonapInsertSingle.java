d16de0c75ea295c85a0f745d7b0f1412c8fb28f5
mmaciel <mmaciel@mind5.com>
2020-01-24 12:42:27 -0200

    Phonap #03

--- src/br/com/mind5/business/phoneSnapshot/dao/PhonapInsertSingle.java
+++ src/br/com/mind5/business/phoneSnapshot/dao/PhonapInsertSingle.java
@@ -10,126 +10,76 @@ import java.util.List;
 import br.com.mind5.business.phoneSnapshot.info.PhonapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
 import br.com.mind5.dao.DaoStmtParamTranslator;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class PhonapInsertSingle implements DaoStmt<PhonapInfo> {
-	private DaoStmt<PhonapInfo> stmtSql;
-	private DaoStmtOption_<PhonapInfo> stmtOption;
-	
+public final class PhonapInsertSingle extends DaoStmtTemplate<PhonapInfo> {
+	private final String MAIN_TABLE = DaoDbTable.PHONE_SNAPSHOT_TABLE;
 	
 	
 	public PhonapInsertSingle(Connection conn, PhonapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();		
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, PhonapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = DaoDbTable.PHONE_SNAPSHOT_TABLE;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(this.stmtOption.tableName);
-		this.stmtOption.stmtParamTranslator = new ParamTranslator();
-		this.stmtOption.resultParser = new ResultParser(recordInfo);
-		this.stmtOption.whereClause = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.INSERT, this.stmtOption, this.getClass());
-	}
-		
-	
-	
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<PhonapInfo> getResultset() {
-		return stmtSql.getResultset();
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.INSERT;
 	}
 	
 	
 	
-	private class ParamTranslator implements DaoStmtParamTranslator<PhonapInfo> {		
-		@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, PhonapInfo recordInfo) throws SQLException {
-			
-			int i = 1;
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwner);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codPhone);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStore);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomer);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCountryPhone);
-			stmt.setString(i++, recordInfo.fullNumber);
-			stmt.setString(i++, recordInfo.recordMode);
-			DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
-			stmt.setString(i++, recordInfo.complement);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUser);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRef);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomerSnapshot);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployeeSnapshot);	
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStoreSnapshot);	
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUserSnapshot);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRefSnapshot);
-			stmt.setString(i++, recordInfo.number);
-			stmt.setString(i++, recordInfo.codArea);
-			DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);	
-			DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);				
-
-			return stmt;
-		}		
-	}
+	@Override protected DaoStmtParamTranslator<PhonapInfo> getParamTranslatorHook() {
+		return new DaoStmtParamTranslator<PhonapInfo>() {			
+			@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, PhonapInfo recordInfo) throws SQLException {				
+				int i = 1;
+				
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwner);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codPhone);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStore);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomer);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCountryPhone);
+				stmt.setString(i++, recordInfo.fullNumber);
+				stmt.setString(i++, recordInfo.recordMode);
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
+				stmt.setString(i++, recordInfo.complement);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUser);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRef);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.lastChangedBy);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codCustomerSnapshot);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployeeSnapshot);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStoreSnapshot);	
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codUserSnapshot);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOwnerRefSnapshot);
+				stmt.setString(i++, recordInfo.number);
+				stmt.setString(i++, recordInfo.codArea);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.createdBy);	
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.createdOn);				
+	
+				return stmt;
+			}		
+		};
+	}	
 	
 	
 	
-	@Override public DaoStmt<PhonapInfo> getNewInstance() {
-		return new PhonapInsertSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<PhonapInfo> {
-		private PhonapInfo recordInfo;
-		
-		public ResultParser(PhonapInfo recordToParse) {
-			recordInfo = recordToParse;
-		}
-		
-		
-		
-		@Override public List<PhonapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<PhonapInfo> finalResult = new ArrayList<>();
-			recordInfo.codSnapshot = lastId;
-			finalResult.add(recordInfo);			
-			return finalResult;
-		}
+	@Override protected DaoResultParserV2<PhonapInfo> getResultParserHook() {
+		return new DaoResultParserV2<PhonapInfo>() {		
+			@Override public List<PhonapInfo> parseResult(PhonapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<PhonapInfo> finalResult = new ArrayList<>();
+				recordInfo.codSnapshot = lastId;
+				finalResult.add(recordInfo);			
+				return finalResult;
+			}
+		};
 	}
 }
--- src/br/com/mind5/business/phoneSnapshot/dao/PhonapSelectSingle.java
+++ src/br/com/mind5/business/phoneSnapshot/dao/PhonapSelectSingle.java
@@ -10,136 +10,87 @@ import br.com.mind5.business.phone.dao.PhoneDbTableColumn;
 import br.com.mind5.business.phoneSnapshot.info.PhonapInfo;
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
+import br.com.mind5.dao.DaoResultParserV2;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoResultParser_;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 
-public final class PhonapSelectSingle implements DaoStmt<PhonapInfo> {
+public final class PhonapSelectSingle extends DaoStmtTemplate<PhonapInfo> {
 	private final String MAIN_TABLE = DaoDbTable.PHONE_SNAPSHOT_TABLE;	
-	
-	private DaoStmt<PhonapInfo> stmtSql;
-	private DaoStmtOption_<PhonapInfo> stmtOption;
-	
 	
 	
 	public PhonapSelectSingle(Connection conn, PhonapInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, PhonapInfo recordInfo, String schemaName) {
-		this.stmtOption = new DaoStmtOption_<>();
-		this.stmtOption.conn = conn;
-		this.stmtOption.recordInfo = recordInfo;
-		this.stmtOption.schemaName = schemaName;
-		this.stmtOption.tableName = MAIN_TABLE;
-		this.stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(MAIN_TABLE);
-		this.stmtOption.stmtParamTranslator = null;
-		this.stmtOption.resultParser = new ResultParser();
-		this.stmtOption.whereClause = buildWhereClause();
-		this.stmtOption.joins = null;
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {		
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.SELECT;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, PhonapInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
+		
 		whereOption.ignoreNull = DaoOptionValue.DONT_IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.DONT_IGNORE_RECORD_MODE;
 		
-		DaoStmtWhere whereClause = new PhonapWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new PhonapWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
-	}
+	}	
 	
 	
 	
-	private void buildStmt() {
-		this.stmtSql = new DaoStmtHelper_<>(DaoOperation.SELECT, this.stmtOption, this.getClass());
-	}
-	
-	
-
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<PhonapInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	@Override public DaoStmt<PhonapInfo> getNewInstance() {
-		return new PhonapSelectSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
-	}
-	
-	
-	
-	
-	
-	
-	private static class ResultParser implements DaoResultParser_<PhonapInfo> {
-		private final boolean EMPTY_RESULT_SET = false;
-		
-		@Override public List<PhonapInfo> parseResult(ResultSet stmtResult, long lastId) throws SQLException {
-			List<PhonapInfo> finalResult = new ArrayList<>();
-			
-			if (stmtResult.next() == EMPTY_RESULT_SET)				
+	@Override protected DaoResultParserV2<PhonapInfo> getResultParserHook() {
+		return new DaoResultParserV2<PhonapInfo>() {
+			@Override public List<PhonapInfo> parseResult(PhonapInfo recordInfo, ResultSet stmtResult, long lastId) throws SQLException {
+				List<PhonapInfo> finalResult = new ArrayList<>();
+				
+				if (stmtResult.next() == false)				
+					return finalResult;
+				
+				do {
+					PhonapInfo dataInfo = new PhonapInfo();
+					
+					dataInfo.codPhone = stmtResult.getLong(PhonapDbTableColumn.COL_COD_PHONE);
+					dataInfo.codSnapshot = stmtResult.getLong(PhonapDbTableColumn.COL_COD_SNAPSHOT);
+					dataInfo.codOwner = stmtResult.getLong(PhonapDbTableColumn.COL_COD_OWNER);
+					dataInfo.codCountryPhone = stmtResult.getInt(PhonapDbTableColumn.COL_COUNTRY_PHONE);
+					dataInfo.fullNumber = stmtResult.getString(PhonapDbTableColumn.COL_FULL_NUMBER);
+					dataInfo.recordMode = stmtResult.getString(PhonapDbTableColumn.COL_RECORD_MODE);
+					dataInfo.complement = stmtResult.getString(PhonapDbTableColumn.COL_COMPLEMENT);
+					dataInfo.number = stmtResult.getString(PhonapDbTableColumn.COL_NUMBER);
+					dataInfo.codArea = stmtResult.getString(PhonapDbTableColumn.COL_COD_AREA);
+					dataInfo.codStore = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_STORE);
+					dataInfo.codStoreSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_STORE_SNAPSHOT);
+					dataInfo.codCustomer = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_CUSTOMER);
+					dataInfo.codCustomerSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_CUSTOMER_SNAPSHOT);
+					dataInfo.codEmployee = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_EMPLOYEE);
+					dataInfo.codEmployeeSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_EMPLOYEE_SNAPSHOT);
+					dataInfo.codUser = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_USER);
+					dataInfo.codUserSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_USER_SNAPSHOT);
+					dataInfo.codOwnerRef = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_OWNER_REF);
+					dataInfo.codOwnerRefSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_OWNER_REF_SNAPSHOT);
+					dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_LAST_CHANGED_BY);
+					dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, PhonapDbTableColumn.COL_LAST_CHANGED);
+					dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, PhoneDbTableColumn.COL_CREATED_ON);
+					dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, PhoneDbTableColumn.COL_CREATED_BY);					
+					
+					finalResult.add(dataInfo);
+				} while (stmtResult.next());
+				
 				return finalResult;
-			
-			do {
-				PhonapInfo dataInfo = new PhonapInfo();
-				dataInfo.codPhone = stmtResult.getLong(PhonapDbTableColumn.COL_COD_PHONE);
-				dataInfo.codSnapshot = stmtResult.getLong(PhonapDbTableColumn.COL_COD_SNAPSHOT);
-				dataInfo.codOwner = stmtResult.getLong(PhonapDbTableColumn.COL_COD_OWNER);
-				dataInfo.codCountryPhone = stmtResult.getInt(PhonapDbTableColumn.COL_COUNTRY_PHONE);
-				dataInfo.fullNumber = stmtResult.getString(PhonapDbTableColumn.COL_FULL_NUMBER);
-				dataInfo.recordMode = stmtResult.getString(PhonapDbTableColumn.COL_RECORD_MODE);
-				dataInfo.complement = stmtResult.getString(PhonapDbTableColumn.COL_COMPLEMENT);
-				dataInfo.number = stmtResult.getString(PhonapDbTableColumn.COL_NUMBER);
-				dataInfo.codArea = stmtResult.getString(PhonapDbTableColumn.COL_COD_AREA);
-				dataInfo.codStore = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_STORE);
-				dataInfo.codStoreSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_STORE_SNAPSHOT);
-				dataInfo.codCustomer = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_CUSTOMER);
-				dataInfo.codCustomerSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_CUSTOMER_SNAPSHOT);
-				dataInfo.codEmployee = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_EMPLOYEE);
-				dataInfo.codEmployeeSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_EMPLOYEE_SNAPSHOT);
-				dataInfo.codUser = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_USER);
-				dataInfo.codUserSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_USER_SNAPSHOT);
-				dataInfo.codOwnerRef = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_OWNER_REF);
-				dataInfo.codOwnerRefSnapshot = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_COD_OWNER_REF_SNAPSHOT);
-				dataInfo.lastChangedBy = DaoFormatter.sqlToLong(stmtResult, PhonapDbTableColumn.COL_LAST_CHANGED_BY);
-				dataInfo.lastChanged = DaoFormatter.sqlToLocalDateTime(stmtResult, PhonapDbTableColumn.COL_LAST_CHANGED);
-				dataInfo.createdOn = DaoFormatter.sqlToLocalDateTime(stmtResult, PhoneDbTableColumn.COL_CREATED_ON);
-				dataInfo.createdBy = DaoFormatter.sqlToLong(stmtResult, PhoneDbTableColumn.COL_CREATED_BY);
-				
-				
-				finalResult.add(dataInfo);
-			} while (stmtResult.next());
-			
-			return finalResult;
-		}
+			}
+		};
 	}
 }
