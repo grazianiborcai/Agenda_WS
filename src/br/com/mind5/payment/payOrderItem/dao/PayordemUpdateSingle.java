ccb1d0b8bf8482a997f060c115e156ffbb958745
mmaciel <mmaciel@mind5.com>
2020-02-03 17:02:14 -0200

    Payordem #04

--- src/br/com/mind5/payment/payOrderItem/dao/PayordemUpdateSingle.java
+++ src/br/com/mind5/payment/payOrderItem/dao/PayordemUpdateSingle.java
@@ -3,124 +3,81 @@ package br.com.mind5.payment.payOrderItem.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
-
 import br.com.mind5.dao.DaoFormatter;
 import br.com.mind5.dao.DaoOperation;
-import br.com.mind5.dao.DaoStmt;
-import br.com.mind5.dao.DaoStmtHelper_;
 import br.com.mind5.dao.DaoStmtParamTranslator;
+import br.com.mind5.dao.DaoStmtTemplate;
 import br.com.mind5.dao.DaoStmtWhere;
 import br.com.mind5.dao.DaoWhereBuilderOption;
 import br.com.mind5.dao.common.DaoDbTable;
-import br.com.mind5.dao.common.DaoDbTableColumnAll;
 import br.com.mind5.dao.common.DaoOptionValue;
-import br.com.mind5.dao.obsolete.DaoStmtOption_;
 import br.com.mind5.payment.payOrderItem.info.PayordemInfo;
 
-public class PayordemUpdateSingle implements DaoStmt<PayordemInfo> {
-	private DaoStmt<PayordemInfo> stmtSql;
-	private DaoStmtOption_<PayordemInfo> stmtOption;
-	
+public class PayordemUpdateSingle extends DaoStmtTemplate<PayordemInfo> {
+	private final String MAIN_TABLE = DaoDbTable.PAY_ORDER_ITM_TABLE;	
 	
 	
 	public PayordemUpdateSingle(Connection conn, PayordemInfo recordInfo, String schemaName) {
-		buildStmtOption(conn, recordInfo, schemaName);
-		buildStmt();		
+		super(conn, recordInfo, schemaName);
 	}
 	
 	
 	
-	private void buildStmtOption(Connection conn, PayordemInfo recordInfo, String schemaName) {
-		stmtOption = new DaoStmtOption_<>();
-		stmtOption.conn = conn;
-		stmtOption.recordInfo = recordInfo;
-		stmtOption.schemaName = schemaName;
-		stmtOption.tableName = DaoDbTable.PAY_ORDER_ITM_TABLE;
-		stmtOption.columns = DaoDbTableColumnAll.getTableColumnsAsList(stmtOption.tableName);
-		stmtOption.stmtParamTranslator = new ParamTranslator();
-		stmtOption.resultParser = null;
-		stmtOption.whereClause = buildWhereClause();
+	@Override protected String getTableNameHook() {
+		return MAIN_TABLE;
 	}
 	
 	
 	
-	private String buildWhereClause() {
+	@Override protected DaoOperation getOperationHook() {
+		return DaoOperation.UPDATE;
+	}
+	
+	
+	
+	@Override protected String buildWhereClauseHook(String tableName, PayordemInfo recordInfo) {
 		DaoWhereBuilderOption whereOption = new DaoWhereBuilderOption();
+		
 		whereOption.ignoreNull = DaoOptionValue.DONT_IGNORE_NULL;
 		whereOption.ignoreRecordMode = DaoOptionValue.IGNORE_RECORD_MODE;
 		whereOption.ignoreNonPrimaryKey = DaoOptionValue.IGNORE_NON_PK;
 		
-		DaoStmtWhere whereClause = new PayordemWhere(whereOption, stmtOption.tableName, stmtOption.recordInfo);
+		DaoStmtWhere whereClause = new PayordemWhere(whereOption, tableName, recordInfo);
 		return whereClause.getWhereClause();
 	}	
 	
 	
 	
-	private void buildStmt() {
-		stmtSql = new DaoStmtHelper_<>(DaoOperation.UPDATE, stmtOption, this.getClass());
-	}
-		
-	
-	
-	@Override public void generateStmt() throws SQLException {
-		stmtSql.generateStmt();		
-	}
-
-	
-	
-	@Override public boolean checkStmtGeneration() {
-		return stmtSql.checkStmtGeneration();
-	}
-
-	
-	
-	@Override public void executeStmt() throws SQLException {
-		stmtSql.executeStmt();
-	}
-
-	
-	
-	@Override public List<PayordemInfo> getResultset() {
-		return stmtSql.getResultset();
-	}
-	
-	
-	
-	private class ParamTranslator implements DaoStmtParamTranslator<PayordemInfo> {		
-		@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, PayordemInfo recordInfo) throws SQLException {			
-			
-			int i = 1;
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStore);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codMat);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);
-			stmt = DaoFormatter.localDateToStmt(stmt, i++, recordInfo.date);
-			stmt = DaoFormatter.localTimeToStmt(stmt, i++, recordInfo.beginTime);
-			stmt.setInt(i++, recordInfo.quantity);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.price);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.totitem);
-			stmt.setString(i++, recordInfo.codCurr);
-			stmt.setString(i++, recordInfo.itemReceiver);
-			stmt.setString(i++, recordInfo.ownId);
-			stmt.setString(i++, recordInfo.idOrderPartner);
-			stmt.setString(i++, recordInfo.statusOrderPartner);
-			stmt = DaoFormatter.charToStmt(stmt, i++, recordInfo.codFeeCateg);
-			stmt.setString(i++, recordInfo.idPaymentPartner);
-			stmt.setString(i++, recordInfo.statusPaymentPartner);
-			stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
-			stmt.setBoolean(i++, recordInfo.isSystemReceiver);
-			stmt.setString(i++, recordInfo.idRefundPartner);
-			stmt.setString(i++, recordInfo.statusRefundPartner);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOrder);
-			stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOrderItem);
-			
-			return stmt;
-		}		
-	}
-	
-	
-	
-	@Override public DaoStmt<PayordemInfo> getNewInstance() {
-		return new PayordemUpdateSingle(stmtOption.conn, stmtOption.recordInfo, stmtOption.schemaName);
+	@Override protected DaoStmtParamTranslator<PayordemInfo> getParamTranslatorHook() {
+		return new DaoStmtParamTranslator<PayordemInfo>() {	
+			@Override public PreparedStatement translateStmtParam(PreparedStatement stmt, PayordemInfo recordInfo) throws SQLException {		
+				int i = 1;
+				
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codStore);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codMat);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codEmployee);
+				stmt = DaoFormatter.localDateToStmt(stmt, i++, recordInfo.date);
+				stmt = DaoFormatter.localTimeToStmt(stmt, i++, recordInfo.beginTime);
+				stmt.setInt(i++, recordInfo.quantity);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.price);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.totitem);
+				stmt.setString(i++, recordInfo.codCurr);
+				stmt.setString(i++, recordInfo.itemReceiver);
+				stmt.setString(i++, recordInfo.ownId);
+				stmt.setString(i++, recordInfo.idOrderPartner);
+				stmt.setString(i++, recordInfo.statusOrderPartner);
+				stmt = DaoFormatter.charToStmt(stmt, i++, recordInfo.codFeeCateg);
+				stmt.setString(i++, recordInfo.idPaymentPartner);
+				stmt.setString(i++, recordInfo.statusPaymentPartner);
+				stmt = DaoFormatter.localDateTimeToStmt(stmt, i++, recordInfo.lastChanged);
+				stmt.setBoolean(i++, recordInfo.isSystemReceiver);
+				stmt.setString(i++, recordInfo.idRefundPartner);
+				stmt.setString(i++, recordInfo.statusRefundPartner);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOrder);
+				stmt = DaoFormatter.numberToStmt(stmt, i++, recordInfo.codOrderItem);
+				
+				return stmt;
+			}		
+		};
 	}
 }
