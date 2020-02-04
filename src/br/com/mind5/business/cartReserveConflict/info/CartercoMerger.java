8471fe05993f6ab61263b5df920b50a746030802
mmaciel <mmaciel@mind5.com>
2020-02-04 09:43:39 -0200

    Carterco #04

--- src/br/com/mind5/business/cartReserveConflict/info/CartercoMerger.java
+++ src/br/com/mind5/business/cartReserveConflict/info/CartercoMerger.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import br.com.mind5.business.cartReserve.info.CarterveInfo;
 import br.com.mind5.info.InfoMerger;
-import br.com.mind5.security.username.info.UsernameInfo;
 
 public final class CartercoMerger {	
 	public static CartercoInfo mergeWithCarterve(CarterveInfo selectedInfo, CartercoInfo baseInfo) {
@@ -16,20 +15,6 @@ public final class CartercoMerger {
 	
 	public static List<CartercoInfo> mergeWithCarterve(List<CarterveInfo> selectedInfos, List<CartercoInfo> baseInfos) {
 		InfoMerger<CartercoInfo, CarterveInfo> merger = new CartercoMergerCarterve();		
-		return merger.merge(selectedInfos, baseInfos);
-	}	
-	
-	
-	
-	public static CartercoInfo mergeWithUsername(UsernameInfo selectedInfo, CartercoInfo baseInfo) {
-		InfoMerger<CartercoInfo, UsernameInfo> merger = new CartercoMergerUsername();		
-		return merger.merge(selectedInfo, baseInfo);
-	}
-	
-	
-	
-	public static List<CartercoInfo> mergeWithUsername(List<UsernameInfo> selectedInfos, List<CartercoInfo> baseInfos) {
-		InfoMerger<CartercoInfo, UsernameInfo> merger = new CartercoMergerUsername();		
 		return merger.merge(selectedInfos, baseInfos);
 	}	
 }
