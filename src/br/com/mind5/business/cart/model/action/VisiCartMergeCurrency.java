450e6dcc961f47ca607b7834fbc9fae510646faa
mmaciel <mmaciel@mind5.com>
2020-02-08 22:29:12 -0200

    Cart #31

--- src/br/com/mind5/business/cart/model/action/VisiCartMergeCurrency.java
+++ src/br/com/mind5/business/cart/model/action/VisiCartMergeCurrency.java
@@ -31,6 +31,6 @@ final class VisiCartMergeCurrency extends ActionVisitorTemplateMergeV2<CartInfo,
 	
 	
 	@Override protected boolean shouldMergeWhenEmptyHook() {
-		return ActionVisitorTemplateMergeV2.DONT_MERGE_WHEN_EMPTY;
+		return ActionVisitorTemplateMergeV2.MERGE_WHEN_EMPTY;
 	}
 }
