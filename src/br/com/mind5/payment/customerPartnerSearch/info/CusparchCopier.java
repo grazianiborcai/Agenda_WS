53792f32f9d765b97a91b17f659d44f6bf34e8bf
mmaciel <mmaciel@mind5.com>
2020-02-09 02:42:40 -0200

    Cusparch #02

--- src/br/com/mind5/payment/customerPartnerSearch/info/CusparchCopier.java
+++ src/br/com/mind5/payment/customerPartnerSearch/info/CusparchCopier.java
@@ -4,6 +4,7 @@ package br.com.mind5.payment.customerPartnerSearch.info;
 import java.util.List;
 
 import br.com.mind5.info.InfoCopier;
+import br.com.mind5.payment.creditCard.info.CrecardInfo;
 import br.com.mind5.payment.customerPartner.info.CusparInfo;
 
 public final class CusparchCopier {	
@@ -14,8 +15,22 @@ public final class CusparchCopier {
 	
 	
 	
-	public static List<CusparchInfo> copyFromCrecard(List<CusparInfo> sources) {
+	public static List<CusparchInfo> copyFromCuspar(List<CusparInfo> sources) {
 		InfoCopier<CusparchInfo, CusparInfo> copier = new CusparchCopyCuspar();
 		return copier.makeCopy(sources);
 	}	
+	
+	
+	
+	public static CusparchInfo copyFromCrecard(CrecardInfo source) {
+		InfoCopier<CusparchInfo, CrecardInfo> copier = new CusparchCopyCrecard();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<CusparchInfo> copyFromCrecard(List<CrecardInfo> sources) {
+		InfoCopier<CusparchInfo, CrecardInfo> copier = new CusparchCopyCrecard();
+		return copier.makeCopy(sources);
+	}		
 }
