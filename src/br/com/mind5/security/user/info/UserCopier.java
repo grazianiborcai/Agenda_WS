c00878935be9c08b4d411900eeef89929c925179
mmaciel <mmaciel@mind5.com>
2020-02-09 23:36:53 -0200

    User #29

--- src/br/com/mind5/security/user/info/UserCopier.java
+++ src/br/com/mind5/security/user/info/UserCopier.java
@@ -9,10 +9,25 @@ import br.com.mind5.business.owner.info.OwnerInfo;
 import br.com.mind5.business.store.info.StoreInfo;
 import br.com.mind5.business.storeSnapshot.info.StorapInfo;
 import br.com.mind5.info.InfoCopier;
+import br.com.mind5.payment.partnerMoip.customerMoip.info.CusmoipInfo;
 import br.com.mind5.security.user.info.UserInfo;
 import br.com.mind5.security.userAuthentication.info.UauthInfo;
 
 public final class UserCopier {
+	public static UserInfo copyFromCusmoip(CusmoipInfo source) {
+		InfoCopier<UserInfo, CusmoipInfo> copier = new UserCopyCusmoip();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<UserInfo> copyFromCusmoip(List<CusmoipInfo> sources) {
+		InfoCopier<UserInfo, CusmoipInfo> copier = new UserCopyCusmoip();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
 	public static UserInfo copyFromUauth(UauthInfo source) {
 		InfoCopier<UserInfo, UauthInfo> copier = new UserCopyUauth();
 		return copier.makeCopy(source);
