7000f5a1565273d3571a2e06d8a2a0ba2207262e
mmaciel <mmaciel@mind5.com>
2020-02-24 20:07:38 -0300

    Stopar #07

--- src/br/com/mind5/payment/storePartner/info/StoparCopier.java
+++ src/br/com/mind5/payment/storePartner/info/StoparCopier.java
@@ -6,10 +6,25 @@ import java.util.List;
 import br.com.mind5.business.planingData.info.PlanataInfo;
 import br.com.mind5.info.InfoCopier;
 import br.com.mind5.payment.payOrderItem.info.PayordemInfo;
+import br.com.mind5.paymentPartner.partnerMoip.orderMoip.info.OrdmoipInfo;
 import br.com.mind5.paymentPartner.partnerMoip.permissionMoip.info.PeresmoipInfo;
 import br.com.mind5.paymentPartner.partnerMoip.refundMoip.info.RefumoipInfo;
 
 public final class StoparCopier {	
+	public static StoparInfo copyFromOrdmoip(OrdmoipInfo source) {
+		InfoCopier<StoparInfo, OrdmoipInfo> copier = new StoparCopyOrdmoip();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<StoparInfo> copyFromOrdmoip(List<OrdmoipInfo> sources) {
+		InfoCopier<StoparInfo, OrdmoipInfo> copier = new StoparCopyOrdmoip();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
 	public static StoparInfo copyFromRefumoip(RefumoipInfo source) {
 		InfoCopier<StoparInfo, RefumoipInfo> copier = new StoparCopyRefumoip();
 		return copier.makeCopy(source);
---
+++ src/br/com/mind5/payment/storePartner/info/StoparCopyOrdmoip.java
@@ -0,0 +1,25 @@
+package br.com.mind5.payment.storePartner.info;
+
+import br.com.mind5.info.InfoCopierTemplate;
+import br.com.mind5.paymentPartner.partnerMoip.orderMoip.info.OrdmoipInfo;
+
+final class StoparCopyOrdmoip extends InfoCopierTemplate<StoparInfo, OrdmoipInfo>{
+	
+	public StoparCopyOrdmoip() {
+		super();
+	}
+	
+	
+	
+	@Override protected StoparInfo makeCopyHook(OrdmoipInfo source) {
+		StoparInfo result = new StoparInfo();
+		
+		result.codOwner = source.payordemData.codOwner;
+		result.codStore = source.payordemData.codStore;	
+		result.codPayPartner = source.payordemData.codPayPartner;
+		result.codLanguage = source.codLanguage;
+		result.username = source.username;
+		
+		return result;
+	}
+}
