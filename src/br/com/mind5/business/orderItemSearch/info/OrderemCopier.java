7bb34f784f839f26813655380a287f361c7c4b79
mmaciel <mmaciel@mind5.com>
2020-02-28 15:38:37 -0300

    Ordemarch #02

---
+++ src/br/com/mind5/business/orderItemSearch/info/OrderemCopier.java
@@ -0,0 +1,21 @@
+package br.com.mind5.business.orderItemSearch.info;
+
+
+import java.util.List;
+
+import br.com.mind5.info.InfoCopierTemplate;
+import br.com.mind5.payment.refundOrder.info.RefuInfo;
+
+public final class OrderemCopier {
+	public static OrdemarchInfo copyFromOrder(RefuInfo source) {
+		InfoCopierTemplate<OrdemarchInfo, RefuInfo> copier = new OrdemarchCopyRefu();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<OrdemarchInfo> copyFromOrder(List<RefuInfo> sources) {
+		InfoCopierTemplate<OrdemarchInfo, RefuInfo> copier = new OrdemarchCopyRefu();
+		return copier.makeCopy(sources);
+	}
+}
