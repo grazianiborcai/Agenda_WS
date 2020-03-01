4fa6e8ab3af60ec4fe358c35ab6d5bb1851c80e3
mmaciel <mmaciel@mind5.com>
2020-03-01 16:09:37 -0300

    Stoparch #03

---
+++ src/br/com/mind5/payment/storePartnerSearch/model/checker/StoparchCheckExist.java
@@ -0,0 +1,35 @@
+package br.com.mind5.payment.storePartnerSearch.model.checker;
+
+import br.com.mind5.common.SystemCode;
+import br.com.mind5.model.action.ActionStd;
+import br.com.mind5.model.checker.ModelCheckerOption;
+import br.com.mind5.model.checker.ModelCheckerTemplateActionV2;
+import br.com.mind5.model.decisionTree.DeciTreeOption;
+import br.com.mind5.payment.storePartnerSearch.info.StoparchInfo;
+import br.com.mind5.payment.storePartnerSearch.model.decisionTree.RootStoparchSelect;
+
+public final class StoparchCheckExist extends ModelCheckerTemplateActionV2<StoparchInfo, StoparchInfo> {
+	
+	public StoparchCheckExist(ModelCheckerOption option) {
+		super(option, StoparchInfo.class);
+	}
+	
+
+	
+	@Override protected ActionStd<StoparchInfo> buildActionHook(DeciTreeOption<StoparchInfo> option) {
+		ActionStd<StoparchInfo> select = new RootStoparchSelect(option).toAction();
+		return select;
+	}
+	
+	
+	
+	@Override protected int getCodMsgOnResultTrueHook() {
+		return SystemCode.PAYPAR_STORE_SEARCH_ALREADY_EXIST;
+	}	
+	
+	
+	
+	@Override protected int getCodMsgOnResultFalseHook() {
+		return SystemCode.PAYPAR_STORE_SEARCH_NOT_FOUND;
+	}
+}
