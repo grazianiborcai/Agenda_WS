ecb568889c9e8b85ec44e741f4d38392e327c289
mmaciel <mmaciel@mind5.com>
2020-03-28 22:48:38 -0300

    Moonase #04

--- src/br/com/mind5/masterData/moonPhase/info/MoonaseCopyPlanime.java
+++ src/br/com/mind5/masterData/moonPhase/info/MoonaseCopyPlanime.java
@@ -2,7 +2,9 @@ package br.com.mind5.masterData.moonPhase.info;
 
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import br.com.mind5.business.planingData.info.PlanataInfo;
 import br.com.mind5.business.planningTime.info.PlanimeInfo;
@@ -17,15 +19,18 @@ final class MoonaseCopyPlanime extends InfoCopierOneToManyTemplate<MoonaseInfo, 
 	
 	
 	@Override protected List<MoonaseInfo> makeCopyHook(PlanimeInfo source) {
-		List<MoonaseInfo> results = new ArrayList<>();
+		Set<MoonaseInfo> results = new HashSet<>();
 		
-		for (PlanataInfo eachPlanata : source.planatas) {
-			MoonaseInfo oneResult = new MoonaseInfo();
-			oneResult.codMoonPhase = eachPlanata.codMoonPhase;
+		for (PlanataInfo eachPlanata: source.planatas) {
+			MoonaseInfo eachResult = new MoonaseInfo();
 			
-			results.add(oneResult);
+			eachResult.codMoonPhase = eachPlanata.codMoonPhase;
+			eachResult.codLanguage = eachPlanata.codLanguage;
+			
+			results.add(eachResult);
 		}
 		
-		return results;
+		
+		return new ArrayList<>(results);
 	}
 }
