04e9fd11b6f42e29a3a74bc2f9303b45a69e2a1e
mmaciel <mmaciel@mind5.com>
2020-04-04 22:23:24 -0300

    Info #34

---
+++ src/br/com/mind5/info/InfoSetterTemplate.java
@@ -0,0 +1,36 @@
+package br.com.mind5.info;
+
+import br.com.mind5.common.SystemLog;
+import br.com.mind5.common.SystemMessage;
+
+public abstract class InfoSetterTemplate<T extends InfoRecord> implements InfoSetter<T> {
+	
+	@Override public T setAttr(T recordInfo) {
+		checkArgument(recordInfo);
+		
+		return setAttrHook(recordInfo);
+	}
+	
+	
+	
+	protected T setAttrHook(T recordInfo) {
+		//Template method to be overridden by subclasses
+		logException(new IllegalStateException(SystemMessage.NO_TEMPLATE_IMPLEMENTATION));
+		throw new IllegalStateException(SystemMessage.NO_TEMPLATE_IMPLEMENTATION);
+	}
+	
+	
+	
+	private void checkArgument(T recordInfo) {
+		if (recordInfo == null) {
+			logException(new NullPointerException("recordInfo" + SystemMessage.NULL_ARGUMENT));
+			throw new NullPointerException("recordInfo" + SystemMessage.NULL_ARGUMENT);
+		}
+	}
+	
+	
+	
+	private void logException(Exception e) {
+		SystemLog.logError(this.getClass(), e);
+	}
+}
