7e180cf5d32866950b31c76b816bc78998f80811
mmaciel <mmaciel@mind5.com>
2020-04-13 10:20:20 -0300

    Formone #04

--- src/br/com/mind5/form/formPhone/model/decisionTree/NodeFormoneSelect.java
+++ src/br/com/mind5/form/formPhone/model/decisionTree/NodeFormoneSelect.java
@@ -12,9 +12,9 @@ import br.com.mind5.model.checker.ModelCheckerHelperQueueV2;
 import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerV1;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
-import br.com.mind5.model.decisionTree.DeciTreeTemplateReadV1;
+import br.com.mind5.model.decisionTree.DeciTreeTemplateReadV2;
 
-public final class NodeFormoneSelect extends DeciTreeTemplateReadV1<FormoneInfo> {
+public final class NodeFormoneSelect extends DeciTreeTemplateReadV2<FormoneInfo> {
 	
 	public NodeFormoneSelect(DeciTreeOption<FormoneInfo> option) {
 		super(option);
