024108b533fd19e70065ea36e0d860bd25eaea85
mmaciel <mmaciel@mind5.com>
2020-04-19 07:04:41 -0300

    Stolis #16

--- src/br/com/mind5/business/storeList/model/action/LazyStolisMergeCurrency.java
+++ src/br/com/mind5/business/storeList/model/action/LazyStolisMergeCurrency.java
@@ -4,12 +4,12 @@ import java.sql.Connection;
 import java.util.List;
 
 import br.com.mind5.business.storeList.info.StolisInfo;
-import br.com.mind5.model.action.ActionLazyTemplateV1;
+import br.com.mind5.model.action.ActionLazyTemplateV2;
 import br.com.mind5.model.action.ActionStdV1;
 import br.com.mind5.model.decisionTree.DeciResult;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
-public final class LazyStolisMergeCurrency extends ActionLazyTemplateV1<StolisInfo, StolisInfo> {
+public final class LazyStolisMergeCurrency extends ActionLazyTemplateV2<StolisInfo, StolisInfo> {
 	
 	public LazyStolisMergeCurrency(Connection conn, String schemaName) {
 		super(conn, schemaName);
--- src/br/com/mind5/business/storeList/model/action/LazyStolisRootSelect.java
+++ src/br/com/mind5/business/storeList/model/action/LazyStolisRootSelect.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import br.com.mind5.business.storeList.info.StolisInfo;
 import br.com.mind5.business.storeList.model.decisionTree.RootStolisSelect;
-import br.com.mind5.model.action.ActionLazyTemplateV1;
+import br.com.mind5.model.action.ActionLazyTemplateV2;
 import br.com.mind5.model.action.ActionStdV1;
 import br.com.mind5.model.decisionTree.DeciResult;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
-public final class LazyStolisRootSelect extends ActionLazyTemplateV1<StolisInfo, StolisInfo> {
+public final class LazyStolisRootSelect extends ActionLazyTemplateV2<StolisInfo, StolisInfo> {
 
 	public LazyStolisRootSelect(Connection conn, String schemaName) {
 		super(conn, schemaName);
