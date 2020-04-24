ea1c83a48c7dc37d47ba2b695db395c4a50e8a85
mmaciel <mmaciel@mind5.com>
2020-04-24 09:33:15 -0300

    Emplate #26

--- src/br/com/mind5/business/employeeLeaveDate/model/decisionTree/RootEmplateSelect.java
+++ src/br/com/mind5/business/employeeLeaveDate/model/decisionTree/RootEmplateSelect.java
@@ -10,13 +10,12 @@ import br.com.mind5.business.employeeLeaveDate.model.checker.EmplateCheckEmp;
 import br.com.mind5.business.employeeLeaveDate.model.checker.EmplateCheckLangu;
 import br.com.mind5.business.employeeLeaveDate.model.checker.EmplateCheckOwner;
 import br.com.mind5.business.employeeLeaveDate.model.checker.EmplateCheckRead;
-import br.com.mind5.business.employeeLeaveDate.model.checker.EmplateCheckStorauth;
 import br.com.mind5.business.employeeLeaveDate.model.checker.EmplateCheckStore;
 import br.com.mind5.model.action.ActionLazyV1;
 import br.com.mind5.model.action.ActionStdV1;
-import br.com.mind5.model.checker.ModelCheckerV1;
-import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerHelperQueueV2;
+import br.com.mind5.model.checker.ModelCheckerOption;
+import br.com.mind5.model.checker.ModelCheckerV1;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 import br.com.mind5.model.decisionTree.DeciTreeTemplateReadV2;
 
@@ -67,14 +66,14 @@ public class RootEmplateSelect extends DeciTreeTemplateReadV2<EmplateInfo> {
 		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;		
 		checker = new EmplateCheckStore(checkerOption);
 		queue.add(checker);	
-		
+		/*
 		checkerOption = new ModelCheckerOption();
 		checkerOption.conn = option.conn;
 		checkerOption.schemaName = option.schemaName;
 		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;		
-		checker = new EmplateCheckStorauth(checkerOption);
+		checker = new EmplateCheckStorauth(checkerOption);	//TODO: corrigir Storauth - Username ao inves do User
 		queue.add(checker);	
-		
+		*/
 		return new ModelCheckerHelperQueueV2<>(queue);
 	}
 	
