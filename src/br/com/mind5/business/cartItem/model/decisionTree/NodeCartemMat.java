c42c297978f61d50782426083c65fa5c14b14715
mmaciel <mmaciel@mind5.com>
2020-04-26 20:36:12 -0300

    Cartem #14

--- src/br/com/mind5/business/cartItem/model/decisionTree/NodeCartemMat.java
+++ src/br/com/mind5/business/cartItem/model/decisionTree/NodeCartemMat.java
@@ -7,11 +7,12 @@ import br.com.mind5.business.cartItem.info.CartemInfo;
 import br.com.mind5.business.cartItem.model.action.LazyCartemNodeMatService;
 import br.com.mind5.business.cartItem.model.action.StdCartemEnforceWeekday;
 import br.com.mind5.business.cartItem.model.checker.CartemCheckMatarchService;
+import br.com.mind5.business.cartItem.model.checker.CartemCheckPlanarch;
 import br.com.mind5.model.action.ActionLazyV1;
 import br.com.mind5.model.action.ActionStdV1;
-import br.com.mind5.model.checker.ModelCheckerV1;
-import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerHelperQueueV2;
+import br.com.mind5.model.checker.ModelCheckerOption;
+import br.com.mind5.model.checker.ModelCheckerV1;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 import br.com.mind5.model.decisionTree.DeciTreeTemplateWriteV2;
 
@@ -33,6 +34,13 @@ public final class NodeCartemMat extends DeciTreeTemplateWriteV2<CartemInfo> {
 		checkerOption.schemaName = option.schemaName;
 		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;	
 		checker = new CartemCheckMatarchService(checkerOption);
+		queue.add(checker);
+		
+		checkerOption = new ModelCheckerOption();
+		checkerOption.conn = option.conn;
+		checkerOption.schemaName = option.schemaName;
+		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;	
+		checker = new CartemCheckPlanarch(checkerOption);
 		queue.add(checker);
 		
 		return new ModelCheckerHelperQueueV2<>(queue);
