faf7a131c34326b06cfcf43e26a7ccbc1a8ccac5
mmaciel <mmaciel@mind5.com>
2020-05-09 23:03:28 -0300

    Order #49

--- src/br/com/mind5/business/order/model/checker/OrderCheckOrdarch.java
+++ src/br/com/mind5/business/order/model/checker/OrderCheckOrdarch.java
@@ -2,7 +2,7 @@ package br.com.mind5.business.order.model.checker;
 
 import br.com.mind5.business.order.info.OrderInfo;
 import br.com.mind5.business.orderSearch.info.OrdarchInfo;
-import br.com.mind5.business.orderSearch.model.checker.OrdarchCheckExist;
+import br.com.mind5.business.orderSearch.model.checker.OrdarchCheckExistAuth;
 import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerTemplateForwardV2;
 import br.com.mind5.model.checker.ModelCheckerV1;
@@ -16,7 +16,7 @@ public final class OrderCheckOrdarch extends ModelCheckerTemplateForwardV2<Order
 
 	
 	@Override protected ModelCheckerV1<OrdarchInfo> getCheckerHook(ModelCheckerOption option) {
-		return new OrdarchCheckExist(option);
+		return new OrdarchCheckExistAuth(option);
 	}
 	
 	
