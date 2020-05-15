e2586c6d46a282477f4af5ea7114ccb059283842
mmaciel <mmaciel@mind5.com>
2020-05-15 16:52:58 -0300

    Matarch #08

--- src/br/com/mind5/business/materialSearch/info/MatarchCopier.java
+++ src/br/com/mind5/business/materialSearch/info/MatarchCopier.java
@@ -3,6 +3,7 @@ package br.com.mind5.business.materialSearch.info;
 import java.util.List;
 
 import br.com.mind5.business.orderItem.info.OrderemInfo;
+import br.com.mind5.business.refundPolicy.info.RefupolInfo;
 import br.com.mind5.info.InfoCopier;
 
 public final class MatarchCopier {
@@ -15,6 +16,20 @@ public final class MatarchCopier {
 	
 	public static List<MatarchInfo> copyFromOrderem(List<OrderemInfo> sources) {
 		InfoCopier<MatarchInfo, OrderemInfo> copier = new MatarchCopyOrderem();
+		return copier.makeCopy(sources);
+	}	
+	
+	
+	
+	public static MatarchInfo copyFromRefupol(RefupolInfo source) {
+		InfoCopier<MatarchInfo, RefupolInfo> copier = new MatarchCopyRefupol();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<MatarchInfo> copyFromRefupol(List<RefupolInfo> sources) {
+		InfoCopier<MatarchInfo, RefupolInfo> copier = new MatarchCopyRefupol();
 		return copier.makeCopy(sources);
 	}	
 }
---
+++ src/br/com/mind5/business/materialSearch/info/MatarchCopyRefupol.java
@@ -0,0 +1,25 @@
+package br.com.mind5.business.materialSearch.info;
+
+
+import br.com.mind5.business.refundPolicy.info.RefupolInfo;
+import br.com.mind5.info.InfoCopierTemplate;
+
+final class MatarchCopyRefupol extends InfoCopierTemplate<MatarchInfo, RefupolInfo>{
+	
+	public MatarchCopyRefupol() {
+		super();
+	}
+	
+	
+	
+	@Override protected MatarchInfo makeCopyHook(RefupolInfo source) {
+		MatarchInfo result = new MatarchInfo();
+		
+		result.codOwner = source.codOwner;
+		result.codMat = source.codMat;
+		result.codLanguage = source.codLanguage;
+		result.username = source.username;
+		
+		return result;
+	}
+}
