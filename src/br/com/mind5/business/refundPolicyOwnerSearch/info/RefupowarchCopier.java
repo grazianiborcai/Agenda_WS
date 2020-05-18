e897df535b2e34df624c758cc4a44118347b9573
mmaciel <mmaciel@mind5.com>
2020-05-17 21:05:03 -0300

    Refupowarch #04

--- src/br/com/mind5/business/refundPolicyOwnerSearch/info/RefupowarchCopier.java
+++ src/br/com/mind5/business/refundPolicyOwnerSearch/info/RefupowarchCopier.java
@@ -7,14 +7,14 @@ import br.com.mind5.business.refundPolicyOwner.info.RefupownInfo;
 import br.com.mind5.info.InfoCopier;
 
 public final class RefupowarchCopier {
-	public static RefupowarchInfo copyFromCrecard(RefupownInfo source) {
+	public static RefupowarchInfo copyFromRefupown(RefupownInfo source) {
 		InfoCopier<RefupowarchInfo, RefupownInfo> copier = new RefupowarchCopyRefupown();
 		return copier.makeCopy(source);
 	}
 	
 	
 	
-	public static List<RefupowarchInfo> copyFromCrecard(List<RefupownInfo> sources) {
+	public static List<RefupowarchInfo> copyFromRefupown(List<RefupownInfo> sources) {
 		InfoCopier<RefupowarchInfo, RefupownInfo> copier = new RefupowarchCopyRefupown();
 		return copier.makeCopy(sources);
 	}
