93e1ba1bfd2f053f052a804dfd272774a3101ba1
mmaciel <mmaciel@mind5.com>
2020-05-25 16:33:18 -0300

    Mooncal #04

--- src/br/com/mind5/business/moonCalendar/info/MooncalCopier.java
+++ src/br/com/mind5/business/moonCalendar/info/MooncalCopier.java
@@ -4,7 +4,9 @@ package br.com.mind5.business.moonCalendar.info;
 import java.util.List;
 
 import br.com.mind5.business.planingData.info.PlanataInfo;
+import br.com.mind5.business.scheduleDay.info.SchedayInfo;
 import br.com.mind5.info.InfoCopier;
+import br.com.mind5.info.InfoCopierOneToMany;
 
 public final class MooncalCopier {	
 	public static MooncalInfo copyFromPlanata(PlanataInfo source) {
@@ -16,6 +18,20 @@ public final class MooncalCopier {
 	
 	public static List<MooncalInfo> copyFromPlanata(List<PlanataInfo> sources) {
 		InfoCopier<MooncalInfo, PlanataInfo> copier = new MooncalCopyPlanata();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
+	public static List<MooncalInfo> copyFromScheday(SchedayInfo source) {
+		InfoCopierOneToMany<MooncalInfo, SchedayInfo> copier = new MooncalCopyScheday();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<MooncalInfo> copyFromScheday(List<SchedayInfo> sources) {
+		InfoCopierOneToMany<MooncalInfo, SchedayInfo> copier = new MooncalCopyScheday();
 		return copier.makeCopy(sources);
 	}
 }
---
+++ src/br/com/mind5/business/moonCalendar/info/MooncalCopyScheday.java
@@ -0,0 +1,37 @@
+package br.com.mind5.business.moonCalendar.info;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import br.com.mind5.business.scheduleDay.info.SchedayInfo;
+import br.com.mind5.business.scheduleDayData.info.SchedaytaInfo;
+import br.com.mind5.info.InfoCopierOneToManyTemplate;
+
+final class MooncalCopyScheday extends InfoCopierOneToManyTemplate<MooncalInfo, SchedayInfo>{
+	
+	public MooncalCopyScheday() {
+		super();
+	}
+	
+	
+	
+	@Override protected List<MooncalInfo> makeCopyHook(SchedayInfo source) {
+		Set<MooncalInfo> results = new HashSet<>();		
+		
+		for (SchedaytaInfo eachSchedayta : source.schedaytas) {
+			MooncalInfo eachResult = new MooncalInfo();
+			
+			eachResult.moonDate = eachSchedayta.date;
+			eachResult.codLanguage = eachSchedayta.codLanguage;
+			eachResult.username = eachSchedayta.username;
+			
+			results.add(eachResult);
+		}
+		
+		
+		return new ArrayList<>(results);
+	}
+}
