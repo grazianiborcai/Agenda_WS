1f70dafb48202f624f14210e452a21e02358ab69
mmaciel <mmaciel@mind5.com>
2020-05-28 18:31:42 -0300

    Schedeekdat #05

--- src/br/com/mind5/business/scheduleWeekData/info/SchedeekdatCopySchedeek.java
+++ src/br/com/mind5/business/scheduleWeekData/info/SchedeekdatCopySchedeek.java
@@ -4,7 +4,7 @@ package br.com.mind5.business.scheduleWeekData.info;
 import br.com.mind5.business.scheduleWeek.info.SchedeekInfo;
 import br.com.mind5.info.InfoCopierTemplate;
 
-final class SchedeekdatCopySchedeek extends InfoCopierTemplate<SchedeekdatInfo, SchedeekInfo>{
+final class SchedeekdatCopySchedeek extends InfoCopierTemplate<SchedeekdatInfo, SchedeekInfo> {
 	
 	public SchedeekdatCopySchedeek() {
 		super();
@@ -14,10 +14,9 @@ final class SchedeekdatCopySchedeek extends InfoCopierTemplate<SchedeekdatInfo, 
 	
 	@Override protected SchedeekdatInfo makeCopyHook(SchedeekInfo source) {
 			SchedeekdatInfo result = new SchedeekdatInfo();
+			
 			result.codOwner = source.codOwner;
 			result.codStore = source.codStore;
-			result.codEmployee = source.codEmployee;
-			result.codCustomer = source.codCustomer;
 			result.codStore = source.codStore;
 			result.year = source.year;
 			result.month = source.month;
