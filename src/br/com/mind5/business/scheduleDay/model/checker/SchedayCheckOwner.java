051a544a3c6c4618369c3363c28727d5e4c89f17
mmaciel <mmaciel@mind5.com>
2020-05-31 08:13:52 -0300

    Scheday #05

---
+++ src/br/com/mind5/business/scheduleDay/model/checker/SchedayCheckOwner.java
@@ -0,0 +1,27 @@
+package br.com.mind5.business.scheduleDay.model.checker;
+
+import br.com.mind5.business.owner.info.OwnerInfo;
+import br.com.mind5.business.owner.model.checker.OwnerCheckExist;
+import br.com.mind5.business.scheduleDay.info.SchedayInfo;
+import br.com.mind5.model.checker.ModelCheckerOption;
+import br.com.mind5.model.checker.ModelCheckerTemplateForwardV2;
+import br.com.mind5.model.checker.ModelCheckerV1;
+
+public final class SchedayCheckOwner extends ModelCheckerTemplateForwardV2<SchedayInfo, OwnerInfo> {
+	
+	public SchedayCheckOwner(ModelCheckerOption option) {
+		super(option);
+	}
+
+
+	
+	@Override protected ModelCheckerV1<OwnerInfo> getCheckerHook(ModelCheckerOption option) {
+		return new OwnerCheckExist(option);
+	}
+	
+	
+	
+	@Override protected OwnerInfo toForwardClass(SchedayInfo baseRecord) {
+		return OwnerInfo.copyFrom(baseRecord);
+	}
+}
--- src/br/com/mind5/business/scheduleDay/model/decisionTree/RootSchedaySelect.java
+++ src/br/com/mind5/business/scheduleDay/model/decisionTree/RootSchedaySelect.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.business.scheduleDay.info.SchedayInfo;
+import br.com.mind5.business.scheduleDay.model.checker.SchedayCheckOwner;
 import br.com.mind5.business.scheduleDay.model.checker.SchedayCheckRead;
+import br.com.mind5.business.scheduleDay.model.checker.SchedayCheckStore;
 import br.com.mind5.model.action.ActionStdV1;
 import br.com.mind5.model.checker.ModelCheckerHelperQueueV2;
 import br.com.mind5.model.checker.ModelCheckerOption;
@@ -30,6 +32,20 @@ public final class RootSchedaySelect extends DeciTreeTemplateWriteV2<SchedayInfo
 		checkerOption.schemaName = option.schemaName;
 		checkerOption.expectedResult = ModelCheckerOption.SUCCESS;	
 		checker = new SchedayCheckRead(checkerOption);
+		queue.add(checker);
+		
+		checkerOption = new ModelCheckerOption();
+		checkerOption.conn = option.conn;
+		checkerOption.schemaName = option.schemaName;
+		checkerOption.expectedResult = ModelCheckerOption.SUCCESS;	
+		checker = new SchedayCheckOwner(checkerOption);
+		queue.add(checker);
+		
+		checkerOption = new ModelCheckerOption();
+		checkerOption.conn = option.conn;
+		checkerOption.schemaName = option.schemaName;
+		checkerOption.expectedResult = ModelCheckerOption.SUCCESS;	
+		checker = new SchedayCheckStore(checkerOption);
 		queue.add(checker);
 		
 		return new ModelCheckerHelperQueueV2<>(queue);
