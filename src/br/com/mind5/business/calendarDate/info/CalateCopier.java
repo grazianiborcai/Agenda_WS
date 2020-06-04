db8e487b746446c1ab550164591be4cf3c3ee00d
mmaciel <mmaciel@mind5.com>
2020-06-04 10:17:43 -0300

    Calate #09

--- src/br/com/mind5/business/calendarDate/info/CalateCopier.java
+++ src/br/com/mind5/business/calendarDate/info/CalateCopier.java
@@ -3,6 +3,7 @@ package br.com.mind5.business.calendarDate.info;
 
 import java.util.List;
 
+import br.com.mind5.business.calendarWeekYear.info.CaleekyInfo;
 import br.com.mind5.business.planingData.info.PlanataInfo;
 import br.com.mind5.info.InfoCopier;
 
@@ -16,6 +17,34 @@ public final class CalateCopier {
 	
 	public static List<CalateInfo> copyFromPlanata(List<PlanataInfo> sources) {
 		InfoCopier<CalateInfo, PlanataInfo> copier = new CalateCopyPlanata();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
+	public static CalateInfo copyFromCaleekyNext(CaleekyInfo source) {
+		InfoCopier<CalateInfo, CaleekyInfo> copier = new CalateCopyCaleekyNext();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<CalateInfo> copyFromCaleekyNext(List<CaleekyInfo> sources) {
+		InfoCopier<CalateInfo, CaleekyInfo> copier = new CalateCopyCaleekyNext();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
+	public static CalateInfo copyFromCaleekyPrevious(CaleekyInfo source) {
+		InfoCopier<CalateInfo, CaleekyInfo> copier = new CalateCopyCaleekyPrevious();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<CalateInfo> copyFromCaleekyPrevious(List<CaleekyInfo> sources) {
+		InfoCopier<CalateInfo, CaleekyInfo> copier = new CalateCopyCaleekyPrevious();
 		return copier.makeCopy(sources);
 	}
 }
---
+++ src/br/com/mind5/business/calendarDate/info/CalateCopyCaleekyPrevious.java
@@ -0,0 +1,24 @@
+package br.com.mind5.business.calendarDate.info;
+
+
+import br.com.mind5.business.calendarWeekYear.info.CaleekyInfo;
+import br.com.mind5.info.InfoCopierTemplate;
+
+final class CalateCopyCaleekyPrevious extends InfoCopierTemplate<CalateInfo, CaleekyInfo>{
+	
+	public CalateCopyCaleekyPrevious() {
+		super();
+	}
+	
+	
+	
+	@Override protected CalateInfo makeCopyHook(CaleekyInfo source) {
+		CalateInfo result = new CalateInfo();
+		
+		result.date = source.dateWeekBegin;
+		result.codLanguage = source.codLanguage;
+		result.username = source.username;
+		
+		return result;
+	}
+}
