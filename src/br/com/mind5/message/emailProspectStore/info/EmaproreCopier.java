d38538e9d49c7024a0709d8e5420f6b0ec588ff4
mmaciel <mmaciel@mind5.com>
2020-06-15 23:53:31 -0300

    Emaprore #06

--- src/br/com/mind5/message/emailProspectStore/info/EmaproreCopier.java
+++ src/br/com/mind5/message/emailProspectStore/info/EmaproreCopier.java
@@ -5,23 +5,8 @@ import java.util.List;
 
 import br.com.mind5.info.InfoCopier;
 import br.com.mind5.security.otpProspectStore.info.OtporeInfo;
-import br.com.mind5.security.otpUserPassword.info.OtperasInfo;
 
 public final class EmaproreCopier {	
-	public static EmaproreInfo copyFromOtperas(OtperasInfo source) {
-		InfoCopier<EmaproreInfo, OtperasInfo> copier = new EmaproreCopyOtperas();
-		return copier.makeCopy(source);
-	}
-	
-	
-	
-	public static List<EmaproreInfo> copyFromOtperas(List<OtperasInfo> sources) {
-		InfoCopier<EmaproreInfo, OtperasInfo> copier = new EmaproreCopyOtperas();
-		return copier.makeCopy(sources);
-	}
-	
-	
-	
 	public static EmaproreInfo copyFromOtpore(OtporeInfo source) {
 		InfoCopier<EmaproreInfo, OtporeInfo> copier = new EmaproreCopyOtpore();
 		return copier.makeCopy(source);
