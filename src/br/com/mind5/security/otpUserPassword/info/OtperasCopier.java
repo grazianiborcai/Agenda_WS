057c4d11e2edc5d0d2f4c2a4609e7fdae7a72c3f
mmaciel <mmaciel@mind5.com>
2020-06-16 17:06:56 -0300

    Otperas #06

--- src/br/com/mind5/security/otpUserPassword/info/OtperasCopier.java
+++ src/br/com/mind5/security/otpUserPassword/info/OtperasCopier.java
@@ -7,14 +7,14 @@ import br.com.mind5.info.InfoCopier;
 import br.com.mind5.security.userPassword.info.UpswdInfo;
 
 public final class OtperasCopier {	
-	public static OtperasInfo copyFromUser(UpswdInfo source) {
+	public static OtperasInfo copyFromUpswd(UpswdInfo source) {
 		InfoCopier<OtperasInfo, UpswdInfo> copier = new OtperasCopyUpswd();
 		return copier.makeCopy(source);
 	}
 	
 	
 	
-	public static List<OtperasInfo> copyFromUser(List<UpswdInfo> sources) {
+	public static List<OtperasInfo> copyFromUpswd(List<UpswdInfo> sources) {
 		InfoCopier<OtperasInfo, UpswdInfo> copier = new OtperasCopyUpswd();
 		return copier.makeCopy(sources);
 	}
