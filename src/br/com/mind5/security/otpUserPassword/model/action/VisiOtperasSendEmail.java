320c119441ddae4c17888f710aadfc3365668201
mmaciel <mmaciel@mind5.com>
2020-06-15 23:53:23 -0300

    Otperas #04

--- src/br/com/mind5/security/otpUserPassword/model/action/VisiOtperasSendEmail.java
+++ src/br/com/mind5/security/otpUserPassword/model/action/VisiOtperasSendEmail.java
@@ -2,35 +2,35 @@ package br.com.mind5.security.otpUserPassword.model.action;
 
 import java.util.List;
 
-import br.com.mind5.message.emailProspectStore.info.EmaproreCopier;
-import br.com.mind5.message.emailProspectStore.info.EmaproreInfo;
-import br.com.mind5.message.emailProspectStore.model.decisionTree.RootEmaproreSend;
+import br.com.mind5.message.emailUserOtp.info.EmusotpCopier;
+import br.com.mind5.message.emailUserOtp.info.EmusotpInfo;
+import br.com.mind5.message.emailUserOtp.model.decisionTree.RootEmusotpSend;
 import br.com.mind5.model.action.ActionVisitorTemplateActionV2;
 import br.com.mind5.model.decisionTree.DeciTree;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 import br.com.mind5.security.otpUserPassword.info.OtperasInfo;
 
-final class VisiOtperasSendEmail extends ActionVisitorTemplateActionV2<OtperasInfo, EmaproreInfo> {
+final class VisiOtperasSendEmail extends ActionVisitorTemplateActionV2<OtperasInfo, EmusotpInfo> {
 	
 	public VisiOtperasSendEmail(DeciTreeOption<OtperasInfo> option) {
-		super(option, OtperasInfo.class, EmaproreInfo.class);
+		super(option, OtperasInfo.class, EmusotpInfo.class);
 	}
 	
 	
 	
-	@Override protected Class<? extends DeciTree<EmaproreInfo>> getTreeClassHook() {
-		return RootEmaproreSend.class;
+	@Override protected Class<? extends DeciTree<EmusotpInfo>> getTreeClassHook() {
+		return RootEmusotpSend.class;
 	}
 	
 	
 	
-	@Override protected List<EmaproreInfo> toActionClassHook(List<OtperasInfo> recordInfos) {
-		return EmaproreCopier.copyFromOtperas(recordInfos);
+	@Override protected List<EmusotpInfo> toActionClassHook(List<OtperasInfo> recordInfos) {
+		return EmusotpCopier.copyFromOtperas(recordInfos);
 	}
 	
 	
 	
-	@Override protected List<OtperasInfo> toBaseClassHook(List<OtperasInfo> baseInfos, List<EmaproreInfo> results) {
+	@Override protected List<OtperasInfo> toBaseClassHook(List<OtperasInfo> baseInfos, List<EmusotpInfo> results) {
 		return baseInfos;
 	}
 }
