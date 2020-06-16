5fde3c8a1f3c7b43900ae6b4af4b952bf3852284
mmaciel <mmaciel@mind5.com>
2020-06-15 23:44:09 -0300

    MasterData #09

--- src/br/com/mind5/business/masterData/info/common/EmailBody.java
+++ src/br/com/mind5/business/masterData/info/common/EmailBody.java
@@ -2,6 +2,7 @@ package br.com.mind5.business.masterData.info.common;
 
 public enum EmailBody {
 	PROSPECT_STORE("PROSPECT_STORE"),
+	OTP_USER_PASSWORD("OTP_USER_PASSWORD"),
 	WELCOME("WELCOME");
 	
 	private final String codBody;
