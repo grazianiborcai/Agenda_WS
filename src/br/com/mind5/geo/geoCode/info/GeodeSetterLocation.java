f3ef97ae440c25090c4128f957198224fbf7a829
mmaciel <mmaciel@mind5.com>
2020-06-29 22:58:39 -0300

    Geode #02

--- src/br/com/mind5/geo/geoCode/info/GeodeSetterLocation.java
+++ src/br/com/mind5/geo/geoCode/info/GeodeSetterLocation.java
@@ -7,26 +7,44 @@ public final class GeodeSetterLocation extends InfoSetterTemplate<GeodeInfo> {
 	@Override protected GeodeInfo setAttrHook(GeodeInfo recordInfo) {
 		StringBuilder result = new StringBuilder();
 		
-		if (recordInfo.street != null)
+		if (recordInfo.street != null) {
 			result.append(recordInfo.street);
+			result.append(" ");
+		}
 		
-		if (recordInfo.streetNumber != null)
+		
+		if (recordInfo.streetNumber != null) {
 			result.append(recordInfo.streetNumber);
+			result.append(" ");
+		}
 		
-		if (recordInfo.district != null)
+		
+		if (recordInfo.district != null) {
 			result.append(recordInfo.district);
+			result.append(" ");
+		}
 		
-		if (recordInfo.city != null)
+		
+		if (recordInfo.city != null) {
 			result.append(recordInfo.city);
+			result.append(" ");
+		}
 		
-		if (recordInfo.txtState != null)
+		
+		if (recordInfo.txtState != null) {
 			result.append(recordInfo.txtState);
+			result.append(" ");
+		}
+
 		
 		if (recordInfo.txtCountry != null)
 			result.append(recordInfo.txtCountry);
 		
 		
 		recordInfo.location = result.toString();
+		recordInfo.location = recordInfo.location.trim();
+		recordInfo.location = recordInfo.location.replaceAll("\\s+","+");
+		
 		return recordInfo;
 	}
 }
