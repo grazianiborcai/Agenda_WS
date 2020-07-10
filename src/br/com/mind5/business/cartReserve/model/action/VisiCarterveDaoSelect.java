f358492ec863bee16545d62f61885b445d7de763
mmaciel <mmaciel@mind5.com>
2020-07-09 21:08:44 -0300

    Carterve #06

--- src/br/com/mind5/business/cartReserve/model/action/VisiCarterveDaoSelect.java
+++ src/br/com/mind5/business/cartReserve/model/action/VisiCarterveDaoSelect.java
@@ -9,7 +9,7 @@ import br.com.mind5.dao.DaoStmtExecV2;
 import br.com.mind5.model.action.ActionVisitorTemplateStmtV2;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
-final class VisiCarterveDaoSelect extends ActionVisitorTemplateStmtV2<CarterveInfo>{
+final class VisiCarterveDaoSelect extends ActionVisitorTemplateStmtV2<CarterveInfo> {
 
 	public VisiCarterveDaoSelect(DeciTreeOption<CarterveInfo> option) {
 		super(option);
