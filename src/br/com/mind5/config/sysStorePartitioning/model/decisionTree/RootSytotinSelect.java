4b181e0be49c2f1beccbc351874394f648fc9ee6
mmaciel <mmaciel@mind5.com>
2020-07-11 22:56:38 -0300

    Sytotin #03

--- src/br/com/mind5/config/sysStorePartitioning/model/decisionTree/RootSytotinSelect.java
+++ src/br/com/mind5/config/sysStorePartitioning/model/decisionTree/RootSytotinSelect.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 import br.com.mind5.config.sysStorePartitioning.info.SytotinInfo;
 import br.com.mind5.config.sysStorePartitioning.model.action.StdSytotinDaoSelect;
+import br.com.mind5.config.sysStorePartitioning.model.checker.SytotinCheckRead;
 import br.com.mind5.model.action.ActionStdV1;
 import br.com.mind5.model.checker.ModelCheckerHelperQueueV2;
+import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerV1;
-import br.com.mind5.model.checker.common.ModelCheckerDummy;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 import br.com.mind5.model.decisionTree.DeciTreeTemplateReadV2;
 
@@ -22,9 +23,14 @@ public final class RootSytotinSelect extends DeciTreeTemplateReadV2<SytotinInfo>
 	
 	@Override protected ModelCheckerV1<SytotinInfo> buildCheckerHook(DeciTreeOption<SytotinInfo> option) {
 		List<ModelCheckerV1<SytotinInfo>> queue = new ArrayList<>();		
-		ModelCheckerV1<SytotinInfo> checker;
-
-		checker = new ModelCheckerDummy<>();
+		ModelCheckerV1<SytotinInfo> checker;	
+		ModelCheckerOption checkerOption;
+		
+		checkerOption = new ModelCheckerOption();
+		checkerOption.conn = option.conn;
+		checkerOption.schemaName = option.schemaName;
+		checkerOption.expectedResult = ModelCheckerOption.SUCCESS;	
+		checker = new SytotinCheckRead(checkerOption);
 		queue.add(checker);
 		
 		return new ModelCheckerHelperQueueV2<>(queue);
