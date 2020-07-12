f5678672a364d8f49de645612a671a130326e3ee
mmaciel <mmaciel@mind5.com>
2020-07-11 22:56:17 -0300

    Sysonup #03

--- src/br/com/mind5/config/sysOwnerSignup/model/action/LazySysonupRootSelect.java
+++ src/br/com/mind5/config/sysOwnerSignup/model/action/LazySysonupRootSelect.java
@@ -4,14 +4,15 @@ import java.sql.Connection;
 import java.util.List;
 
 import br.com.mind5.config.sysOwnerSignup.info.SysonupInfo;
+import br.com.mind5.config.sysOwnerSignup.model.decisionTree.RootSysonupSelect;
 import br.com.mind5.model.action.ActionLazyTemplateV2;
 import br.com.mind5.model.action.ActionStdV1;
 import br.com.mind5.model.decisionTree.DeciResult;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
-public final class LazySysonupDaoSelect extends ActionLazyTemplateV2<SysonupInfo, SysonupInfo> {
+public final class LazySysonupRootSelect extends ActionLazyTemplateV2<SysonupInfo, SysonupInfo> {
 
-	public LazySysonupDaoSelect(Connection conn, String schemaName) {
+	public LazySysonupRootSelect(Connection conn, String schemaName) {
 		super(conn, schemaName);
 	}
 	
@@ -24,7 +25,7 @@ public final class LazySysonupDaoSelect extends ActionLazyTemplateV2<SysonupInfo
 	
 	
 	@Override protected ActionStdV1<SysonupInfo> getInstanceOfActionHook(DeciTreeOption<SysonupInfo> option) {
-		return new StdSysonupDaoSelect(option);
+		return new RootSysonupSelect(option).toAction();
 	}
 	
 	
--- src/br/com/mind5/config/sysOwnerSignup/model/decisionTree/RootSysonupSelectEnabled.java
+++ src/br/com/mind5/config/sysOwnerSignup/model/decisionTree/RootSysonupSelectEnabled.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.config.sysOwnerSignup.info.SysonupInfo;
-import br.com.mind5.config.sysOwnerSignup.model.action.LazySysonupDaoSelect;
+import br.com.mind5.config.sysOwnerSignup.model.action.LazySysonupRootSelect;
 import br.com.mind5.config.sysOwnerSignup.model.action.StdSysonupEnforceEnabled;
 import br.com.mind5.model.action.ActionLazyV1;
 import br.com.mind5.model.action.ActionStdV1;
@@ -38,7 +38,7 @@ public final class RootSysonupSelectEnabled extends DeciTreeTemplateReadV2<Syson
 		List<ActionStdV1<SysonupInfo>> actions = new ArrayList<>();
 		
 		ActionStdV1<SysonupInfo> enforceEnabled = new StdSysonupEnforceEnabled(option);
-		ActionLazyV1<SysonupInfo> select = new LazySysonupDaoSelect(option.conn, option.schemaName);
+		ActionLazyV1<SysonupInfo> select = new LazySysonupRootSelect(option.conn, option.schemaName);
 		
 		enforceEnabled.addPostAction(select);
 		
