a6a48b9cd6b480dfd348063d9a9bf1ff41a91440
mmaciel <mmaciel@mind5.com>
2020-07-11 21:01:52 -0300

    Userarch #15

--- src/br/com/mind5/security/userSearch/model/checker/UserarchCheckReadAuth.java
+++ src/br/com/mind5/security/userSearch/model/checker/UserarchCheckReadAuth.java
@@ -2,7 +2,6 @@ package br.com.mind5.security.userSearch.model.checker;
 
 import java.sql.Connection;
 
-import br.com.mind5.common.DefaultValue;
 import br.com.mind5.common.SystemCode;
 import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerTemplateSimpleV2;
@@ -17,10 +16,10 @@ public final class UserarchCheckReadAuth extends ModelCheckerTemplateSimpleV2<Us
 	
 	
 	@Override protected boolean checkHook(UserarchInfo recordInfo, Connection conn, String schemaName) {	
-		if (recordInfo.codOwner 		<= 0 	||
-			recordInfo.codLanguage 		== null	||
-			recordInfo.username			== null	||
-			recordInfo.codUserCategory	== DefaultValue.character() )		
+		if (recordInfo.codOwner 	<= 0 	||
+			recordInfo.codLanguage 	== null	||
+			recordInfo.username		== null	||
+			recordInfo.codAuthGroup	== null 	)		
 			
 			return super.FAILED;	
 
