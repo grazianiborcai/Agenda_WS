e86daf96e7e6998c2f645954e534b9874d467f26
mmaciel <mmaciel@mind5.com>
2020-07-21 08:10:28 -0300

    Matext #18

--- src/br/com/mind5/business/materialText/info/MatextSetterTxtSearch.java
+++ src/br/com/mind5/business/materialText/info/MatextSetterTxtSearch.java
@@ -1,20 +1,12 @@
 package br.com.mind5.business.materialText.info;
 
-import java.text.Normalizer;
-
+import br.com.mind5.common.StringUtil;
 import br.com.mind5.info.InfoSetterTemplate;
 
 public final class MatextSetterTxtSearch extends InfoSetterTemplate<MatextInfo> {
 	
 	@Override protected MatextInfo setAttrHook(MatextInfo recordInfo) {	
-		if (recordInfo.txtMat == null)
-			return recordInfo;
-		
-		
-		recordInfo.txtMatSearch = recordInfo.txtMat.toUpperCase();
-		recordInfo.txtMatSearch = Normalizer.normalize(recordInfo.txtMatSearch, Normalizer.Form.NFD);
-		recordInfo.txtMatSearch = recordInfo.txtMatSearch.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-		
+		recordInfo.txtMatSearch = StringUtil.normalizeSearch(recordInfo.txtMat);		
 		return recordInfo;
 	}
 }
