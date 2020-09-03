b7e97ce50bb77d3ef5e80324f1613619e872aa49
mmaciel <mmaciel@mind5.com>
2020-09-02 22:13:59 -0300

    Daypart #02

--- src/br/com/mind5/masterData/dayParting/info/DaypartCopier.java
+++ src/br/com/mind5/masterData/dayParting/info/DaypartCopier.java
@@ -3,10 +3,25 @@ package br.com.mind5.masterData.dayParting.info;
 
 import java.util.List;
 
+import br.com.mind5.business.calendarCatalogue.info.CalgueInfo;
 import br.com.mind5.business.planningTime.info.PlanimeInfo;
 import br.com.mind5.info.InfoCopierOneToMany;
 
 public final class DaypartCopier {
+	public static List<DaypartInfo> copyFromCalgue(CalgueInfo source) {
+		InfoCopierOneToMany<DaypartInfo, CalgueInfo> copier = new DaypartCopyCalgue();
+		return copier.makeCopy(source);
+	}
+	
+	
+	
+	public static List<DaypartInfo> copyFromCalgue(List<CalgueInfo> sources) {
+		InfoCopierOneToMany<DaypartInfo, CalgueInfo> copier = new DaypartCopyCalgue();
+		return copier.makeCopy(sources);
+	}
+	
+	
+	
 	public static List<DaypartInfo> copyFromPlanime(PlanimeInfo source) {
 		InfoCopierOneToMany<DaypartInfo, PlanimeInfo> copier = new DaypartCopyPlanime();
 		return copier.makeCopy(source);
