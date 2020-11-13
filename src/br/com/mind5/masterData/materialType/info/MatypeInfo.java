ffe7b5162218d4fb6ccab51cb3ccf16946dcb205
mmaciel <mmaciel@mind5.com>
2020-11-13 07:41:50 -0300

    Matype #02

--- src/br/com/mind5/masterData/materialType/info/MatypeInfo.java
+++ src/br/com/mind5/masterData/materialType/info/MatypeInfo.java
@@ -11,7 +11,7 @@ public final class MatypeInfo extends InfoRecord implements Cloneable {
 	
 	
 	public MatypeInfo() {
-		super(MatypeInfo.class);
+		super();
 		
 		codType = DefaultValue.number();
 	}
