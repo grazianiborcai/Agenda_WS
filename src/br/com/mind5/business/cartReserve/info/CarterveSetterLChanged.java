53218942304aba26bf938ea7976898d4b59e4c94
mmaciel <mmaciel@mind5.com>
2020-11-15 09:18:50 -0300

    Carterve #07

--- src/br/com/mind5/business/cartReserve/info/CarterveSetterLChanged.java
+++ src/br/com/mind5/business/cartReserve/info/CarterveSetterLChanged.java
@@ -1,24 +1,12 @@
 package br.com.mind5.business.cartReserve.info;
 
-import java.time.LocalDateTime;
-
-import br.com.mind5.common.DefaultValue;
+import br.com.mind5.common.TimeAge;
 import br.com.mind5.info.InfoSetterTemplate;
 
 public final class CarterveSetterLChanged extends InfoSetterTemplate<CarterveInfo> {
-	private long TIME_SPAN = 15;
-	
-	
-	@Override protected CarterveInfo setAttrHook(CarterveInfo recordInfo) {
-		LocalDateTime now = DefaultValue.localDateTimeNow();			
-		recordInfo.lastChanged = shiftTime(now);	
-		
+
+	@Override protected CarterveInfo setAttrHook(CarterveInfo recordInfo) {		
+		recordInfo.lastChanged = new TimeAge().getNowMinusOffset();		
 		return recordInfo;
-	}
-	
-	
-	
-	private LocalDateTime shiftTime(LocalDateTime now) {
-		return now.minusMinutes(TIME_SPAN);
 	}
 }
