64c7cf4e59ce526a456ef9ec03639b12970be41f
mmaciel <mmaciel@mind5.com>
2020-11-20 06:35:36 -0300

    Frite #04

--- src/br/com/mind5/file/fileWrite/info/FriteCopyFimg.java
+++ src/br/com/mind5/file/fileWrite/info/FriteCopyFimg.java
@@ -13,6 +13,7 @@ final class FriteCopyFimg extends InfoCopierTemplate<FriteInfo, FimgInfo> {
 	
 	@Override protected FriteInfo makeCopyHook(FimgInfo source) {
 		FriteInfo result = new FriteInfo();
+		
 		result.fileUri = source.fileImgUri;
 		result.fileData = source.fileImgData;
 		
