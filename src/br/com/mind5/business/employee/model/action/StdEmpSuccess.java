139ec2daffe9687d6af158a6daf20f185f84137f
mmaciel <mmaciel@mind5.com>
2020-11-21 21:13:47 -0300

    Emp #36

--- src/br/com/mind5/business/employee/model/action/StdEmpSuccess.java
+++ src/br/com/mind5/business/employee/model/action/StdEmpSuccess.java
@@ -5,6 +5,7 @@ import br.com.mind5.model.action.commom.ActionStdSuccessTemplate;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
 public final class StdEmpSuccess extends ActionStdSuccessTemplate<EmpInfo> {
+	
 	public StdEmpSuccess(DeciTreeOption<EmpInfo> option) {
 		super(option);
 	}
