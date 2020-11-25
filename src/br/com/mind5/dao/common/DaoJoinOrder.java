dc43bf41405cd6ddc6d8c401246efa06a229519b
mmaciel <mmaciel@mind5.com>
2020-11-25 10:13:05 -0300

    Dao #22

---
+++ src/br/com/mind5/dao/common/DaoJoinOrder.java
@@ -0,0 +1,58 @@
+package br.com.mind5.dao.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.mind5.dao.DaoJoin;
+import br.com.mind5.dao.DaoJoinBuilder;
+import br.com.mind5.dao.DaoJoinColumn;
+import br.com.mind5.dao.DaoJoinType;
+
+public final class DaoJoinOrder implements DaoJoinBuilder {
+	private final String leftTable;
+	
+	
+	public DaoJoinOrder(String leftTableName) {
+		leftTable = leftTableName;
+	}
+
+	
+	
+	@Override public DaoJoin build() {
+		DaoJoin join = new DaoJoin();
+		join.rightTableName = DaoDbTable.ORDER_HDR_TABLE;
+		join.joinType = DaoJoinType.INNER_JOIN;
+		join.joinColumns = getJoinColumns(leftTable);
+		join.constraintClause = null;
+		
+		return join;
+	}
+	
+	
+	
+	private List<DaoJoinColumn> getJoinColumns(String leftTableName) {
+		List<DaoJoinColumn> joinColumns = new ArrayList<>();
+		
+		DaoJoinColumn oneColumn = new DaoJoinColumn();
+		oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = DaoDbField.COL_COD_OWNER;
+		oneColumn.rightColumnName = DaoDbField.COL_COD_OWNER;
+		joinColumns.add(oneColumn);
+		
+		oneColumn = new DaoJoinColumn();
+		oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = DaoDbField.COL_COD_ORDER;
+		oneColumn.rightColumnName = DaoDbField.COL_COD_ORDER;
+		joinColumns.add(oneColumn);
+		
+		oneColumn = new DaoJoinColumn();
+		oneColumn.leftTableName = leftTableName;
+		oneColumn.leftColumnName = DaoDbField.COL_RECORD_MODE;
+		oneColumn.rightColumnName = DaoDbField.COL_RECORD_MODE;
+		joinColumns.add(oneColumn);
+		
+		return joinColumns;
+	}
+}
