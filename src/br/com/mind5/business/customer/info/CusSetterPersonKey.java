6ea3ab7134b18c320dd32f45fa23d911517e94fd
mmaciel <mmaciel@mind5.com>
2020-11-26 12:10:42 -0300

    Cus #52

--- src/br/com/mind5/business/customer/info/CusSetterPersonKey.java
+++ src/br/com/mind5/business/customer/info/CusSetterPersonKey.java
@@ -7,6 +7,7 @@ public final class CusSetterPersonKey extends InfoSetterTemplate<CusInfo> {
 	@Override protected CusInfo setAttrHook(CusInfo recordInfo) {
 		recordInfo.personData.codOwner = recordInfo.codOwner;
 		recordInfo.personData.codPerson = recordInfo.codPerson;
+		recordInfo.personData.codStore = recordInfo.codStore;
 		recordInfo.personData.username = recordInfo.username;
 		recordInfo.personData.codLanguage = recordInfo.codLanguage;
 		
