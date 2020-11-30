9bfc46342a5023422f6409ebdf4ef7a8c7d8e0f7
mmaciel <mmaciel@mind5.com>
2020-11-30 10:06:26 -0300

    Personap #07

--- src/br/com/mind5/business/personSnapshot/dao/DaoPersonapDbTableColumn.java
+++ src/br/com/mind5/business/personSnapshot/dao/DaoPersonapDbTableColumn.java
@@ -48,7 +48,7 @@ public final class DaoPersonapDbTableColumn extends DaoDbTableColumnTemplate {
 		oneColumn.columnName = COL_COD_SNAPSHOT;
 		oneColumn.isPK = IS_PRIMARY_KEY;
 		oneColumn.isLookUp = NEGATIVE;
-		oneColumn.isAutoIncremented = NEGATIVE;
+		oneColumn.isAutoIncremented = IS_AUTO_INCREMENTED;
 		columns.add(oneColumn);
 		
 		oneColumn = new DaoColumn();
