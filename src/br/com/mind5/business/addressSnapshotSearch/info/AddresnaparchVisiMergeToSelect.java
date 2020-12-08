5537bc8c538ae42752de7b80b1fecc7b806121bb
mmaciel <mmaciel@mind5.com>
2020-12-08 08:16:20 -0300

    Addresnaparch #02

--- src/br/com/mind5/business/addressSnapshotSearch/info/AddresnaparchVisiMergeToSelect.java
+++ src/br/com/mind5/business/addressSnapshotSearch/info/AddresnaparchVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class AddresnaparchVisiMergeToSelect extends InfoMergerVisitorTemplate<AddresnaparchInfo, AddresnaparchInfo> {
-	
-	@Override public List<AddresnaparchInfo> beforeMerge(List<AddresnaparchInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(AddresnaparchInfo baseInfo, AddresnaparchInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class AddresnaparchVisiMergeToSelect extends InfoMergerVisitorTemplate<Add
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<AddresnaparchInfo> getUniquifier() {
-		return null;
 	}
 }
