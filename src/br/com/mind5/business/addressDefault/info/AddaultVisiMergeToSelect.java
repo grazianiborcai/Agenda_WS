23fe908956c330394bf746b1eaa9cc3934b10ec1
mmaciel <mmaciel@mind5.com>
2020-12-08 07:50:29 -0300

    Addault #03

--- src/br/com/mind5/business/addressDefault/info/AddaultVisiMergeToSelect.java
+++ src/br/com/mind5/business/addressDefault/info/AddaultVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class AddaultVisiMergeToSelect extends InfoMergerVisitorTemplate<AddaultInfo, AddaultInfo> {
-	
-	@Override public List<AddaultInfo> beforeMerge(List<AddaultInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(AddaultInfo baseInfo, AddaultInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class AddaultVisiMergeToSelect extends InfoMergerVisitorTemplate<AddaultIn
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<AddaultInfo> getUniquifier() {
-		return null;
 	}
 }
