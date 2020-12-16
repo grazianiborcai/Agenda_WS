db6d9b6d38fc3437a7a4b209689d1827fb7b6d1d
mmaciel <mmaciel@mind5.com>
2020-12-16 11:58:02 -0300

    Orderem #16

--- src/br/com/mind5/business/orderItem/model/decisionTree/NodeOrderemUpdate.java
+++ src/br/com/mind5/business/orderItem/model/decisionTree/NodeOrderemUpdate.java
@@ -6,13 +6,14 @@ import java.util.List;
 import br.com.mind5.business.orderItem.info.OrderemInfo;
 import br.com.mind5.business.orderItem.model.action.LazyOrderemEnforceLChanged;
 import br.com.mind5.business.orderItem.model.action.LazyOrderemNodeSnapshot;
+import br.com.mind5.business.orderItem.model.action.LazyOrderemStusorageUpsert;
 import br.com.mind5.business.orderItem.model.action.StdOrderemMergeUsername;
 import br.com.mind5.business.orderItem.model.checker.OrderemCheckWrite;
 import br.com.mind5.model.action.ActionLazy;
 import br.com.mind5.model.action.ActionStd;
+import br.com.mind5.model.checker.ModelChecker;
 import br.com.mind5.model.checker.ModelCheckerHelperQueue;
 import br.com.mind5.model.checker.ModelCheckerOption;
-import br.com.mind5.model.checker.ModelChecker;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 import br.com.mind5.model.decisionTree.DeciTreeTemplateWrite;
 
@@ -47,9 +48,11 @@ public final class NodeOrderemUpdate extends DeciTreeTemplateWrite<OrderemInfo> 
 		ActionStd<OrderemInfo> mergeUsername = new StdOrderemMergeUsername(option);
 		ActionLazy<OrderemInfo> enforceLChanged = new LazyOrderemEnforceLChanged(option.conn, option.schemaName);
 		ActionLazy<OrderemInfo> snapshot = new LazyOrderemNodeSnapshot(option.conn, option.schemaName);
+		ActionLazy<OrderemInfo> stusorageUpsert = new LazyOrderemStusorageUpsert(option.conn, option.schemaName);
 		
 		mergeUsername.addPostAction(enforceLChanged);
 		enforceLChanged.addPostAction(snapshot);
+		snapshot.addPostAction(stusorageUpsert);
 		
 		actions.add(mergeUsername);
 		return actions;
