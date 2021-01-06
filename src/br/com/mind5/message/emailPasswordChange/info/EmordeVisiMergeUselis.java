805b5597c74023a1322bef600c10516b97cab06b
mmaciel <mmaciel@mind5.com>
2021-01-06 07:33:25 -0300

    Emorde #03

--- src/br/com/mind5/message/emailPasswordChange/info/EmordeVisiMergeUselis.java
+++ src/br/com/mind5/message/emailPasswordChange/info/EmordeVisiMergeUselis.java
@@ -4,17 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 import br.com.mind5.security.userList.info.UselisInfo;
 
 final class EmordeVisiMergeUselis extends InfoMergerVisitorTemplate<EmordeInfo, UselisInfo> {
-	
-	@Override public List<EmordeInfo> beforeMerge(List<EmordeInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(EmordeInfo baseInfo, UselisInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner &&
 				baseInfo.codUser == selectedInfo.codUser);
@@ -29,11 +22,5 @@ final class EmordeVisiMergeUselis extends InfoMergerVisitorTemplate<EmordeInfo, 
 		
 		results.add(baseInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<EmordeInfo> getUniquifier() {
-		return null;
 	}
 }
