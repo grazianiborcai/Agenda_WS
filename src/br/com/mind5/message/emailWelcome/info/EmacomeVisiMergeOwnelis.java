fe5c054a0a9c6ebac275f609fac5504b6fd1582a
mmaciel <mmaciel@mind5.com>
2021-01-06 07:32:27 -0300

    Emacome #06

--- src/br/com/mind5/message/emailWelcome/info/EmacomeVisiMergeOwnelis.java
+++ src/br/com/mind5/message/emailWelcome/info/EmacomeVisiMergeOwnelis.java
@@ -5,16 +5,9 @@ import java.util.List;
 
 import br.com.mind5.business.ownerList.info.OwnelisInfo;
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class EmacomeVisiMergeOwnelis extends InfoMergerVisitorTemplate<EmacomeInfo, OwnelisInfo> {
-	
-	@Override public List<EmacomeInfo> beforeMerge(List<EmacomeInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(EmacomeInfo baseInfo, OwnelisInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class EmacomeVisiMergeOwnelis extends InfoMergerVisitorTemplate<EmacomeInf
 		
 		results.add(baseInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<EmacomeInfo> getUniquifier() {
-		return null;
 	}
 }
--- src/br/com/mind5/message/emailWelcome/info/EmacomeVisiMergeUselis.java
+++ src/br/com/mind5/message/emailWelcome/info/EmacomeVisiMergeUselis.java
@@ -4,17 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 import br.com.mind5.security.userList.info.UselisInfo;
 
 final class EmacomeVisiMergeUselis extends InfoMergerVisitorTemplate<EmacomeInfo, UselisInfo> {
-	
-	@Override public List<EmacomeInfo> beforeMerge(List<EmacomeInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(EmacomeInfo baseInfo, UselisInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner &&
 				baseInfo.codUser == selectedInfo.codUser);
@@ -29,11 +22,5 @@ final class EmacomeVisiMergeUselis extends InfoMergerVisitorTemplate<EmacomeInfo
 		
 		results.add(baseInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<EmacomeInfo> getUniquifier() {
-		return null;
 	}
 }
