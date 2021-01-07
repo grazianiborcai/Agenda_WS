0bf73ebb057796518ee05951115ed58f7ae441f2
mmaciel <mmaciel@mind5.com>
2021-01-07 09:55:39 -0300

    Stolarg #04

--- src/br/com/mind5/business/storeLeaveDateRange/info/StolargVisiMergeToSelect.java
+++ src/br/com/mind5/business/storeLeaveDateRange/info/StolargVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class StolargVisiMergeToSelect extends InfoMergerVisitorTemplate<StolargInfo, StolargInfo> {
-	
-	@Override public List<StolargInfo> beforeMerge(List<StolargInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(StolargInfo baseInfo, StolargInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class StolargVisiMergeToSelect extends InfoMergerVisitorTemplate<StolargIn
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<StolargInfo> getUniquifier() {
-		return null;
 	}
 }
