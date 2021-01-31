84fe6e9aa350b0377c2c0e3f3026f1ccb316f156
mmaciel <mmaciel@mind5.com>
2021-01-31 19:05:05 -0300

    Cartem #22

--- src/br/com/mind5/business/cartItem/model/checker/CartemCheckExistUser.java
+++ src/br/com/mind5/business/cartItem/model/checker/CartemCheckExistUser.java
@@ -1,23 +1,23 @@
 package br.com.mind5.business.cartItem.model.checker;
 
 import br.com.mind5.business.cartItem.info.CartemInfo;
-import br.com.mind5.business.cartItem.model.action.StdCartemDaoSelect;
+import br.com.mind5.business.cartItem.model.decisionTree.RootCartemSearchUser;
 import br.com.mind5.common.SystemCode;
 import br.com.mind5.model.action.ActionStd;
 import br.com.mind5.model.checker.ModelCheckerOption;
 import br.com.mind5.model.checker.ModelCheckerTemplateAction;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
-public final class CartemCheckExist extends ModelCheckerTemplateAction<CartemInfo, CartemInfo> {
+public final class CartemCheckExistUser extends ModelCheckerTemplateAction<CartemInfo, CartemInfo> {
 	
-	public CartemCheckExist(ModelCheckerOption option) {
+	public CartemCheckExistUser(ModelCheckerOption option) {
 		super(option, CartemInfo.class);
 	}
 	
 
 	
 	@Override protected ActionStd<CartemInfo> buildActionHook(DeciTreeOption<CartemInfo> option) {
-		ActionStd<CartemInfo> select = new StdCartemDaoSelect(option);
+		ActionStd<CartemInfo> select = new RootCartemSearchUser(option).toAction();
 		return select;
 	}
 	
