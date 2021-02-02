d97875850d7eed97d6d807b2f0810282c90ef568
mmaciel <mmaciel@mind5.com>
2021-02-02 00:30:42 -0300

    Compnap #05

--- src/br/com/mind5/business/companySnapshot/info/CompnapVisiMergeToSelect.java
+++ src/br/com/mind5/business/companySnapshot/info/CompnapVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class CompnapVisiMergeToSelect extends InfoMergerVisitorTemplate<CompnapInfo, CompnapInfo> {
-	
-	@Override public List<CompnapInfo> beforeMerge(List<CompnapInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(CompnapInfo baseInfo, CompnapInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -29,11 +22,5 @@ final class CompnapVisiMergeToSelect extends InfoMergerVisitorTemplate<CompnapIn
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<CompnapInfo> getUniquifier() {
-		return null;
 	}
 }
