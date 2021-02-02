e5bb81a47b8bdbb4a3bdb8db415bb21926d5c8d2
mmaciel <mmaciel@mind5.com>
2021-02-02 00:28:51 -0300

    Compco #04

--- src/br/com/mind5/business/companyConflict/info/CompcoVisiMergeToSelect.java
+++ src/br/com/mind5/business/companyConflict/info/CompcoVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class CompcoVisiMergeToSelect extends InfoMergerVisitorTemplate<CompcoInfo, CompcoInfo> {
-	
-	@Override public List<CompcoInfo> beforeMerge(List<CompcoInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(CompcoInfo baseInfo, CompcoInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -29,11 +22,5 @@ final class CompcoVisiMergeToSelect extends InfoMergerVisitorTemplate<CompcoInfo
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<CompcoInfo> getUniquifier() {
-		return null;
 	}
 }
