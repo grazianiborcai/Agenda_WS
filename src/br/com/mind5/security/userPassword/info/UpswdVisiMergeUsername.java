13fb3af03a555ff04c943e7d14ca66a75d4a5e8a
mmaciel <mmaciel@mind5.com>
2021-02-02 23:52:37 -0300

    Upswd #22

--- src/br/com/mind5/security/userPassword/info/UpswdVisiMergeUsername.java
+++ src/br/com/mind5/security/userPassword/info/UpswdVisiMergeUsername.java
@@ -4,17 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 import br.com.mind5.security.username.info.UsernameInfo;
 
 final class UpswdVisiMergeUsername extends InfoMergerVisitorTemplate<UpswdInfo, UsernameInfo> {
-	
-	@Override public List<UpswdInfo> beforeMerge(List<UpswdInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(UpswdInfo baseInfo, UsernameInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner		&&
 				baseInfo.username.equals(selectedInfo.username)		);
@@ -29,11 +22,5 @@ final class UpswdVisiMergeUsername extends InfoMergerVisitorTemplate<UpswdInfo, 
 		
 		results.add(baseInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<UpswdInfo> getUniquifier() {
-		return null;
 	}
 }
