7ace1093b53ff78832379306e348a17ad047495c
mmaciel <mmaciel@mind5.com>
2021-02-02 23:38:24 -0300

    Payormarch #10

--- src/br/com/mind5/payment/payOrderItemSearch/info/PayormarchVisiMergeToSelect.java
+++ src/br/com/mind5/payment/payOrderItemSearch/info/PayormarchVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class PayormarchVisiMergeToSelect extends InfoMergerVisitorTemplate<PayormarchInfo, PayormarchInfo> {
-	
-	@Override public List<PayormarchInfo> beforeMerge(List<PayormarchInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(PayormarchInfo baseInfo, PayormarchInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class PayormarchVisiMergeToSelect extends InfoMergerVisitorTemplate<Payorm
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<PayormarchInfo> getUniquifier() {
-		return null;
 	}
 }
