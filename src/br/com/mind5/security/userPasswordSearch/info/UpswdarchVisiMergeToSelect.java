f9448ac803879f201a4556e3fc50862e47ec0399
mmaciel <mmaciel@mind5.com>
2021-02-02 23:52:59 -0300

    Upswdarch #03

--- src/br/com/mind5/security/userPasswordSearch/info/UpswdarchVisiMergeToSelect.java
+++ src/br/com/mind5/security/userPasswordSearch/info/UpswdarchVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class UpswdarchVisiMergeToSelect extends InfoMergerVisitorTemplate<UpswdarchInfo, UpswdarchInfo> {
-	
-	@Override public List<UpswdarchInfo> beforeMerge(List<UpswdarchInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(UpswdarchInfo baseInfo, UpswdarchInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}	
@@ -27,11 +20,5 @@ final class UpswdarchVisiMergeToSelect extends InfoMergerVisitorTemplate<Upswdar
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<UpswdarchInfo> getUniquifier() {
-		return null;
 	}
 }
--- src/br/com/mind5/security/userPasswordSearch/info/UpswdarchVisiMergeUsername.java
+++ src/br/com/mind5/security/userPasswordSearch/info/UpswdarchVisiMergeUsername.java
@@ -4,17 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 import br.com.mind5.security.username.info.UsernameInfo;
 
 final class UpswdarchVisiMergeUsername extends InfoMergerVisitorTemplate<UpswdarchInfo, UsernameInfo> {
-	
-	@Override public List<UpswdarchInfo> beforeMerge(List<UpswdarchInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(UpswdarchInfo baseInfo, UsernameInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner		&&
 				baseInfo.username.equals(selectedInfo.username)		);
@@ -29,11 +22,5 @@ final class UpswdarchVisiMergeUsername extends InfoMergerVisitorTemplate<Upswdar
 		
 		results.add(baseInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<UpswdarchInfo> getUniquifier() {
-		return null;
 	}
 }
