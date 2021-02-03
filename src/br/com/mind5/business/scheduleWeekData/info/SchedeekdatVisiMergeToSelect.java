2fb15983ca1b503c4e9478e02dcc1750149c3b42
mmaciel <mmaciel@mind5.com>
2021-02-02 23:07:16 -0300

    Schedeekdat #07

--- src/br/com/mind5/business/scheduleWeekData/info/SchedeekdatVisiMergeToSelect.java
+++ src/br/com/mind5/business/scheduleWeekData/info/SchedeekdatVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class SchedeekdatVisiMergeToSelect extends InfoMergerVisitorTemplate<SchedeekdatInfo, SchedeekdatInfo> {
-	
-	@Override public List<SchedeekdatInfo> beforeMerge(List<SchedeekdatInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(SchedeekdatInfo baseInfo, SchedeekdatInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class SchedeekdatVisiMergeToSelect extends InfoMergerVisitorTemplate<Sched
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<SchedeekdatInfo> getUniquifier() {
-		return null;
 	}
 }
