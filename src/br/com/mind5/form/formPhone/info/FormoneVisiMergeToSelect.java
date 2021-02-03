dd3295c7f4620df4b2aad420cb198a887966b604
mmaciel <mmaciel@mind5.com>
2021-02-02 23:11:28 -0300

    Formone #06

--- src/br/com/mind5/form/formPhone/info/FormoneVisiMergeToSelect.java
+++ src/br/com/mind5/form/formPhone/info/FormoneVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class FormoneVisiMergeToSelect extends InfoMergerVisitorTemplate<FormoneInfo, FormoneInfo> {
-	
-	@Override public List<FormoneInfo> beforeMerge(List<FormoneInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(FormoneInfo baseInfo, FormoneInfo selectedInfo) {
 		return true;
 	}
@@ -27,11 +20,5 @@ final class FormoneVisiMergeToSelect extends InfoMergerVisitorTemplate<FormoneIn
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<FormoneInfo> getUniquifier() {
-		return null;
 	}
 }
