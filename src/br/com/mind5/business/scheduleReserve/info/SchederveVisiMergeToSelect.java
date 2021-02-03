2288be20174502b29b04fff83044fce2e38600c9
mmaciel <mmaciel@mind5.com>
2021-02-02 23:06:00 -0300

    Schederve #04

--- src/br/com/mind5/business/scheduleReserve/info/SchederveVisiMergeToSelect.java
+++ src/br/com/mind5/business/scheduleReserve/info/SchederveVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class SchederveVisiMergeToSelect extends InfoMergerVisitorTemplate<SchederveInfo, SchederveInfo> {
-	
-	@Override public List<SchederveInfo> beforeMerge(List<SchederveInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(SchederveInfo baseInfo, SchederveInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class SchederveVisiMergeToSelect extends InfoMergerVisitorTemplate<Scheder
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<SchederveInfo> getUniquifier() {
-		return null;
 	}
 }
