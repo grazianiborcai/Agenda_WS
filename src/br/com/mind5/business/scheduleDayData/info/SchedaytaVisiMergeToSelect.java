44760353264c67561cab9a2c450e62aa015aa739
mmaciel <mmaciel@mind5.com>
2021-02-02 23:03:23 -0300

    Schedayta #08

--- src/br/com/mind5/business/scheduleDayData/info/SchedaytaVisiMergeToSelect.java
+++ src/br/com/mind5/business/scheduleDayData/info/SchedaytaVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class SchedaytaVisiMergeToSelect extends InfoMergerVisitorTemplate<SchedaytaInfo, SchedaytaInfo> {
-	
-	@Override public List<SchedaytaInfo> beforeMerge(List<SchedaytaInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(SchedaytaInfo baseInfo, SchedaytaInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class SchedaytaVisiMergeToSelect extends InfoMergerVisitorTemplate<Scheday
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<SchedaytaInfo> getUniquifier() {
-		return null;
 	}
 }
