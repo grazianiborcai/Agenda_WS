9ae9f52adc7c3d7bc4b73552f21e7a7411d0b564
mmaciel <mmaciel@mind5.com>
2021-02-02 22:03:00 -0300

    Phonarch #11

--- src/br/com/mind5/business/phoneSearch/info/PhonarchVisiMergeToSelect.java
+++ src/br/com/mind5/business/phoneSearch/info/PhonarchVisiMergeToSelect.java
@@ -4,16 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.info.InfoMergerVisitorTemplate;
-import br.com.mind5.info.InfoUniquifier;
 
 final class PhonarchVisiMergeToSelect extends InfoMergerVisitorTemplate<PhonarchInfo, PhonarchInfo> {
-	
-	@Override public List<PhonarchInfo> beforeMerge(List<PhonarchInfo> baseInfos) {
-		return baseInfos;
-	}
-	
-	
-	
+
 	@Override public boolean shouldMerge(PhonarchInfo baseInfo, PhonarchInfo selectedInfo) {
 		return (baseInfo.codOwner == selectedInfo.codOwner);
 	}
@@ -28,11 +21,5 @@ final class PhonarchVisiMergeToSelect extends InfoMergerVisitorTemplate<Phonarch
 		
 		results.add(selectedInfo);
 		return results;
-	}
-	
-	
-	
-	@Override public InfoUniquifier<PhonarchInfo> getUniquifier() {
-		return null;
 	}
 }
