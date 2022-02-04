8da65b5f8295be5ff2f47e8757800a3d96ed199e
mmaciel <mmaciel@mind5.com.br>
2022-02-04 14:07:58 -0300

    Storac #05

--- src/br/com/mind5/stats/statsStoreAccount/storeAccount/info/StoracSetterZerofy.java
+++ src/br/com/mind5/stats/statsStoreAccount/storeAccount/info/StoracSetterZerofy.java
@@ -5,16 +5,16 @@ import br.com.mind5.info.InfoSetterTemplate;
 public final class StoracSetterZerofy extends InfoSetterTemplate<StoracInfo> {
 	
 	@Override protected StoracInfo setAttrHook(StoracInfo recordInfo) {
-		recordInfo.countStoreMonth = 0;
+		recordInfo.countStoreCreatedMonth = 0;
 		recordInfo.countStoreCompletedMonth = 0;
 		recordInfo.countStorePendingMonth = 0;
-		recordInfo.countStoreMonthLastYear = 0;
+		recordInfo.countStoreCreatedMonthLastYear = 0;
 		recordInfo.countStoreCompletedMonthLastYear = 0;
 		recordInfo.countStorePendingMonthLastYear = 0;
-		recordInfo.countStoreVar = 0;
+		recordInfo.countStoreCreatedVar = 0;
 		recordInfo.countStoreCompletedVar = 0;
 		recordInfo.countStorePendingVar = 0;
-		recordInfo.countStoreCumulative = 0;
+		recordInfo.countStoreCreatedCumulative = 0;
 		recordInfo.countStoreCompletedCumulative = 0;
 		recordInfo.countStorePendingCumulative = 0;
 
