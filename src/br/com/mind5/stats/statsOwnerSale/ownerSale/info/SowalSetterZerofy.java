66ac0b2684c2a35dfe3ad2db7bd7ec5fac1f4c76
mmaciel <mmaciel@mind5.com.br>
2022-02-11 09:42:00 -0300

    Sowal #02

--- src/br/com/mind5/stats/statsOwnerSale/ownerSale/info/SowalSetterZerofy.java
+++ src/br/com/mind5/stats/statsOwnerSale/ownerSale/info/SowalSetterZerofy.java
@@ -5,15 +5,28 @@ import br.com.mind5.info.InfoSetterTemplate;
 public final class SowalSetterZerofy extends InfoSetterTemplate<SowalInfo> {
 	
 	@Override protected SowalInfo setAttrHook(SowalInfo recordInfo) {
-		recordInfo.totalFeeMonth = 0;
-		recordInfo.totalSaleMonth = 0;
-		recordInfo.countOrderMonth = 0;
-		recordInfo.totalFeeLastYear = 0;
-		recordInfo.totalSaleMonthLastYear = 0;
-		recordInfo.countOrderLastYear = 0;
-		recordInfo.totalFeeVar = 0;
-		recordInfo.totalSaleVar = 0;
-		recordInfo.countOrderVar = 0;
+		recordInfo.totalFee12m 				= 0;
+		recordInfo.totalFee30d 				= 0;
+		recordInfo.totalFeeCancelled12m 	= 0;
+		recordInfo.totalFeeCancelled30d 	= 0;
+		recordInfo.totalFeePaid12m 			= 0;
+		recordInfo.totalFeePaid30d 			= 0;
+		recordInfo.totalFeePlaced12m 		= 0;
+		recordInfo.totalFeePlaced30d 		= 0;
+		recordInfo.totalFeeWaiting12m 		= 0;
+		recordInfo.totalFeeWaiting30d 		= 0;	
+		recordInfo.totalSale12m 			= 0;
+		recordInfo.totalSale30d 			= 0;
+		recordInfo.totalSaleCancelled12m 	= 0;
+		recordInfo.totalSaleCancelled30d 	= 0;	
+		recordInfo.totalSaleCreated12m 		= 0;
+		recordInfo.totalSaleCreated30d 		= 0;	
+		recordInfo.totalSalePlaced12m 		= 0;
+		recordInfo.totalSalePlaced30d 		= 0;
+		recordInfo.totalSalePaid12m 		= 0;
+		recordInfo.totalSalePaid30d 		= 0;
+		recordInfo.totalSaleWaiting12m 		= 0;
+		recordInfo.totalSaleWaiting30d 		= 0;
 
 		return recordInfo;
 	}
