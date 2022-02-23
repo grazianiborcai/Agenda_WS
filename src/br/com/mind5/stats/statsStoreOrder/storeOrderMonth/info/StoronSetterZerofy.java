071e206db1f479aad0be04712858e3f73b913713
mmaciel <mmaciel@mind5.com.br>
2022-02-23 13:25:20 -0300

    Storon #02

--- src/br/com/mind5/stats/statsStoreOrder/storeOrderMonth/info/StoronSetterZerofy.java
+++ src/br/com/mind5/stats/statsStoreOrder/storeOrderMonth/info/StoronSetterZerofy.java
@@ -23,6 +23,24 @@ public final class StoronSetterZerofy extends InfoSetterTemplate<StoronInfo> {
 		recordInfo.totalFeePaidMonth = 0;
 		recordInfo.totalFeePlacedMonth = 0;
 		recordInfo.totalFeeCancelledMonth = 0;
+		recordInfo.countOrderTotalLastYear = 0;
+		recordInfo.countOrderCreatedLastYear = 0;
+		recordInfo.countOrderWaitingLastYear = 0;
+		recordInfo.countOrderPaidLastYear = 0;
+		recordInfo.countOrderPlacedLastYear = 0;
+		recordInfo.countOrderCancelledLastYear = 0;
+		recordInfo.totalSaleLastYear = 0;
+		recordInfo.totalSaleCreatedLastYear = 0;
+		recordInfo.totalSaleWaitingLastYear = 0;
+		recordInfo.totalSalePaidLastYear = 0;
+		recordInfo.totalSalePlacedLastYear = 0;
+		recordInfo.totalSaleCancelledLastYear = 0;
+		recordInfo.totalFeeLastYear = 0;
+		recordInfo.totalFeeCreatedLastYear = 0;
+		recordInfo.totalFeeWaitingLastYear = 0;
+		recordInfo.totalFeePaidLastYear = 0;
+		recordInfo.totalFeePlacedLastYear = 0;
+		recordInfo.totalFeeCancelledLastYear = 0;
 
 		return recordInfo;
 	}
