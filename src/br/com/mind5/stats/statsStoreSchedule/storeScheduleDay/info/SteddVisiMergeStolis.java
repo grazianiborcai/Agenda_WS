85d3e327eab1b3740b3f30a09857de7d0904f5be
mmaciel <mmaciel@mind5.com.br>
2022-03-02 10:07:02 -0300

    Stedd #07

--- src/br/com/mind5/stats/statsStoreSchedule/storeScheduleDay/info/SteddVisiMergeStolis.java
+++ src/br/com/mind5/stats/statsStoreSchedule/storeScheduleDay/info/SteddVisiMergeStolis.java
@@ -18,6 +18,13 @@ final class SteddVisiMergeStolis extends InfoMergerVisitorTemplate<SteddInfo, St
 	@Override public List<SteddInfo> merge(SteddInfo baseInfo, StolisInfo selectedInfo) {
 		List<SteddInfo> results = new ArrayList<>();
 		
+		baseInfo.codCountry = "N/A";
+		baseInfo.txtCountry = "N/A";
+		baseInfo.codState 	= "N/A";
+		baseInfo.txtState 	= "N/A";
+		baseInfo.city 		= "N/A";
+		
+		
 		if (selectedInfo.addressData != null) {
 			baseInfo.codCountry = selectedInfo.addressData.codCountry;
 			baseInfo.txtCountry = selectedInfo.addressData.txtCountry;
