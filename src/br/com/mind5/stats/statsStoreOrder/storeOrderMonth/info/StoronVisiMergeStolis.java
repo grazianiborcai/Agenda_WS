f835310a783a932733b689fb4bd7b94975328140
mmaciel <mmaciel@mind5.com.br>
2022-03-02 10:08:10 -0300

    Storon #04

--- src/br/com/mind5/stats/statsStoreOrder/storeOrderMonth/info/StoronVisiMergeStolis.java
+++ src/br/com/mind5/stats/statsStoreOrder/storeOrderMonth/info/StoronVisiMergeStolis.java
@@ -18,6 +18,13 @@ final class StoronVisiMergeStolis extends InfoMergerVisitorTemplate<StoronInfo, 
 	@Override public List<StoronInfo> merge(StoronInfo baseInfo, StolisInfo selectedInfo) {
 		List<StoronInfo> results = new ArrayList<>();
 		
+		baseInfo.codCountry = "N/A";
+		baseInfo.txtCountry = "N/A";
+		baseInfo.codState 	= "N/A";
+		baseInfo.txtState 	= "N/A";
+		baseInfo.city 		= "N/A";
+		
+		
 		if (selectedInfo.addressData != null) {
 			baseInfo.codCountry = selectedInfo.addressData.codCountry;
 			baseInfo.txtCountry = selectedInfo.addressData.txtCountry;
