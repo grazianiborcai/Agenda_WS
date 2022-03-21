223bab9760204e5f562109447d280f1d6f5dcdfd
mmaciel <mmaciel@mind5.com.br>
2022-03-21 01:04:26 -0300

    Sowash #10

--- src/br/com/mind5/stats/statsOwnerDashboard/model/decisionTree/SowashRootSelect.java
+++ src/br/com/mind5/stats/statsOwnerDashboard/model/decisionTree/SowashRootSelect.java
@@ -50,11 +50,11 @@ public final class SowashRootSelect extends DeciTreeTemplateWrite<SowashInfo> {
 		List<ActionStd<SowashInfo>> actions = new ArrayList<>();
 
 		ActionStd<SowashInfo> mergeSowot = new ActionStdCommom<SowashInfo>(option, SowashVisiMergeSowot.class);
-		ActionLazy<SowashInfo> mergeSoword = new ActionLazyCommom<SowashInfo>(option.conn, option.schemaName, SowashVisiMergeSoword.class);
-		ActionLazy<SowashInfo> mergeSowus = new ActionLazyCommom<SowashInfo>(option.conn, option.schemaName, SowashVisiMergeSowus.class);
-		ActionLazy<SowashInfo> mergeSowedul = new ActionLazyCommom<SowashInfo>(option.conn, option.schemaName, SowashVisiMergeSowedul.class);
-//		ActionLazy<SowashInfo> mergeSowal = new ActionLazyCommom<SowashInfo>(option.conn, option.schemaName, SowashVisiMergeSowal.class);
-		ActionLazy<SowashInfo> enforceLChanged = new ActionLazyCommom<SowashInfo>(option.conn, option.schemaName, SowashVisiEnforceLChanged.class);
+		ActionLazy<SowashInfo> mergeSoword = new ActionLazyCommom<SowashInfo>(option, SowashVisiMergeSoword.class);
+		ActionLazy<SowashInfo> mergeSowus = new ActionLazyCommom<SowashInfo>(option, SowashVisiMergeSowus.class);
+		ActionLazy<SowashInfo> mergeSowedul = new ActionLazyCommom<SowashInfo>(option, SowashVisiMergeSowedul.class);
+//		ActionLazy<SowashInfo> mergeSowal = new ActionLazyCommom<SowashInfo>(option, SowashVisiMergeSowal.class);
+		ActionLazy<SowashInfo> enforceLChanged = new ActionLazyCommom<SowashInfo>(option, SowashVisiEnforceLChanged.class);
 		
 		
 		mergeSowot.addPostAction(mergeSoword);
