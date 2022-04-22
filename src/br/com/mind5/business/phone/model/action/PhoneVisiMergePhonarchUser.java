37c228e6bfba699d90c76f6a1bc747e0e054df07
mmaciel <mmaciel@mind5.com>
2022-04-22 06:51:12 -0300

    Phone #37

---
+++ src/br/com/mind5/business/phone/model/action/PhoneVisiMergePhonarchUser.java
@@ -0,0 +1,36 @@
+package br.com.mind5.business.phone.model.action;
+
+import java.util.List;
+
+import br.com.mind5.business.phone.info.PhoneInfo;
+import br.com.mind5.business.phone.info.PhoneMerger;
+import br.com.mind5.business.phoneSearch.info.PhonarchInfo;
+import br.com.mind5.business.phoneSearch.model.decisionTree.PhonarchRootSelectUser;
+import br.com.mind5.model.action.ActionVisitorTemplateMerge;
+import br.com.mind5.model.decisionTree.DeciTree;
+import br.com.mind5.model.decisionTree.DeciTreeOption;
+
+public final class PhoneVisiMergePhonarchUser extends ActionVisitorTemplateMerge<PhoneInfo, PhonarchInfo> {
+	
+	public PhoneVisiMergePhonarchUser(DeciTreeOption<PhoneInfo> option) {
+		super(option, PhonarchInfo.class);
+	}
+	
+	
+	
+	@Override protected Class<? extends DeciTree<PhonarchInfo>> getTreeClassHook() {
+		return PhonarchRootSelectUser.class;
+	}
+	
+	
+	
+	@Override protected List<PhoneInfo> mergeHook(List<PhoneInfo> baseInfos, List<PhonarchInfo> selectedInfos) {	
+		return PhoneMerger.mergeWithPhonarch(baseInfos, selectedInfos);
+	}
+	
+	
+	
+	@Override protected boolean shouldMergeWhenEmptyHook() {
+		return super.DONT_MERGE_WHEN_EMPTY;
+	}
+}
