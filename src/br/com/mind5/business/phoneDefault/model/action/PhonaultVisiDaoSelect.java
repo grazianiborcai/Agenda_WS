fb49285d4dbb260c01afe6c5f7180669ee9f199e
mmaciel <mmaciel@mind5.com>
2022-04-23 22:30:55 -0300

    Phonault #04

--- src/br/com/mind5/business/phoneDefault/model/action/PhonaultVisiDaoSelect.java
+++ src/br/com/mind5/business/phoneDefault/model/action/PhonaultVisiDaoSelect.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import br.com.mind5.business.phoneDefault.dao.PhonaultDaoSelect;
 import br.com.mind5.business.phoneDefault.info.PhonaultInfo;
-import br.com.mind5.dao.DaoStmtExecOption;
 import br.com.mind5.dao.DaoStmtExec;
+import br.com.mind5.dao.DaoStmtExecOption;
 import br.com.mind5.model.action.ActionVisitorTemplateStmt;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 
-final class PhonaultVisiDaoSelect extends ActionVisitorTemplateStmt<PhonaultInfo> {
+public final class PhonaultVisiDaoSelect extends ActionVisitorTemplateStmt<PhonaultInfo> {
 
 	public PhonaultVisiDaoSelect(DeciTreeOption<PhonaultInfo> option) {
 		super(option);
