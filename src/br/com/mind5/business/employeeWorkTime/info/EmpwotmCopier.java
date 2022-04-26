3b191b9dac90f991baf5e0f0dcbf1cd52ee6ca69
mmaciel <mmaciel@mind5.com>
2022-04-26 06:22:14 -0300

    Empwotm #50

--- src/br/com/mind5/business/employeeWorkTime/info/EmpwotmCopier.java
+++ src/br/com/mind5/business/employeeWorkTime/info/EmpwotmCopier.java
@@ -6,7 +6,7 @@ import br.com.mind5.business.employee.info.EmpInfo;
 import br.com.mind5.info.InfoCopierOneToMany;
 
 public final class EmpwotmCopier {
-	public static List<EmpwotmInfo> copyFromStore(List<EmpInfo> sources) {
+	public static List<EmpwotmInfo> copyFromEmp(List<EmpInfo> sources) {
 		InfoCopierOneToMany<EmpwotmInfo, EmpInfo> copier = new EmpwotmCopyEmp();
 		return copier.makeCopy(sources);
 	}	
