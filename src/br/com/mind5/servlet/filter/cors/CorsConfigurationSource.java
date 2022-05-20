a910d0c9346917d6312301ef0a90a75e16a5624c
mmaciel <mmaciel@mind5.com>
2022-05-20 16:03:34 -0300

    Servlet #15

---
+++ src/br/com/mind5/servlet/filter/cors/CorsConfigurationSource.java
@@ -0,0 +1,31 @@
+package br.com.mind5.servlet.filter.cors;
+
+import javax.ws.rs.HttpMethod;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+public final class CorsConfigurationSource extends UrlBasedCorsConfigurationSource {
+	public CorsConfigurationSource() {
+		super();
+		
+		registerCorsConfiguration("/**", getCorsConfig());
+	}
+	
+	
+	
+	private CorsConfiguration getCorsConfig() {
+		long maxAge = 3600;
+		CorsConfiguration config = new CorsConfiguration();		
+		
+		config.addAllowedHeader("*"); 					// Exemplo: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization
+		config.addAllowedMethod(HttpMethod.GET);
+		config.addAllowedMethod(HttpMethod.PUT);
+		config.addAllowedMethod(HttpMethod.DELETE);
+		config.addAllowedOrigin("*");					// Exemplo: http://localhost:3000
+		config.setAllowCredentials(true);
+		config.setMaxAge(maxAge);
+		
+		return config;
+	}
+}
