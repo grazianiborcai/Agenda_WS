9c8e6b23abdcd8ee9bb8f38bc567dea024658253
mmaciel <mmaciel@mind5.com>
2022-08-02 19:55:46 -0300

    Fimg #39

--- src/br/com/mind5/file/fileImage/model/checker/FimgCheckInsertUser.java
+++ src/br/com/mind5/file/fileImage/model/checker/FimgCheckInsertUser.java
@@ -18,7 +18,6 @@ public final class FimgCheckInsertUser extends ModelCheckerTemplateSimple<FimgIn
 	@Override protected boolean checkHook(FimgInfo recordInfo, Connection conn, String schemaName) {	
 		if ( recordInfo.codOwner 	<= 0	||
 			 recordInfo.username 	== null ||
-			 recordInfo.username 	== null	||
 			 recordInfo.codLanguage == null		)
 			
 			return super.FAILED;			
