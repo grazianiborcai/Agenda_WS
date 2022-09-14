042d87bdcf8a728091f751bd871267fac360fbf4
mmaciel <mmaciel@mind5.com>
2022-09-14 08:36:38 -0300

    Store #66

--- src/br/com/mind5/business/store/model/action/StoreVisiMergeStuntm.java
+++ src/br/com/mind5/business/store/model/action/StoreVisiMergeStuntm.java
@@ -5,7 +5,7 @@ import java.util.List;
 import br.com.mind5.business.store.info.StoreInfo;
 import br.com.mind5.business.store.info.StoreMerger;
 import br.com.mind5.business.storeLunchTime.info.StuntmInfo;
-import br.com.mind5.business.storeLunchTime.model.decisionTree.StuntmRootSearchStore;
+import br.com.mind5.business.storeLunchTime.model.decisionTree.StuntmRootSearchStoreFallback;
 import br.com.mind5.model.action.ActionVisitorTemplateMerge;
 import br.com.mind5.model.decisionTree.DeciTree;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
@@ -19,7 +19,7 @@ public final class StoreVisiMergeStuntm extends ActionVisitorTemplateMerge<Store
 	
 	
 	@Override protected Class<? extends DeciTree<StuntmInfo>> getTreeClassHook() {
-		return StuntmRootSearchStore.class;
+		return StuntmRootSearchStoreFallback.class;
 	}
 	
 	
