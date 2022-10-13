2accf7bd6face197dbe3c94877570ff987163d00
mmaciel <mmaciel@mind5.com>
2022-10-13 10:03:35 -0300

    Owner #48

--- src/br/com/mind5/business/owner/info/OwnerSetterAddressKey.java
+++ src/br/com/mind5/business/owner/info/OwnerSetterAddressKey.java
@@ -1,6 +1,7 @@
 package br.com.mind5.business.owner.info;
 
 import br.com.mind5.business.address.info.AddressInfo;
+import br.com.mind5.common.DefaultValue;
 import br.com.mind5.info.InfoSetterTemplate;
 
 public final class OwnerSetterAddressKey extends InfoSetterTemplate<OwnerInfo> {
@@ -11,6 +12,12 @@ public final class OwnerSetterAddressKey extends InfoSetterTemplate<OwnerInfo> {
 			eachAddress.codOwnerRef = recordInfo.codOwner;
 			eachAddress.codLanguage = recordInfo.codLanguage;
 			eachAddress.username = recordInfo.username;
+			
+			eachAddress.codCustomer = DefaultValue.number();
+			eachAddress.codStore = DefaultValue.number();
+			eachAddress.codEmployee = DefaultValue.number();
+			eachAddress.codUser = DefaultValue.number();
+			eachAddress.codLegalPerson = DefaultValue.number();
 		}
 		
 		return recordInfo;
