5c888f09e8ff3c9212d73de7de4a5411c92cbc3f
mmaciel <mmaciel@mind5.com>
2022-10-13 17:53:49 -0300

    Personap #10

--- src/br/com/mind5/business/personSnapshot/info/PersonapCopier.java
+++ src/br/com/mind5/business/personSnapshot/info/PersonapCopier.java
@@ -4,25 +4,10 @@ package br.com.mind5.business.personSnapshot.info;
 import java.util.List;
 
 import br.com.mind5.business.employeeSnapshot.info.EmpnapInfo;
-import br.com.mind5.business.storeSnapshot.info.StorapInfo;
 import br.com.mind5.info.InfoCopier;
 import br.com.mind5.security.userSnapshot.info.UserapInfo;
 
 public final class PersonapCopier {		
-	public static PersonapInfo copyFromStorap(StorapInfo source) {
-		InfoCopier<PersonapInfo, StorapInfo> copier = new PersonapCopyStorap();
-		return copier.makeCopy(source);
-	}
-	
-	
-	
-	public static List<PersonapInfo> copyFromStorap(List<StorapInfo> sources) {
-		InfoCopier<PersonapInfo, StorapInfo> copier = new PersonapCopyStorap();
-		return copier.makeCopy(sources);
-	}
-	
-	
-	
 	public static PersonapInfo copyFromEmpnapKey(EmpnapInfo source) {
 		InfoCopier<PersonapInfo, EmpnapInfo> copier = new PersonapCopyEmpnapKey();
 		return copier.makeCopy(source);
