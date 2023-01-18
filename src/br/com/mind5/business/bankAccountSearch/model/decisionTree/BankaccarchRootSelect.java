fd95b4a1a18c958099ab70bb5f09038a53fcf866
mmaciel <mmaciel@mind5.com>
2023-01-18 16:27:24 -0300

    Bankaccarch #02

--- src/br/com/mind5/business/bankAccountSearch/model/decisionTree/BankaccarchRootSelect.java
+++ src/br/com/mind5/business/bankAccountSearch/model/decisionTree/BankaccarchRootSelect.java
@@ -4,22 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.business.bankAccountSearch.info.BankaccarchInfo;
-import br.com.mind5.business.bankAccountSearch.model.action.BankaccarchVisiEnforceStoreKey;
-import br.com.mind5.business.bankAccountSearch.model.action.BankaccarchVisiRootSelect;
-import br.com.mind5.model.action.ActionLazy;
+import br.com.mind5.business.bankAccountSearch.model.action.BankaccarchVisiMergeToSelect;
+import br.com.mind5.business.bankAccountSearch.model.checker.BankaccarchCheckLangu;
+import br.com.mind5.business.bankAccountSearch.model.checker.BankaccarchCheckOwner;
+import br.com.mind5.business.bankAccountSearch.model.checker.BankaccarchCheckRead;
 import br.com.mind5.model.action.ActionStd;
-import br.com.mind5.model.action.commom.ActionLazyCommom;
 import br.com.mind5.model.action.commom.ActionStdCommom;
 import br.com.mind5.model.checker.ModelChecker;
 import br.com.mind5.model.checker.ModelCheckerHelperQueue;
 import br.com.mind5.model.checker.ModelCheckerOption;
-import br.com.mind5.model.checker.common.ModelCheckerDummy;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 import br.com.mind5.model.decisionTree.DeciTreeTemplateRead;
 
-public final class BankaccnapRootSelectStore extends DeciTreeTemplateRead<BankaccarchInfo> {
+public final class BankaccarchRootSelect extends DeciTreeTemplateRead<BankaccarchInfo> {
 	
-	public BankaccnapRootSelectStore(DeciTreeOption<BankaccarchInfo> option) {
+	public BankaccarchRootSelect(DeciTreeOption<BankaccarchInfo> option) {
 		super(option);
 	}
 	
@@ -34,7 +33,21 @@ public final class BankaccnapRootSelectStore extends DeciTreeTemplateRead<Bankac
 		checkerOption.conn = option.conn;
 		checkerOption.schemaName = option.schemaName;
 		checkerOption.expectedResult = ModelCheckerOption.SUCCESS;	
-		checker = new ModelCheckerDummy<BankaccarchInfo>();
+		checker = new BankaccarchCheckRead(checkerOption);
+		queue.add(checker);		
+		
+		checkerOption = new ModelCheckerOption();
+		checkerOption.conn = option.conn;
+		checkerOption.schemaName = option.schemaName;
+		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;	
+		checker = new BankaccarchCheckLangu(checkerOption);
+		queue.add(checker);
+		
+		checkerOption = new ModelCheckerOption();
+		checkerOption.conn = option.conn;
+		checkerOption.schemaName = option.schemaName;
+		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;	
+		checker = new BankaccarchCheckOwner(checkerOption);
 		queue.add(checker);
 		
 		return new ModelCheckerHelperQueue<>(queue);
@@ -45,12 +58,9 @@ public final class BankaccnapRootSelectStore extends DeciTreeTemplateRead<Bankac
 	@Override protected List<ActionStd<BankaccarchInfo>> buildActionsOnPassedHook(DeciTreeOption<BankaccarchInfo> option) {
 		List<ActionStd<BankaccarchInfo>> actions = new ArrayList<>();
 		
-		ActionStd<BankaccarchInfo> enforceStoreKey = new ActionStdCommom<BankaccarchInfo>(option, BankaccarchVisiEnforceStoreKey.class);
-		ActionLazy<BankaccarchInfo> select = new ActionLazyCommom<BankaccarchInfo>(option, BankaccarchVisiRootSelect.class);
+		ActionStd<BankaccarchInfo> select = new ActionStdCommom<BankaccarchInfo>(option, BankaccarchVisiMergeToSelect.class);
 		
-		enforceStoreKey.addPostAction(select);
-		
-		actions.add(enforceStoreKey);
+		actions.add(select);
 		return actions;
 	}
 }
--- src/br/com/mind5/business/bankAccountSearch/model/decisionTree/BankaccarchRootSelectStore.java
+++ src/br/com/mind5/business/bankAccountSearch/model/decisionTree/BankaccarchRootSelectStore.java
@@ -4,21 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.mind5.business.bankAccountSearch.info.BankaccarchInfo;
-import br.com.mind5.business.bankAccountSearch.model.action.BankaccarchVisiMergeToSelect;
-import br.com.mind5.business.bankAccountSearch.model.checker.BankaccarchCheckLangu;
-import br.com.mind5.business.bankAccountSearch.model.checker.BankaccarchCheckOwner;
-import br.com.mind5.business.bankAccountSearch.model.checker.BankaccarchCheckRead;
+import br.com.mind5.business.bankAccountSearch.model.action.BankaccarchVisiEnforceStoreKey;
+import br.com.mind5.business.bankAccountSearch.model.action.BankaccarchVisiRootSelect;
+import br.com.mind5.model.action.ActionLazy;
 import br.com.mind5.model.action.ActionStd;
+import br.com.mind5.model.action.commom.ActionLazyCommom;
 import br.com.mind5.model.action.commom.ActionStdCommom;
 import br.com.mind5.model.checker.ModelChecker;
 import br.com.mind5.model.checker.ModelCheckerHelperQueue;
 import br.com.mind5.model.checker.ModelCheckerOption;
+import br.com.mind5.model.checker.common.ModelCheckerDummy;
 import br.com.mind5.model.decisionTree.DeciTreeOption;
 import br.com.mind5.model.decisionTree.DeciTreeTemplateRead;
 
-public final class BankaccnapRootSelect extends DeciTreeTemplateRead<BankaccarchInfo> {
+public final class BankaccarchRootSelectStore extends DeciTreeTemplateRead<BankaccarchInfo> {
 	
-	public BankaccnapRootSelect(DeciTreeOption<BankaccarchInfo> option) {
+	public BankaccarchRootSelectStore(DeciTreeOption<BankaccarchInfo> option) {
 		super(option);
 	}
 	
@@ -33,21 +34,7 @@ public final class BankaccnapRootSelect extends DeciTreeTemplateRead<Bankaccarch
 		checkerOption.conn = option.conn;
 		checkerOption.schemaName = option.schemaName;
 		checkerOption.expectedResult = ModelCheckerOption.SUCCESS;	
-		checker = new BankaccarchCheckRead(checkerOption);
-		queue.add(checker);		
-		
-		checkerOption = new ModelCheckerOption();
-		checkerOption.conn = option.conn;
-		checkerOption.schemaName = option.schemaName;
-		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;	
-		checker = new BankaccarchCheckLangu(checkerOption);
-		queue.add(checker);
-		
-		checkerOption = new ModelCheckerOption();
-		checkerOption.conn = option.conn;
-		checkerOption.schemaName = option.schemaName;
-		checkerOption.expectedResult = ModelCheckerOption.EXIST_ON_DB;	
-		checker = new BankaccarchCheckOwner(checkerOption);
+		checker = new ModelCheckerDummy<BankaccarchInfo>();
 		queue.add(checker);
 		
 		return new ModelCheckerHelperQueue<>(queue);
@@ -58,9 +45,12 @@ public final class BankaccnapRootSelect extends DeciTreeTemplateRead<Bankaccarch
 	@Override protected List<ActionStd<BankaccarchInfo>> buildActionsOnPassedHook(DeciTreeOption<BankaccarchInfo> option) {
 		List<ActionStd<BankaccarchInfo>> actions = new ArrayList<>();
 		
-		ActionStd<BankaccarchInfo> select = new ActionStdCommom<BankaccarchInfo>(option, BankaccarchVisiMergeToSelect.class);
+		ActionStd<BankaccarchInfo> enforceStoreKey = new ActionStdCommom<BankaccarchInfo>(option, BankaccarchVisiEnforceStoreKey.class);
+		ActionLazy<BankaccarchInfo> select = new ActionLazyCommom<BankaccarchInfo>(option, BankaccarchVisiRootSelect.class);
 		
-		actions.add(select);
+		enforceStoreKey.addPostAction(select);
+		
+		actions.add(enforceStoreKey);
 		return actions;
 	}
 }
