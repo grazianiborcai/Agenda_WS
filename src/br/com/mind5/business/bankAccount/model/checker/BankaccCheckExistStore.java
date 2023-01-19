34e8bdcf622d5149ba98d591f71f5b211a729c6f
mmaciel <mmaciel@mind5.com>
2023-01-19 11:34:48 -0300

    Bankacc #05

---
+++ src/br/com/mind5/business/bankAccount/model/checker/BankaccCheckExistStore.java
@@ -0,0 +1,45 @@
+package br.com.mind5.business.bankAccount.model.checker;
+
+import java.sql.Connection;
+
+import br.com.mind5.business.bankAccount.info.BankaccInfo;
+import br.com.mind5.business.bankAccount.model.decisionTree.BankaccRootSearchStore;
+import br.com.mind5.common.SystemCode;
+import br.com.mind5.common.SystemMessageBuilder;
+import br.com.mind5.message.sysMessage.info.SymsgInfo;
+import br.com.mind5.model.action.ActionStd;
+import br.com.mind5.model.checker.ModelCheckerOption;
+import br.com.mind5.model.checker.ModelCheckerTemplateAction;
+import br.com.mind5.model.decisionTree.DeciTreeOption;
+
+public final class BankaccCheckExistStore extends ModelCheckerTemplateAction<BankaccInfo, BankaccInfo> {	
+	
+	public BankaccCheckExistStore(ModelCheckerOption option) {
+		super(option, BankaccInfo.class);
+	}
+	
+	
+	
+	@Override protected ActionStd<BankaccInfo> buildActionHook(DeciTreeOption<BankaccInfo> option) {
+		ActionStd<BankaccInfo> select = new BankaccRootSearchStore(option).toAction();
+		return select;
+	}
+	
+	
+	
+	@Override protected SymsgInfo getSymsgOnResultTrueHook(Connection dbConn, String dbSchema, String codLangu) {
+		SystemMessageBuilder builder = new SystemMessageBuilder(dbConn, dbSchema, codLangu, SystemCode.GEN_P1_ALREADY_EXIST_F);
+		builder.addParam01(SystemCode.BANK_ACCOUNT);
+
+		return builder.build();
+	}
+	
+	
+	
+	@Override protected SymsgInfo getSymsgOnResultFalseHook(Connection dbConn, String dbSchema, String codLangu) {
+		SystemMessageBuilder builder = new SystemMessageBuilder(dbConn, dbSchema, codLangu, SystemCode.GEN_P1_NOT_FOUND_F);
+		builder.addParam01(SystemCode.BANK_ACCOUNT);
+
+		return builder.build();
+	}
+}
