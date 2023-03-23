ededb2824b68098ed985c128ec98170658fc65c7
mmaciel <mmaciel@mind5.com>
2023-03-23 13:10:25 -0300

    Langu #07

--- src/br/com/mind5/masterData/language/info/Langu.java
+++ src/br/com/mind5/masterData/language/info/Langu.java
@@ -2,13 +2,14 @@ package br.com.mind5.masterData.language.info;
 
 public enum Langu {
 	ENGLISH("EN"),
+	DEFAULT("EN"),
 	PORTUGUESE_BR("PT");
 	
 	
 	private final String codLanguage;
 	
 	
-	private Langu(String cod) {
+	private Langu(String cod) {			
 		codLanguage = cod;
 	}
 	
