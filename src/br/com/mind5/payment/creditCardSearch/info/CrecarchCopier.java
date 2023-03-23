10a1749bf48ca4f883992904c89be8a87f3424a7
mmaciel <mmaciel@mind5.com>
2023-03-23 13:35:41 -0300

    Crecarch #14

--- src/br/com/mind5/payment/creditCardSearch/info/CrecarchCopier.java
+++ src/br/com/mind5/payment/creditCardSearch/info/CrecarchCopier.java
@@ -4,7 +4,6 @@ package br.com.mind5.payment.creditCardSearch.info;
 import java.util.List;
 
 import br.com.mind5.info.InfoCopier;
-import br.com.mind5.payment.creditCard.info.CrecardInfo;
 import br.com.mind5.payment.payOrder.info.PayordInfo;
 import br.com.mind5.paymentPartner.partnerMoip.multiPayMoip.info.PaymoipInfo;
 
@@ -18,20 +17,6 @@ public final class CrecarchCopier {
 	
 	public static List<CrecarchInfo> copyFromPaymoip(List<PaymoipInfo> sources) {
 		InfoCopier<CrecarchInfo, PaymoipInfo> copier = new CrecarchCopyPaymoip();
-		return copier.makeCopy(sources);
-	}
-	
-	
-	
-	public static CrecarchInfo copyFromCrecard(CrecardInfo source) {
-		InfoCopier<CrecarchInfo, CrecardInfo> copier = new CrecarchCopyCrecard();
-		return copier.makeCopy(source);
-	}
-	
-	
-	
-	public static List<CrecarchInfo> copyFromCrecard(List<CrecardInfo> sources) {
-		InfoCopier<CrecarchInfo, CrecardInfo> copier = new CrecarchCopyCrecard();
 		return copier.makeCopy(sources);
 	}
 	
